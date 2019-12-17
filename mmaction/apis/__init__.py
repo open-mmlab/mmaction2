ea2b2c96949cc23617085fb7b5c8f50309075b69
Kai Chen <chenkaidev@gmail.com>
2019-12-17 17:57:02 +0800

    move train.py from api/ to core/

--- mmaction/apis/__init__.py
+++
@@ -1,3 +0,0 @@
-from .train import train_model
-
-__all__ = ['train_model']
--- mmaction/core/__init__.py
+++ mmaction/core/__init__.py
@@ -1,2 +1,3 @@
 from .dist_utils import *  # noqa: F401, F403
 from .fp16 import *  # noqa: F401, F403
+from .train import train_model  # noqa: F401
