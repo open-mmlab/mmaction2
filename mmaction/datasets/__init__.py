a5fc6d791bfb32896cabded01a45c29bc6194aef
linjintao <linjintao@sensetime.com>
2020-01-08 22:56:27 +0800

    add RepeatDataset
    add docstring for build_dataset

--- mmaction/datasets/__init__.py
+++ mmaction/datasets/__init__.py
@@ -1,5 +1,8 @@
 from .builder import build_dataset
+from .dataset_wrappers import RepeatDataset
 from .loader import build_dataloader
 from .video_dataset import VideoDataset
 
-__all__ = ['VideoDataset', 'build_dataset', 'build_dataloader']
+__all__ = [
+    'VideoDataset', 'build_dataset', 'build_dataloader', 'RepeatDataset'
+]
