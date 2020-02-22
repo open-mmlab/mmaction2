1cd8e283b7d8ed22a224661ddb968e8550623cc5
linjintao <linjintao@sensetime.com>
2020-02-22 12:55:38 +0800

    Fix sh scripts which take those in MMDet as a reference

--- tools/slurm_test.sh
+++ tools/slurm_test.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
 set -x
+
 PARTITION=$1
 JOB_NAME=$2
 CONFIG=$3
 CHECKPOINT=$4
-GPUS=${5}
-GPUS_PER_NODE=$GPUS
-CPUS_PER_TASK=$GPUS
+GPUS=${GPUS:-8}
+GPUS_PER_NODE=${GPUS_PER_NODE:-8}
+CPUS_PER_TASK=${CPUS_PER_TASK:-5}
+PY_ARGS=${@:5}
 SRUN_ARGS=${SRUN_ARGS:-""}
-PY_ARGS=${@:6}
-#PY_ARGS=${PY_ARGS:-""}
 
 srun -p ${PARTITION} \
     --job-name=${JOB_NAME} \
     --gres=gpu:${GPUS_PER_NODE} \
     --ntasks=${GPUS} \
-    --ntasks-per-node=${CPUS_PER_TASK} \
-    --cpus-per-task=2 \
+    --ntasks-per-node=${GPUS_PER_NODE} \
+    --cpus-per-task=${CPUS_PER_TASK} \
     --kill-on-bad-exit=1 \
     ${SRUN_ARGS} \
-    python3 -u tools/test.py ${CONFIG} ${CHECKPOINT} --launcher='slurm' ${PY_ARGS}
+    python -u tools/test.py ${CONFIG} ${CHECKPOINT} --launcher="slurm" ${PY_ARGS}
