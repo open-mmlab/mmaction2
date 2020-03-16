3d74f685ff825f75965700afa4ae1110cd2a4d2e
linjintao <linjintao@sensetime.com>
2020-03-16 17:25:26 +0800

    Add unittest for base class to remove redundant codes

--- mmaction/models/heads/__init__.py
+++ mmaction/models/heads/__init__.py
@@ -1,4 +1,5 @@
+from .base import BaseHead
 from .i3d_head import I3DHead
 from .tsn_head import TSNHead
 
-__all__ = ['TSNHead', 'I3DHead']
+__all__ = ['TSNHead', 'I3DHead', 'BaseHead']
--- tests/test_head.py
+++ tests/test_head.py
@@ -1,7 +1,27 @@
 import torch
 import torch.nn as nn
 
-from mmaction.models.heads import I3DHead, TSNHead
+from mmaction.models.heads import BaseHead, I3DHead, TSNHead
+
+
+class ExampleHead(BaseHead):
+
+    def init_weights(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+
+def test_base_head():
+    head = ExampleHead(3, 400)
+
+    cls_scores = torch.rand((3, 4))
+    # When truth is non-empty then cls loss should be nonzero for random inputs
+    gt_labels = torch.LongTensor([2] * 3).squeeze()
+    losses = head.loss(cls_scores, gt_labels)
+    assert 'loss_cls' in losses.keys()
+    assert losses.get('loss_cls') > 0, 'cls loss should be non-zero'
 
 
 def test_i3d_head():
@@ -25,17 +45,11 @@ def test_i3d_head():
     assert isinstance(self.avg_pool, nn.AdaptiveAvgPool3d)
     assert self.avg_pool.output_size == (1, 1, 1)
 
-    input_shape = (30, 2048, 4, 7, 7)
+    input_shape = (3, 2048, 4, 7, 7)
     feat = torch.rand(input_shape)
 
     cls_scores = self(feat)
-    assert cls_scores.shape == torch.Size([30, 4])
-
-    # When truth is non-empty then cls loss should be nonzero for random inputs
-    gt_labels = torch.LongTensor([2] * 30).squeeze()
-    losses = self.loss(cls_scores, gt_labels)
-    assert 'loss_cls' in losses.keys()
-    assert losses.get('loss_cls') > 0, 'cls loss should be non-zero'
+    assert cls_scores.shape == torch.Size([3, 4])
 
 
 def test_tsn_head():
@@ -60,15 +74,9 @@ def test_tsn_head():
     assert isinstance(self.avg_pool, nn.AdaptiveAvgPool2d)
     assert self.avg_pool.output_size == (1, 1)
 
-    input_shape = (250, 2048, 7, 7)
+    input_shape = (8, 2048, 7, 7)
     feat = torch.rand(input_shape)
 
     num_segs = input_shape[0]
     cls_scores = self(feat, num_segs)
     assert cls_scores.shape == torch.Size([1, 4])
-
-    # When truth is non-empty then cls loss should be nonzero for random inputs
-    gt_labels = torch.LongTensor([2])
-    losses = self.loss(cls_scores, gt_labels)
-    assert 'loss_cls' in losses.keys()
-    assert losses.get('loss_cls') > 0, 'cls loss should be non-zero'
