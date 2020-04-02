02db7f9faeb086a9b59e44e8addbb1c4ab12ecdd
chenkai <chenkai@sensetime.com>
2020-04-02 22:06:32 +0800

    remove loader folder and add samplers folder

--- mmaction/datasets/__init__.py
+++ mmaction/datasets/__init__.py
@@ -1,11 +1,10 @@
 from .base import BaseDataset
-from .builder import build_dataset
+from .builder import build_dataloader, build_dataset
 from .dataset_wrappers import RepeatDataset
-from .loader import build_dataloader
 from .rawframe_dataset import RawframeDataset
 from .video_dataset import VideoDataset
 
 __all__ = [
-    'VideoDataset', 'build_dataset', 'build_dataloader', 'RepeatDataset',
+    'VideoDataset', 'build_dataloader', 'build_dataset', 'RepeatDataset',
     'RawframeDataset', 'BaseDataset'
 ]
--- mmaction/datasets/loader/__init__.py
+++
@@ -1,4 +0,0 @@
-from .build_loader import build_dataloader
-from .sampler import DistributedSampler
-
-__all__ = ['build_dataloader', 'DistributedSampler']
