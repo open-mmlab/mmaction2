7ef06ab9419f5a040107fe6650098ccd6e02937e
lixuanyi <lixuanyi@sensetime.com>
2020-06-18 12:28:39 +0800

    fix
    Signed-off-by: lixuanyi <lixuanyi@sensetime.com>

--- configs/localization/bmn/bmn_400x100_2x8_9e_activitynet_feature.py
+++ configs/localization/bmn/bmn_400x100_2x8_9e_activitynet_feature.py
@@ -66,6 +66,7 @@ data = dict(
     videos_per_gpu=8,
     workers_per_gpu=8,
     train_dataloader=dict(drop_last=True),
+    val_dataloader=dict(videos_per_gpu=1),
     test=dict(
         type=dataset_type,
         ann_file=ann_file_test,
