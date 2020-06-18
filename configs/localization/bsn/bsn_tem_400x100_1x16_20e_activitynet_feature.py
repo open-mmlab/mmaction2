f3530dbc6625c9d219978d601d772a1f1a8b0ed5
lixuanyi <lixuanyi@sensetime.com>
2020-06-18 11:51:29 +0800

    update bsn/bmn modelzoo and improve bmn speed
    Signed-off-by: lixuanyi <lixuanyi@sensetime.com>

--- configs/localization/bsn/bsn_tem_400x100_1x16_20e_activitynet_feature.py
+++ configs/localization/bsn/bsn_tem_400x100_1x16_20e_activitynet_feature.py
@@ -1,15 +1,11 @@
 # model settings
 model = dict(
-    type='BMN',
+    type='TEM',
     temporal_dim=100,
-    boundary_ratio=0.5,
-    num_samples=32,
-    num_samples_per_bin=3,
-    feat_dim=400,
-    soft_nms_alpha=0.4,
-    soft_nms_low_threshold=0.5,
-    soft_nms_high_threshold=0.9,
-    post_process_top_k=100)
+    boundary_ratio=0.1,
+    tem_feat_dim=400,
+    tem_hidden_dim=512,
+    tem_match_threshold=0.5)
 # model training and testing settings
 train_cfg = None
 test_cfg = dict(average_clips='score')
@@ -19,7 +15,10 @@ data_root = 'data/activitynet_feature_cuhk/csv_mean_100/'
 data_root_val = 'data/activitynet_feature_cuhk/csv_mean_100/'
 ann_file_train = 'data/ActivityNet/anet_anno_train.json'
 ann_file_val = 'data/ActivityNet/anet_anno_val.json'
-ann_file_test = 'data/ActivityNet/anet_anno_test.json'
+ann_file_test = 'data/ActivityNet/anet_anno_full.json'
+
+work_dir = 'work_dirs/bsn_400x100_20e_1x16_activitynet_feature/'
+tem_results_dir = f'{work_dir}/tem_results/'
 
 test_pipeline = [
     dict(type='LoadLocalizationFeature'),
@@ -27,11 +26,8 @@ test_pipeline = [
         type='Collect',
         keys=['raw_feature'],
         meta_name='video_meta',
-        meta_keys=[
-            'video_name', 'duration_second', 'duration_frame', 'annotations',
-            'feature_frame'
-        ]),
-    dict(type='ToTensor', keys=['raw_feature']),
+        meta_keys=['video_name']),
+    dict(type='ToTensor', keys=['raw_feature'])
 ]
 train_pipeline = [
     dict(type='LoadLocalizationFeature'),
@@ -51,16 +47,14 @@ val_pipeline = [
         type='Collect',
         keys=['raw_feature', 'gt_bbox'],
         meta_name='video_meta',
-        meta_keys=[
-            'video_name', 'duration_second', 'duration_frame', 'annotations',
-            'feature_frame'
-        ]),
+        meta_keys=['video_name']),
     dict(type='ToTensor', keys=['raw_feature', 'gt_bbox']),
     dict(type='ToDataContainer', fields=[dict(key='gt_bbox', stack=False)])
 ]
+
 data = dict(
-    videos_per_gpu=8,
-    workers_per_gpu=4,
+    videos_per_gpu=16,
+    workers_per_gpu=8,
     train_dataloader=dict(drop_last=True),
     test=dict(
         type=dataset_type,
@@ -80,19 +74,19 @@ data = dict(
 
 # optimizer
 optimizer = dict(type='Adam', lr=0.001, weight_decay=0.0001)
+
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(policy='step', step=7)
 
-total_epochs = 9
-checkpoint_config = dict(interval=1)
-evaluation = dict(interval=1, metrics=['AR@AN'])
+total_epochs = 20
+checkpoint_config = dict(interval=1, filename_tmpl='tem_epoch_{}.pth')
+
 log_config = dict(interval=50, hooks=[dict(type='TextLoggerHook')])
 # runtime settings
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/bmn_400x100_9e_activitynet_feature/'
 load_from = None
 resume_from = None
-workflow = [('train', 1)]
-output_config = dict(out=f'{work_dir}/results.json', output_format='json')
+workflow = [('train', 1), ('val', 1)]
+output_config = dict(out=tem_results_dir, output_format='csv')
