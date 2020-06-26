5c2eb25ddfee318610d37ba684df7e2dd2b217eb
linjintao <linjintao@sensetime.com>
2020-06-26 22:10:08 +0800

    Calculate FLOPs from mmcv.

--- mmaction/utils/__init__.py
+++ mmaction/utils/__init__.py
@@ -1,9 +1,8 @@
 from .collect_env import collect_env
-from .flops_counter import get_model_complexity_info
 from .logger import get_root_logger
 from .misc import get_random_string, get_shm_dir, get_thread_id
 
 __all__ = [
-    'get_root_logger', 'collect_env', 'get_model_complexity_info',
-    'get_random_string', 'get_thread_id', 'get_shm_dir'
+    'get_root_logger', 'collect_env', 'get_random_string', 'get_thread_id',
+    'get_shm_dir'
 ]
