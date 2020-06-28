3e3f3a83079ebfe0f9ba12d4a7a8e9b7f1fa5700
lizz <lizz@sensetime.com>
2020-06-28 14:27:24 +0800

    Update docs/add_models.sh

--- docs/add_models.sh
+++ docs/add_models.sh
@@ -5,7 +5,7 @@ cat  ../configs/recognition/*/*.md > recognition_models.md
 sed -i 's/#/#&/' localization_models.md
 sed -i 's/#/#&/' recognition_models.md
 sed -i '1i\# Action Localization Models' localization_models.md
-sed -i '1i\# Action Recognition Model' recognition_models.md
+sed -i '1i\# Action Recognition Models' recognition_models.md
 cat index.rst | grep -q "recognition_models.md"
 if [ $? -ne 0 ] ;then
     sed -i '/api.rst/i\   recognition_models.md' index.rst
