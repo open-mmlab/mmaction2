6a56a6a29ea1a80c802b9908585448e5be76d4b6
linjintao <linjintao@sensetime.com>
2020-07-03 14:42:56 +0800

    Rename shortedge -> 320p

--- configs/recognition/tsn/tsn_r50_320p_1x1x3_100e_kinetics400_rgb.py
+++ configs/recognition/tsn/tsn_r50_320p_1x1x3_100e_kinetics400_rgb.py
@@ -19,11 +19,11 @@ train_cfg = None
 test_cfg = dict(average_clips=None)
 # dataset settings
 dataset_type = 'RawframeDataset'
-data_root = 'data/kinetics400/rawframes_train_shortedge/'
-data_root_val = 'data/kinetics400/rawframes_val_shortedge/'
-ann_file_train = 'data/kinetics400/kinetics_train_list_shortedge.txt'
-ann_file_val = 'data/kinetics400/kinetics_val_list_shortedge.txt'
-ann_file_test = 'data/kinetics400/kinetics_val_list_shortedge.txt'
+data_root = 'data/kinetics400/rawframes_train_320p/'
+data_root_val = 'data/kinetics400/rawframes_val_320p/'
+ann_file_train = 'data/kinetics400/kinetics_train_list_320p.txt'
+ann_file_val = 'data/kinetics400/kinetics_val_list_320p.txt'
+ann_file_test = 'data/kinetics400/kinetics_val_list_320p.txt'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=False)
 mc_cfg = dict(
@@ -31,7 +31,7 @@ mc_cfg = dict(
     client_cfg='/mnt/lustre/share/memcached_client/client.conf',
     sys_path='/mnt/lustre/share/pymc/py3')
 train_pipeline = [
-    dict(type='SampleFrames', clip_len=1, frame_interval=1, num_clips=8),
+    dict(type='SampleFrames', clip_len=1, frame_interval=1, num_clips=3),
     dict(type='FrameSelector', io_backend='memcached', **mc_cfg),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='RandomResizedCrop'),
@@ -47,7 +47,7 @@ val_pipeline = [
         type='SampleFrames',
         clip_len=1,
         frame_interval=1,
-        num_clips=8,
+        num_clips=3,
         test_mode=True),
     dict(type='FrameSelector', io_backend='memcached', **mc_cfg),
     dict(type='Resize', scale=(-1, 256)),
@@ -75,7 +75,7 @@ test_pipeline = [
     dict(type='ToTensor', keys=['imgs'])
 ]
 data = dict(
-    videos_per_gpu=12,
+    videos_per_gpu=32,
     workers_per_gpu=4,
     train=dict(
         type=dataset_type,
@@ -93,14 +93,14 @@ data = dict(
         data_prefix=data_root_val,
         pipeline=test_pipeline))
 # optimizer
-optimizer = dict(type='SGD', lr=0.00375, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
 # learning policy
 lr_config = dict(policy='step', step=[40, 80])
 total_epochs = 100
-checkpoint_config = dict(interval=1)
+checkpoint_config = dict(interval=5)
 evaluation = dict(
-    interval=2, metrics=['top_k_accuracy', 'mean_class_accuracy'], topk=(1, 5))
+    interval=5, metrics=['top_k_accuracy', 'mean_class_accuracy'], topk=(1, 5))
 log_config = dict(
     interval=20,
     hooks=[
@@ -110,7 +110,7 @@ log_config = dict(
 # runtime settings
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/tsn_r50_shortedge_1x1x8_100e_kinetics400_rgb/'
+work_dir = './work_dirs/tsn_r50_320p_1x1x3_100e_kinetics400_rgb/'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
