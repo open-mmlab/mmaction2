4bb087885dfa137c88a56fa9e3972d9abc04dbd9
linjintao <linjintao@sensetime.com>
2020-07-08 02:19:43 +0800

    Fix data preparation links in doc

--- docs/merge_docs.sh
+++ docs/merge_docs.sh
@@ -5,7 +5,18 @@ cat  ../configs/recognition/*/*.md > recognition_models.md
 
 cat  ./tutorials/finetune.md ./tutorials/new_dataset.md ./tutorials/data_pipeline.md ./tutorials/new_modules.md > tutorials.md
 
+sed -i 's/](\/docs\//](/g' ../tools/data/*/*.md
+sed -i 's=](/=](http://gitlab.sz.sensetime.com/open-mmlab/mmaction-lite/tree/master/=g' ../tools/data/*/*.md
 cat  ../tools/data/*/*.md > prepare_data.md
+
+sed -i 's/.md](\/tools\/data\/activitynet\/preparing_activitynet.md/](#activitynet/g' data_preparation.md
+sed -i 's/.md](\/tools\/data\/kinetics400\/preparing_kinetics400.md/](#kinetics-400/g' data_preparation.md
+sed -i 's/.md](\/tools\/data\/mit\/preparing_mit.md/](#moments-in-time/g' data_preparation.md
+sed -i 's/.md](\/tools\/data\/mmit\/preparing_mmit.md/](#multi-moments-in-time/g' data_preparation.md
+sed -i 's/.md](\/tools\/data\/sthv1\/preparing_sthv1.md/](#something-something-v1/g' data_preparation.md
+sed -i 's/.md](\/tools\/data\/sthv2\/preparing_sthv2.md/](#something-something-v2/g' data_preparation.md
+sed -i 's/.md](\/tools\/data\/thumos14\/preparing_thumos14.md/](#thumos-14/g' data_preparation.md
+sed -i 's/.md](\/tools\/data\/ucf101\/preparing_ucf101.md/](#ucf-101/g' data_preparation.md
 
 sed -i 's/#/##&/' localization_models.md
 sed -i 's/#/##&/' recognition_models.md
@@ -23,18 +34,18 @@ sed -i '1i\## Action Recognition Models' recognition_models.md
 sed -i '1i\## Preparing Datasets' prepare_data.md
 cat prepare_data.md >> data_preparation.md
 
+sed -i 's/..\/imgs/_images/g' tutorials.md
+sed -i 's/](new_dataset.md)/](#tutorial-2-adding-new-dataset)/g' tutorials.md
+
 sed -i 's/](\/docs\//](/g' recognition_models.md # remove /docs/ for link used in doc site
 sed -i 's/](\/docs\//](/g' localization_models.md
-sed -i 's/](\/docs\//](/g' data_preparation.md
 sed -i 's=](/=](http://gitlab.sz.sensetime.com/open-mmlab/mmaction-lite/tree/master/=g' recognition_models.md
 sed -i 's=](/=](http://gitlab.sz.sensetime.com/open-mmlab/mmaction-lite/tree/master/=g' localization_models.md
-sed -i 's=](/=](http://gitlab.sz.sensetime.com/open-mmlab/mmaction-lite/tree/master/=g' data_preparation.md
 sed -i 's=](/=](http://gitlab.sz.sensetime.com/open-mmlab/mmaction-lite/tree/master/=g' benchmark.md
 sed -i 's=](/=](http://gitlab.sz.sensetime.com/open-mmlab/mmaction-lite/tree/master/=g' getting_started.md
 sed -i 's=](/=](http://gitlab.sz.sensetime.com/open-mmlab/mmaction-lite/tree/master/=g' install.md
 sed -i 's=](/=](http://gitlab.sz.sensetime.com/open-mmlab/mmaction-lite/tree/master/=g' tutorials.md
-sed -i 's/..\/imgs/_images/g' tutorials.md
-sed -i 's/](new_dataset.md)/](#tutorial-2-adding-new-dataset)/g' tutorials.md
+
 
 cat localization_models.md recognition_models.md > modelzoo.md
 sed -i '1i\# Modelzoo' modelzoo.md
