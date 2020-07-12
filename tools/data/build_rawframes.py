bb6efba248f4cf29806c31393bfaa6b4b1063737
linjintao <linjintao@sensetime.com>
2020-07-12 21:53:42 +0800

    use 'osp.split' to replace '.split('/')'

--- tools/data/build_rawframes.py
+++ tools/data/build_rawframes.py
@@ -23,7 +23,7 @@ def extract_frame(vid_item, dev_id=0):
     """
     full_path, vid_path, vid_id, method, task = vid_item
     if ('/' in vid_path):
-        act_name = osp.splitext(vid_path)[0].split('/')[0]
+        act_name = osp.split(osp.splitext(vid_path)[0])[0]
         out_full_path = osp.join(args.out_dir, act_name)
     else:
         out_full_path = args.out_dir
