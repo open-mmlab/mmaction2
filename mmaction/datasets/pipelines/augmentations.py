dc08671eee92f82cd61ab49b6b4fdd9a29700ea8
linjintao <linjintao@sensetime.com>
2020-07-15 14:02:18 +0800

    add ColorJitter code

--- mmaction/datasets/pipelines/augmentations.py
+++ mmaction/datasets/pipelines/augmentations.py
@@ -697,6 +697,116 @@ class Normalize(object):
 
 
 @PIPELINES.register_module()
+class ColorJitter(object):
+    """
+
+    """
+
+    def __init__(self,
+                 color_space_aug=False,
+                 alphastd=0.1,
+                 eigval=None,
+                 eigvec=None):
+        if eigval is None:
+            # note that the data range should be [0, 255]
+            self.eigval = np.array([55.46, 4.794, 1.148])
+        else:
+            self.eigval = eigval
+
+        if eigvec is None:
+            self.eigvec = np.array([[-0.5675, 0.7192, 0.4009],
+                                    [-0.5808, -0.0045, -0.8140],
+                                    [-0.5836, -0.6948, 0.4203]])
+        else:
+            self.eigvec = eigvec
+
+        self.alphastd = alphastd
+        self.color_space_aug = color_space_aug
+
+    @staticmethod
+    def brightnetss(imgs, delta):
+        if random.uniform(0, 1) > 0.5:
+            delta = np.array(delta).astype(np.float32)
+            imgs = imgs + delta
+        return imgs
+
+    @staticmethod
+    def contrast(imgs, alpha):
+        if random.uniform(0, 1) > 0.5:
+            alpha = np.array(alpha).astype(np.float32)
+            imgs = imgs * alpha
+        return imgs
+
+    @staticmethod
+    def saturation(imgs, alpha):
+        if random.uniform(0, 1) > 0.5:
+            gray = imgs * np.array([0.114, 0.587, 0.299]).astype(np.float32)
+            gray = np.sum(gray, 2, keepdims=True)
+            gray *= (1.0 - alpha)
+            imgs = imgs * alpha
+            imgs = imgs + gray
+        return imgs
+
+    @staticmethod
+    def hue(img, alpha):
+        if random.uniform(0, 1) > 0.5:
+            u = np.cos(alpha * np.pi)
+            w = np.sin(alpha * np.pi)
+            bt = np.array([[1.0, 0.0, 0.0], [0.0, u, -w], [0.0, w, u]])
+            tyiq = np.array([[0.299, 0.587, 0.114], [0.596, -0.274, -0.321],
+                             [0.211, -0.523, 0.311]])
+            ityiq = np.array([[1.0, 0.956, 0.621], [1.0, -0.272, -0.647],
+                              [1.0, -1.107, 1.705]])
+            t = np.dot(np.dot(ityiq, bt), tyiq).T
+            t = np.array(t).astype(np.float32)
+            t = np.flip(t)
+            img = np.dot(img, t)
+        return img
+
+    def __call__(self, results):
+        imgs = results['imgs']
+        out = []
+        if self.color_space_aug:
+            bright_delta = np.random.uniform(-32, 32)
+            contrast_alpha = np.random.uniform(0.6, 1.4)
+            saturation_alpha = np.random.uniform(0.6, 1.4)
+            hue_alpha = random.uniform(-18, 18)
+            for img in imgs:
+                img = self.brightnetss(img, delta=bright_delta)
+                if random.uniform(0, 1) > 0.5:
+                    img = self.contrast(img, alpha=contrast_alpha)
+                    img = self.saturation(img, alpha=saturation_alpha)
+                    img = self.hue(img, alpha=hue_alpha)
+                else:
+                    img = self.saturation(img, alpha=saturation_alpha)
+                    img = self.hue(img, alpha=hue_alpha)
+                    img = self.contrast(img, alpha=contrast_alpha)
+                out.append(img)
+        else:
+            out = imgs
+        alpha = np.random.normal(0, self.alphastd, size=(3, ))
+        rgb = np.array(np.dot(self.eigvec * alpha,
+                              self.eigval)).astype(np.float32)
+        rgb = np.expand_dims(np.expand_dims(rgb, 0), 0)
+
+        results['imgs'] = [img + rgb for img in out]
+        results['eigval'] = self.eigval
+        results['eigvec'] = self.eigvec
+        results['alphastd'] = self.alphastd
+        results['color_space_aug'] = self.color_space_aug
+
+        return results
+
+    def __repr__(self):
+        repr_str = (f'{self.__class__.__name__}('
+                    f'color_space_aug={self.color_space_aug}, '
+                    f'alphastd={self.alphastd}, '
+                    f'eigval={self.eigval}, '
+                    f'eigvec={self.eigvec})')
+        return repr_str
+
+
+@PIPELINES.register_module()
 class CenterCrop(object):
     """Crop the center area from images.
 
