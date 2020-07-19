7f851416863d0b762e6df52623092ec5de728ba2
tony2016uestc <630251328@qq.com>
2020-07-19 20:06:45 +0800

    fix preparaing sthv1.

--- tools/data/sthv1/extract_frames.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/bash env
-
-cd ../
-python build_rawframes.py ../../data/sthv1/videos/ ../../data/sthv1/rawframes/ --task both --level 1 --flow-type tvl1 --ext webm
-echo "Raw frames (RGB and tv-l1) Generated"
-cd sthv1/
--- tools/data/sthv1/extract_rgb_frames_opencv.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/bash env
-
-cd ../
-python build_rawframes.py ../../data/sthv1/videos/ ../../data/sthv1/rawframes/ --task rgb --level 1  --ext webm  --use-opencv
-echo "Genearte raw frames (RGB only)"
-
-cd sthv1/
