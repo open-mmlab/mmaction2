9303361225b1d37c35d2781009964c478f84bb77
HaodongDuan <duanhaodong@sensetime.com>
2020-07-26 13:17:20 +0800

    add UntrimSampleFrames

--- mmaction/datasets/pipelines/__init__.py
+++ mmaction/datasets/pipelines/__init__.py
@@ -7,14 +7,15 @@ from .formating import (Collect, FormatShape, ImageToTensor, ToDataContainer,
 from .loading import (DecordDecode, DecordInit, DenseSampleFrames,
                       FrameSelector, GenerateLocalizationLabels,
                       LoadLocalizationFeature, LoadProposals, OpenCVDecode,
-                      OpenCVInit, PyAVDecode, PyAVInit, SampleFrames)
+                      OpenCVInit, PyAVDecode, PyAVInit, SampleFrames,
+                      UntrimSampleFrames)
 
 __all__ = [
     'SampleFrames', 'PyAVDecode', 'DecordDecode', 'DenseSampleFrames',
-    'OpenCVDecode', 'FrameSelector', 'MultiGroupCrop', 'MultiScaleCrop',
-    'RandomResizedCrop', 'RandomCrop', 'Resize', 'Flip', 'Fuse', 'Normalize',
-    'ThreeCrop', 'CenterCrop', 'TenCrop', 'ImageToTensor', 'Transpose',
-    'Collect', 'FormatShape', 'Compose', 'ToTensor', 'ToDataContainer',
-    'GenerateLocalizationLabels', 'LoadLocalizationFeature', 'LoadProposals',
-    'DecordInit', 'OpenCVInit', 'PyAVInit'
+    'UntrimSampleFrames', 'OpenCVDecode', 'FrameSelector', 'MultiGroupCrop',
+    'MultiScaleCrop', 'RandomResizedCrop', 'RandomCrop', 'Resize', 'Flip',
+    'Fuse', 'Normalize', 'ThreeCrop', 'CenterCrop', 'TenCrop', 'ImageToTensor',
+    'Transpose', 'Collect', 'FormatShape', 'Compose', 'ToTensor',
+    'ToDataContainer', 'GenerateLocalizationLabels', 'LoadLocalizationFeature',
+    'LoadProposals', 'DecordInit', 'OpenCVInit', 'PyAVInit'
 ]
