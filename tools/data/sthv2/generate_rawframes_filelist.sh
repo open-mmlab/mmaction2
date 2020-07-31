4febf402a3cae116d03b00e315b804ebc40496ca
yzfly <yz.liu.me@gmail.com>
2020-07-31 17:14:49 +0800

    correct the sthv1/sthv2 rawframes filelist generate command

--- tools/data/sthv2/generate_rawframes_filelist.sh
+++ tools/data/sthv2/generate_rawframes_filelist.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 cd ../../../
-PYTHONPATH=. python tools/data/build_file_list.py sthv2 data/sthv2/rawframes/ --num-split 1 --level 1 --subset train --format rawframe --shuffle
-PYTHONPATH=. python tools/data/build_file_list.py sthv2 data/sthv2/rawframes/ --num-split 1 --level 1 --subset val --format rawframe --shuffle
+PYTHONPATH=. python tools/data/build_file_list.py sthv2 data/sthv2/rawframes/ --num-split 1 --level 1 --subset train --format rawframes --shuffle
+PYTHONPATH=. python tools/data/build_file_list.py sthv2 data/sthv2/rawframes/ --num-split 1 --level 1 --subset val --format rawframes --shuffle
 echo "Filelist for rawframes generated."
 
 cd tools/data/sthv2/
