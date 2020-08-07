f014003e00fbb35391e2ba2abb90130f2c6dcc37
linjintao <linjintao@sensetime.com>
2020-08-07 21:49:22 +0800

    modify docstring

--- mmaction/datasets/pipelines/loading.py
+++ mmaction/datasets/pipelines/loading.py
@@ -16,7 +16,7 @@ from ..registry import PIPELINES
 class SampleFrames(object):
     """Sample frames from the video.
 
-    Required keys are "filename", added or modified keys are "total_frames",
+    Required keys are "filename", "total_frames", added or modified keys are
     "frame_inds", "frame_interval" and "num_clips".
 
     Args:
