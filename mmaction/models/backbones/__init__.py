23f9cd9a45c58c567ae5dae56b180949aca6c95e
HaodongDuan <duanhaodong@sensetime.com>
2020-09-07 17:15:55 +0800

    add x3d model

--- mmaction/models/backbones/__init__.py
+++ mmaction/models/backbones/__init__.py
@@ -6,8 +6,9 @@ from .resnet3d_slowfast import ResNet3dSlowFast
 from .resnet3d_slowonly import ResNet3dSlowOnly
 from .resnet_tin import ResNetTIN
 from .resnet_tsm import ResNetTSM
+from .x3d import X3d
 
 __all__ = [
     'ResNet', 'ResNet3d', 'ResNetTSM', 'ResNet2Plus1d', 'ResNet3dSlowFast',
-    'ResNet3dSlowOnly', 'ResNet3dCSN', 'ResNetTIN'
+    'ResNet3dSlowOnly', 'ResNet3dCSN', 'ResNetTIN', 'X3d'
 ]
--- mmaction/models/heads/__init__.py
+++ mmaction/models/heads/__init__.py
@@ -4,7 +4,9 @@ from .slowfast_head import SlowFastHead
 from .ssn_head import SSNHead
 from .tsm_head import TSMHead
 from .tsn_head import TSNHead
+from .x3d_head import X3dHead
 
 __all__ = [
-    'TSNHead', 'I3DHead', 'BaseHead', 'TSMHead', 'SlowFastHead', 'SSNHead'
+    'TSNHead', 'I3DHead', 'BaseHead', 'TSMHead', 'SlowFastHead', 'SSNHead',
+    'X3dHead'
 ]
