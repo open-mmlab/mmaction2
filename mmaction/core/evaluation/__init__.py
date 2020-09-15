3c0d908b4ab4d823ea53f2383cad716b17b55c1f
HaodongDuan <duanhaodong@sensetime.com>
2020-09-15 09:24:24 +0800

    update

--- mmaction/core/evaluation/__init__.py
+++ mmaction/core/evaluation/__init__.py
@@ -1,6 +1,6 @@
 from .accuracy import (average_precision_at_temporal_iou,
                        average_recall_at_avg_proposals, confusion_matrix,
-                       get_weighted_score, interpolated_prec_rec,
+                       get_weighted_score, interpolated_precision_recall,
                        mean_average_precision, mean_class_accuracy,
                        pairwise_temporal_iou, softmax, top_k_accuracy)
 from .eval_detection import ANetDetection
@@ -10,6 +10,6 @@ __all__ = [
     'DistEvalHook', 'EvalHook', 'top_k_accuracy', 'mean_class_accuracy',
     'confusion_matrix', 'mean_average_precision', 'get_weighted_score',
     'average_recall_at_avg_proposals', 'pairwise_temporal_iou',
-    'average_precision_at_temporal_iou', 'interpolated_prec_rec',
-    'ANetDetection', 'softmax'
+    'average_precision_at_temporal_iou', 'ANetDetection', 'softmax',
+    'interpolated_precision_recall'
 ]
