760806960c4c44aa5a940c176e47f311fb8ad009
HaodongDuan <duanhaodong@sensetime.com>
2020-09-15 09:19:11 +0800

    remove segment_iou

--- mmaction/core/evaluation/accuracy.py
+++ mmaction/core/evaluation/accuracy.py
@@ -467,33 +467,6 @@ def average_precision_at_temporal_iou(ground_truth,
     return ap
 
 
-def segment_iou(target_segment, candidate_segments):
-    """Compute the temporal intersection over union between a
-    target segment and all the test segments.
-    Parameters
-    ----------
-    target_segment : 1d array
-        Temporal target segment containing [starting, ending] times.
-    candidate_segments : 2d array
-        Temporal candidate segments containing N x [starting, ending] times.
-    Outputs
-    -------
-    tiou : 1d array
-        Temporal intersection over union score of the N's candidate segments.
-    """
-    tt1 = np.maximum(target_segment[0], candidate_segments[:, 0])
-    tt2 = np.minimum(target_segment[1], candidate_segments[:, 1])
-    # Intersection including Non-negative overlap score.
-    segments_intersection = (tt2 - tt1).clip(0)
-    # Segment union.
-    segments_union = (candidate_segments[:, 1] - candidate_segments[:, 0]) \
-        + (target_segment[1] - target_segment[0]) - segments_intersection
-    # Compute overlap as the ratio of the intersection
-    # over union of two segments.
-    t_iou = segments_intersection.astype(float) / segments_union
-    return t_iou
-
-
 def interpolated_prec_rec(prec, rec):
     """Interpolated AP - VOCdevkit from VOC 2011.
     """
