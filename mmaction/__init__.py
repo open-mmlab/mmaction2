5af19f9000e71dd018239a5e895fbda23a4aaca5
dreamerlin <528557675@qq.com>
2020-09-17 19:25:00 +0800

    use digit_version from mmcv

--- mmaction/__init__.py
+++ mmaction/__init__.py
@@ -1,28 +1,15 @@
 import mmcv
+from mmcv import digit_version
 
 from .version import __version__, short_version
-
-
-def digit_version(version_str):
-    digit_version = []
-    for x in version_str.split('.'):
-        if x.isdigit():
-            digit_version.append(int(x))
-        elif x.find('rc') != -1:
-            patch_version = x.split('rc')
-            digit_version.append(int(patch_version[0]) - 1)
-            digit_version.append(int(patch_version[1]))
-    return digit_version
-
 
 mmcv_minimum_version = '1.1.1'
 mmcv_maximum_version = '1.2'
 mmcv_version = digit_version(mmcv.__version__)
 
-
-assert (mmcv_version >= digit_version(mmcv_minimum_version)
-        and mmcv_version <= digit_version(mmcv_maximum_version)), \
-    f'MMCV=={mmcv.__version__} is used but incompatible. ' \
-    f'Please install mmcv>={mmcv_minimum_version}, <={mmcv_maximum_version}.'
+assert digit_version(mmcv_minimum_version) <= mmcv_version \
+        <= digit_version(mmcv_maximum_version), \
+        f'MMCV=={mmcv.__version__} is used but incompatible. ' \
+        f'Please install mmcv>={mmcv_minimum_version}, <={mmcv_maximum_version}.'  # noqa: E501
 
 __all__ = ['__version__', 'short_version']
