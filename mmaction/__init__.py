fb1a5b5d339d0c9cdc258b183e8251ee1cb65790
dreamerlin <528557675@qq.com>
2020-09-17 19:37:44 +0800

    get_short_git_hash

--- mmaction/__init__.py
+++ mmaction/__init__.py
@@ -4,12 +4,10 @@ from mmcv import digit_version
 from .version import __version__, short_version
 
 mmcv_minimum_version = '1.1.1'
-mmcv_maximum_version = '1.2'
 mmcv_version = digit_version(mmcv.__version__)
 
-assert digit_version(mmcv_minimum_version) <= mmcv_version \
-        <= digit_version(mmcv_maximum_version), \
+assert digit_version(mmcv_minimum_version) <= mmcv_version, \
         f'MMCV=={mmcv.__version__} is used but incompatible. ' \
-        f'Please install mmcv>={mmcv_minimum_version}, <={mmcv_maximum_version}.'  # noqa: E501
+        f'Please install mmcv>={mmcv_minimum_version}.'
 
 __all__ = ['__version__', 'short_version']
