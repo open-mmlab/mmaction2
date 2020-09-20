8fd68a9573b778683e9152ec4acd646d2f34ccba
HaodongDuan <duanhaodong@sensetime.com>
2020-09-20 19:43:22 +0800

    init commit

---
+++ tools/data/hvu/download_videos.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# set up environment
+conda env create -f environment.yml
+source activate hvu
+pip install --upgrade youtube-dl
+
+DATA_DIR="../../../data/hvu"
+ANNO_DIR="../../../data/hvu/annotations"
+python download.py ${ANNO_DIR}/hvu_train.csv ${DATA_DIR}/videos_train
+python download.py ${ANNO_DIR}/hvu_val.csv ${DATA_DIR}/videos_val
+
+source deactivate hvu
+conda remove -n hvu --all
