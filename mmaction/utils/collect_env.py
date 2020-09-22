f24acdd5fbfd21a66e9f651d0716acb33a7b90e1
Ycr <34575412+yaochaorui@users.noreply.github.com>
2020-09-22 16:23:43 +0800

    Revert "Reused mmcv's utility functions. (#195)"
    This reverts commit 5444189865e99378d2072c376f27b0abb18abe53.

--- mmaction/utils/collect_env.py
+++ mmaction/utils/collect_env.py
@@ -3,11 +3,17 @@ from mmcv.utils import get_git_hash
 
 import mmaction
 
+NUM_HASHES = 7
+
+
+def get_short_git_hash(num_hashes=7):
+    return get_git_hash()[:num_hashes]
+
 
 def collect_env():
     env_info = collect_basic_env()
     env_info['MMAction2'] = (
-        mmaction.__version__ + '+' + get_git_hash(digits=7))
+        mmaction.__version__ + '+' + get_short_git_hash(NUM_HASHES))
     return env_info
 
 
