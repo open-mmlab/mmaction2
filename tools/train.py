a093181ce04f9ae376d98bdd352dbb46e6786b48
dreamerlin <528557675@qq.com>
2020-09-26 15:23:58 +0800

    add customized imports

--- tools/train.py
+++ tools/train.py
@@ -7,7 +7,7 @@ import warnings
 
 import mmcv
 import torch
-from mmcv import Config
+from mmcv import Config, DictAction
 from mmcv.runner import init_dist, set_random_seed
 from mmcv.utils import get_git_hash
 
@@ -46,6 +46,12 @@ def parse_args():
         action='store_true',
         help='whether to set deterministic options for CUDNN backend.')
     parser.add_argument(
+        '--cfg-options',
+        nargs='+',
+        action=DictAction,
+        help='override some settings in the used config, the key-value pair '
+        'in xxx=yyy format will be merged into config file.')
+    parser.add_argument(
         '--launcher',
         choices=['none', 'pytorch', 'slurm', 'mpi'],
         default='none',
@@ -62,6 +68,8 @@ def main():
     args = parse_args()
 
     cfg = Config.fromfile(args.config)
+    if args.cfg_options is not None:
+        cfg.merge_from_dict(args.cfg_options)
     # set cudnn_benchmark
     if cfg.get('cudnn_benchmark', False):
         torch.backends.cudnn.benchmark = True
