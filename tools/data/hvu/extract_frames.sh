ce5acc7338625fb94680fa5f3641393b9be8b2df
HaodongDuan <duanhaodong@sensetime.com>
2020-09-28 14:43:26 +0800

    add file list generation

--- tools/data/hvu/extract_frames.sh
+++ tools/data/hvu/extract_frames.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 cd ../
-python build_rawframes.py ../../data/kinetics400/videos_train/ ../../data/kinetics400/rawframes_train/ --level 2 --flow-type tvl1 --ext mp4 --task both  --new-width 340 --new-height 256
+python build_rawframes.py ../../data/hvu/videos_train/ ../../data/hvu/rawframes_train/ --level 1 --flow-type tvl1 --ext mp4 --task both  --new-short 256
 echo "Raw frames (RGB and tv-l1) Generated for train set"
 
-python build_rawframes.py ../../data/kinetics400/videos_val/ ../../data/kinetics400/rawframes_val/ --level 2 --flow-type tvl1 --ext mp4 --task both  --new-width 340 --new-height 256
+python build_rawframes.py ../../data/hvu/videos_val/ ../../data/hvu/rawframes_val/ --level 1 --flow-type tvl1 --ext mp4 --task both  --new-short 256
 echo "Raw frames (RGB and tv-l1) Generated for val set"
 
-cd kinetics400/
+cd hvu/
