1ad286f1e28928ff5495f6c7061c2ce772253f0b
HaodongDuan <duanhaodong@sensetime.com>
2020-09-28 10:18:15 +0800

    parse hvu tags

---
+++ tools/data/hvu/parse_tag_list.py
@@ -0,0 +1,15 @@
+import mmcv
+
+tag_list = '../../../data/hvu/annotations/hvu_categories.csv'
+
+lines = open(tag_list).readlines()
+lines = [x.strip().split(',') for x in lines[1:]]
+tag_categories = {}
+for line in lines:
+    tag, category = line
+    tag_categories.setdefault(category, []).append(tag)
+
+for k in tag_categories:
+    tag_categories[k].sort()
+
+mmcv.dump(tag_categories, 'hvu_tags.json')
