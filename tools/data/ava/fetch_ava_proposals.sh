244562dd433121346c195cd4136cbf9dad4c3c61
dreamerlin <528557675@qq.com>
2020-10-05 23:20:40 +0800

    update link

--- tools/data/ava/fetch_ava_proposals.sh
+++ tools/data/ava/fetch_ava_proposals.sh
@@ -4,6 +4,6 @@ set -e
 
 DATA_DIR="../../../data/ava/"
 
-wget https://open-mmlab.s3.ap-northeast-2.amazonaws.com/mmaction/filelist/ava_dense_proposals_train.FAIR.recall_93.9.pkl -P ${DATA_DIR}
-wget https://open-mmlab.s3.ap-northeast-2.amazonaws.com/mmaction/filelist/ava_dense_proposals_val.FAIR.recall_93.9.pkl -P ${DATA_DIR}
-wget https://open-mmlab.s3.ap-northeast-2.amazonaws.com/mmaction/filelist/ava_dense_proposals_test.FAIR.recall_93.9.pkl -P ${DATA_DIR}
+wget https://download.openmmlab.com/mmaction/dataset/ava/ava_dense_proposals_train.FAIR.recall_93.9.pkl -P ${DATA_DIR}
+wget https://download.openmmlab.com/mmaction/dataset/ava/ava_dense_proposals_val.FAIR.recall_93.9.pkl -P ${DATA_DIR}
+wget https://download.openmmlab.com/mmaction/dataset/ava/ava_dense_proposals_test.FAIR.recall_93.9.pkl -P ${DATA_DIR}
