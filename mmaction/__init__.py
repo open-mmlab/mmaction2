7c5df4c8cefc1a56971d66b02f0e992ac389367f
dreamerlin <528557675@qq.com>
2020-10-07 21:07:19 +0800

    use  and remove

--- mmaction/__init__.py
+++ mmaction/__init__.py
@@ -1,20 +1,15 @@
 import mmcv
-from mmcv import digit_version, parse_version_info
+from mmcv import digit_version
 
-from .version import __version__, short_version
-
-version_info = parse_version_info(__version__)
+from .version import __version__
 
 mmcv_minimum_version = '1.1.1'
 mmcv_maximum_version = '1.2'
 mmcv_version = digit_version(mmcv.__version__)
 
-assert digit_version(mmcv_minimum_version) <= mmcv_version, \
-        f'MMCV=={mmcv.__version__} is used but incompatible. ' \
-        f'Please install mmcv>={mmcv_minimum_version}.'
+assert (digit_version(mmcv_minimum_version) <= mmcv_version
+        <= digit_version(mmcv_maximum_version)), \
+    f'MMCV=={mmcv.__version__} is used but incompatible. ' \
+    f'Please install mmcv>={mmcv_minimum_version}, <={mmcv_maximum_version}.'
 
-assert digit_version(mmcv_maximum_version) > mmcv_version, \
-        f'MMCV=={mmcv.__version__} is used but incompatible. ' \
-        f'Please install mmcv<{mmcv_maximum_version}.'
-
-__all__ = ['__version__', 'short_version', 'version_info']
+__all__ = ['__version__']
