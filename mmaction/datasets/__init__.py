086b8ad1265d431789c9ba42487fa54ffbd6efae
HaodongDuan <duanhaodong@sensetime.com>
2020-10-07 13:19:49 +0800

    update __init__.py

--- mmaction/datasets/__init__.py
+++ mmaction/datasets/__init__.py
@@ -2,11 +2,14 @@ from .activitynet_dataset import ActivityNetDataset
 from .base import BaseDataset
 from .builder import build_dataloader, build_dataset
 from .dataset_wrappers import RepeatDataset
+from .image_dataset import ImageDataset
 from .rawframe_dataset import RawframeDataset
+from .rawvideo_dataset import RawVideoDataset
 from .ssn_dataset import SSNDataset
 from .video_dataset import VideoDataset
 
 __all__ = [
     'VideoDataset', 'build_dataloader', 'build_dataset', 'RepeatDataset',
-    'RawframeDataset', 'BaseDataset', 'ActivityNetDataset', 'SSNDataset'
+    'RawframeDataset', 'BaseDataset', 'ActivityNetDataset', 'SSNDataset',
+    'ImageDataset', 'RawVideoDataset'
 ]
