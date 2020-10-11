d017d2f8655e64e2768895e1d0ed0791414302cb
dreamerlin <528557675@qq.com>
2020-10-11 22:35:58 +0800

    add ava dataset

---
+++ mmaction/datasets/ava_dataset.py
@@ -0,0 +1,150 @@
+import copy
+from collections import defaultdict
+
+import mmcv
+import numpy as np
+
+from ..utils import get_root_logger
+from .base import BaseDataset
+from .registry import DATASETS
+
+
+@DATASETS.register_module()
+class AVADataset(BaseDataset):
+
+    _FPS = 30
+    _NUM_CLASSES = 81
+
+    def __init__(self,
+                 ann_file,
+                 exclude_file,
+                 label_file,
+                 pipeline,
+                 proposal_file=None,
+                 data_prefix=None,
+                 test_mode=False,
+                 modality='RGB',
+                 timestamp_start=900,
+                 timestamp_end=1800):
+        self.exclude_file = exclude_file
+        self.label_file = label_file
+        self.proposal_file = proposal_file
+        self.timestamp_start = timestamp_start
+        self.timestamp_end = timestamp_end
+        self.logger = get_root_logger()
+        super().__init__(
+            ann_file, pipeline, data_prefix, test_mode, modality=modality)
+        if self.proposal_file is not None:
+            self.proposals = mmcv.load(self.proposal_file)
+        else:
+            self.proposals = None
+
+        if not test_mode:
+            valid_indexes = self.filter_exclude_file()
+            self.logger.info(
+                f'{len(valid_indexes)} out of {len(self.video_infos)} '
+                f'frames are valid.')
+            self.video_infos = self.video_infos = [
+                self.video_infos[i] for i in valid_indexes
+            ]
+
+    def parse_img_record(self, img_records):
+        bboxes, labels, entity_ids = [], [], []
+        while len(img_records) > 0:
+            img_record = img_records[0]
+            num_img_records = len(img_records)
+            selected_records = list(
+                filter(
+                    lambda x: np.array_equal(x['entity_box'], img_record[
+                        'entity_box']), img_records))
+            num_selected_records = len(selected_records)
+            img_records = list(set(img_records) - set(selected_records))
+            assert len(img_records) + num_selected_records == num_img_records
+
+            bboxes.append(img_record['entity_box'])
+            valid_labels = np.array([selected_record['label']]
+                                    for selected_record in selected_records)
+            padded_labels = np.pad(
+                valid_labels, (0, self._NUM_CLASSES - valid_labels.shape[0]),
+                'constant',
+                constant_values=-1)
+            labels.append(padded_labels)
+            entity_ids.append(img_record['entity_id'])
+        bboxes = np.stack(bboxes)
+        labels = np.stack(labels)
+        entity_ids = np.stack(entity_ids)
+        return bboxes, labels, entity_ids
+
+    def filter_exclude_file(self):
+        valid_indexes = []
+        if self.exclude_file is None:
+            valid_indexes = list(np.range(self.video_infos))
+        else:
+            exclude_video_infos = [
+                x.strip().split(',') for x in open(self.exclude_file)
+            ]
+            for i, video_info in enumerate(self.video_infos):
+                valid_indexes.append(i)
+                for video_id, timestamp in exclude_video_infos:
+                    if (video_info['video_id'] == video_id
+                            and video_info['timestamp'] == int(timestamp)):
+                        valid_indexes.pop()
+                        break
+        return valid_indexes
+
+    def load_annotations(self):
+        video_infos = []
+        records_dict_by_img = defaultdict(list)
+        with open(self.ann_file, 'r') as fin:
+            for line in fin:
+                line_split = line.strip().split(',')
+
+                video_id = line_split[0]
+                timestamp = int(line_split[1])
+                img_key = f'{video_id},{timestamp:04d}'
+
+                entity_box = np.array(list(map(float, line_split[2:6])))
+                label = int(line_split[6])
+                entity_id = int(line_split[7])
+                shot_info = (0, (self.timestamp_end - self.timestamp_start) *
+                             self._FPS)
+
+                video_info = dict(
+                    video_id=video_id,
+                    timestamp=timestamp,
+                    entity_box=entity_box,
+                    label=label,
+                    entity_id=entity_id,
+                    shot_info=shot_info)
+                records_dict_by_img[img_key].append(video_info)
+
+        for img_key in records_dict_by_img:
+            video_id, timestamp = img_key.split(',')
+            bboxes, labels, entity_ids = self.parse_img_record(
+                records_dict_by_img[img_key])
+            ann = dict(bboxes=bboxes, labels=labels, entity_ids=entity_ids)
+            video_info = dict(
+                video_id=video_id,
+                timestamp=timestamp,
+                shot_info=shot_info,
+                fps=self._FPS,
+                ann=ann)
+            video_infos.append(video_info)
+
+        return video_infos
+
+    def prepare_train_frames(self, idx):
+        """Prepare the frames for training given the index."""
+        results = copy.deepcopy(self.video_infos[idx])
+        results['filename_tmpl'] = self.filename_tmpl
+        results['modality'] = self.modality
+        results['start_index'] = self.start_index
+        return self.pipeline(results)
+
+    def prepare_test_frames(self, idx):
+        """Prepare the frames for testing given the index."""
+        results = copy.deepcopy(self.video_infos[idx])
+        results['filename_tmpl'] = self.filename_tmpl
+        results['modality'] = self.modality
+        results['start_index'] = self.start_index
+        return self.pipeline(results)
