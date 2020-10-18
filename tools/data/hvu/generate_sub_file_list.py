30096b7888b7b3a6ae2efde8680e42d464cc3644
Haodong Duan <34324155+kennymckormick@users.noreply.github.com>
2020-10-18 17:54:49 +0800

    extend HVU data tools (#258)
    * extend HVU data tools
    
    * Update preparing_hvu.md
    
    fix typo
    
    * resolve comments
    
    * resolve comments
    
    Co-authored-by: Jintao Lin <528557675@qq.com>

---
+++ tools/data/hvu/generate_sub_file_list.py
@@ -0,0 +1,49 @@
+import argparse
+import os.path as osp
+
+import mmcv
+
+
+def main(annotation_file, category):
+    assert category in [
+        'action', 'attribute', 'concept', 'event', 'object', 'scene'
+    ]
+
+    data = mmcv.load(annotation_file)
+    basename = osp.basename(annotation_file)
+    dirname = osp.dirname(annotation_file)
+    basename = basename.replace('hvu', f'hvu_{category}')
+
+    target_file = osp.join(dirname, basename)
+
+    def parse_item(item, category):
+        label = item['label']
+        if category in label:
+            item['label'] = label[category]
+            return item
+        else:
+            return None
+
+    result = []
+    for item in data:
+        label = item['label']
+        if category in label:
+            item['label'] = label[category]
+            result.append(item)
+
+    mmcv.dump(data, target_file)
+
+
+if __name__ == '__main__':
+    description = 'Helper script for generating HVU per-category file list.'
+    p = argparse.ArgumentParser(description=description)
+    p.add_argument(
+        'annotation_file',
+        type=str,
+        help=('The annotation file which contains tags of all categories.'))
+    p.add_argument(
+        'category',
+        type=str,
+        choices=['action', 'attribute', 'concept', 'event', 'object', 'scene'],
+        help='The tag category that you want to generate file list for.')
+    main(**vars(p.parse_args()))
