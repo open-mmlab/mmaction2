3a89dd3f0747a4c006346657446dad96dc4f00ea
Jintao Lin <528557675@qq.com>
2020-10-21 18:24:23 +0800

    [Feature] Add init_eval to evaluation hook (#216)

--- mmaction/core/evaluation/__init__.py
+++ mmaction/core/evaluation/__init__.py
@@ -5,12 +5,13 @@ from .accuracy import (average_precision_at_temporal_iou,
                        mmit_mean_average_precision, pairwise_temporal_iou,
                        softmax, top_k_accuracy)
 from .eval_detection import ActivityNetDetection
-from .eval_hooks import DistEvalHook, EvalHook
+from .eval_hooks import DistEpochEvalHook, EpochEvalHook
 
 __all__ = [
-    'DistEvalHook', 'EvalHook', 'top_k_accuracy', 'mean_class_accuracy',
-    'confusion_matrix', 'mean_average_precision', 'get_weighted_score',
-    'average_recall_at_avg_proposals', 'pairwise_temporal_iou',
-    'average_precision_at_temporal_iou', 'ActivityNetDetection', 'softmax',
-    'interpolated_precision_recall', 'mmit_mean_average_precision'
+    'DistEpochEvalHook', 'EpochEvalHook', 'top_k_accuracy',
+    'mean_class_accuracy', 'confusion_matrix', 'mean_average_precision',
+    'get_weighted_score', 'average_recall_at_avg_proposals',
+    'pairwise_temporal_iou', 'average_precision_at_temporal_iou',
+    'ActivityNetDetection', 'softmax', 'interpolated_precision_recall',
+    'mmit_mean_average_precision'
 ]
