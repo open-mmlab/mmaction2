eb9b75250933b29e33bb2a950ab9a9b366ef71b6
dreamerlin <528557675@qq.com>
2020-10-21 12:30:13 +0800

    remove import modules

--- tools/analysis/eval_metric.py
+++ tools/analysis/eval_metric.py
@@ -40,11 +40,6 @@ def main():
 
     cfg = Config.fromfile(args.config)
 
-    # import modules from string list.
-    if cfg.get('custom_imports', None):
-        from mmcv.utils import import_modules_from_strings
-        import_modules_from_strings(**cfg['custom_imports'])
-
     assert args.eval is not None
 
     if args.cfg_options is not None:
