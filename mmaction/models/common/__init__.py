ba5f9e918efd5ba7e11d7faff7b3283892f1ccc6
bkhuang <congee524@gmail.com>
2020-11-02 19:21:06 +0800

    draft of TANet

--- mmaction/models/common/__init__.py
+++ mmaction/models/common/__init__.py
@@ -1,4 +1,5 @@
 from .conv2plus1d import Conv2plus1d
 from .conv_audio import ConvAudio
+from .tam import TAM
 
-__all__ = ['Conv2plus1d', 'ConvAudio']
+__all__ = ['Conv2plus1d', 'ConvAudio', 'TAM']
