4eed79ead527b0ad3d5a011a1d215562b06515c6
Haodong Duan <34324155+kennymckormick@users.noreply.github.com>
2020-11-08 15:12:52 +0800

    Omni validate (#321)
    * resolve comments
    
    * update changelog
    
    * minor fix for OmniSource

--- configs/recognition/omnisource/tsn_r50_1x1x8_100e_minikinetics/tsn_r50_1x1x8_100e_minikinetics_rgb.py
+++ configs/recognition/omnisource/tsn_r50_1x1x8_100e_minikinetics/tsn_r50_1x1x8_100e_minikinetics_rgb.py
@@ -20,7 +20,6 @@ test_cfg = dict(average_clips=None)
 # dataset settings
 dataset_type = 'VideoDataset'
 # The flag indicates using joint training
-omnisource = True
 
 data_root = 'data/OmniSource/kinetics_200_train'
 data_root_val = 'data/OmniSource/kinetics_200_val'
