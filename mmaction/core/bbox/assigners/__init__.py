7a547d4f9a3a4c5871139c4bcb94ed5d33066002
HaodongDuan <duanhaodong@sensetime.com>
2020-11-17 19:42:04 +0800

    add assigner

---
+++ mmaction/core/bbox/assigners/__init__.py
@@ -0,0 +1,5 @@
+from .assign_result import AssignResult
+from .base_assigner import BaseAssigner
+from .max_iou_assigner import MaxIoUAssigner
+
+__all__ = ['BaseAssigner', 'MaxIoUAssigner', 'AssignResult']
