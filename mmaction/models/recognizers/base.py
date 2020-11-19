5319dfcefaf21c936600adc024c5855e19ddf499
irvingzhang0512 <irvingzhang0512@gmail.com>
2020-11-19 10:04:37 +0800

    [Docs] Update docs about test crops. 1. Add more docs. 2. Update default configs in TSM model when using DenseSampleFrames.

--- mmaction/models/recognizers/base.py
+++ mmaction/models/recognizers/base.py
@@ -77,10 +77,14 @@ class BaseRecognizer(nn.Module, metaclass=ABCMeta):
 
         Using different averaging types ('score' or 'prob' or None,
         which defined in test_cfg) to computed the final averaged
-        class score.
+        class score. Only called in test mode.
 
         Args:
             cls_score (torch.Tensor): Class score to be averaged.
+            num_segs (int): Number of clips for each input sample. 
+                For Recognizer3D, `num_segs` is set automatically.
+                For Recognizer2D, `num_segs` HAS TO BE SET MANUALLY by 
+                `test_crops` and `twice_sample` in cfg.test_cfg.
 
         Returns:
             torch.Tensor: Averaged class score.
