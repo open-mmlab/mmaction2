e19b0c75646c56afcd92300b69674cd30959ecd7
HaodongDuan <duanhaodong@sensetime.com>
2020-11-20 21:56:31 +0800

    fix bug

--- mmaction/core/bbox/__init__.py
+++ mmaction/core/bbox/__init__.py
@@ -1,8 +1,11 @@
 from .assign_sampling import build_assigner, build_sampler
 from .assigners import AssignResult, BaseAssigner, MaxIoUAssigner
+from .bbox_target import bbox_target
 from .samplers import BaseSampler, RandomSampler, SamplingResult
+from .transforms import bbox2result, bbox2roi
 
 __all__ = [
     'BaseAssigner', 'MaxIoUAssigner', 'AssignResult', 'BaseSampler',
-    'RandomSampler', 'SamplingResult', 'build_assigner', 'build_sampler'
+    'RandomSampler', 'SamplingResult', 'build_assigner', 'build_sampler',
+    'bbox_target', 'bbox2roi', 'bbox2result'
 ]
