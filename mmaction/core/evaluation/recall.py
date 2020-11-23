a57a8c884b5aa2f6dfe0ff4e93480f1b1e2b143f
HaodongDuan <duanhaodong@sensetime.com>
2020-11-23 15:03:34 +0800

    use our own bbox_overlaps

--- mmaction/core/evaluation/recall.py
+++ mmaction/core/evaluation/recall.py
@@ -1,53 +1,7 @@
 import numpy as np
+import torch
 
-
-# Have to add this piece of code, since during evaluation, bboxes are of type
-# 'ndarray'. However, bbox_overlaps in mmcv only support cuda tensors.
-def bbox_overlaps(bboxes1, bboxes2, mode='iou'):
-    """Calculate the ious between each bbox of bboxes1 and bboxes2.
-
-    Args:
-        bboxes1(ndarray): shape (n, 4)
-        bboxes2(ndarray): shape (k, 4)
-        mode(str): iou (intersection over union) or iof (intersection
-            over foreground)
-    Returns:
-        ious(ndarray): shape (n, k)
-    """
-
-    assert mode in ['iou', 'iof']
-
-    bboxes1 = bboxes1.astype(np.float32)
-    bboxes2 = bboxes2.astype(np.float32)
-    rows = bboxes1.shape[0]
-    cols = bboxes2.shape[0]
-    ious = np.zeros((rows, cols), dtype=np.float32)
-    if rows * cols == 0:
-        return ious
-    exchange = False
-    if bboxes1.shape[0] > bboxes2.shape[0]:
-        bboxes1, bboxes2 = bboxes2, bboxes1
-        ious = np.zeros((cols, rows), dtype=np.float32)
-        exchange = True
-    area1 = (bboxes1[:, 2] - bboxes1[:, 0] + 1) * (
-        bboxes1[:, 3] - bboxes1[:, 1] + 1)
-    area2 = (bboxes2[:, 2] - bboxes2[:, 0] + 1) * (
-        bboxes2[:, 3] - bboxes2[:, 1] + 1)
-    for i in range(bboxes1.shape[0]):
-        x_start = np.maximum(bboxes1[i, 0], bboxes2[:, 0])
-        y_start = np.maximum(bboxes1[i, 1], bboxes2[:, 1])
-        x_end = np.minimum(bboxes1[i, 2], bboxes2[:, 2])
-        y_end = np.minimum(bboxes1[i, 3], bboxes2[:, 3])
-        overlap = np.maximum(x_end - x_start + 1, 0) * np.maximum(
-            y_end - y_start + 1, 0)
-        if mode == 'iou':
-            union = area1[i] + area2 - overlap
-        else:
-            union = area1[i] if not exchange else area2
-        ious[i, :] = overlap / union
-    if exchange:
-        ious = ious.T
-    return ious
+from mmaction.core.bbox.transforms import bbox_overlaps
 
 
 def _recalls(all_ious, proposal_nums, thrs):
@@ -134,7 +88,10 @@ def eval_recalls(gts, proposals, proposal_nums=None, iou_thrs=None):
         if gts[i] is None or gts[i].shape[0] == 0:
             ious = np.zeros((0, img_proposal.shape[0]), dtype=np.float32)
         else:
-            ious = bbox_overlaps(gts[i], img_proposal[:prop_num, :4])
+            ious = bbox_overlaps(
+                torch.tensor(gts[i]),
+                torch.tensor(img_proposal[:prop_num, :4]))
+            ious = ious.data.numpy()
         all_ious.append(ious)
     all_ious = np.array(all_ious)
     recalls = _recalls(all_ious, proposal_nums, iou_thrs)
