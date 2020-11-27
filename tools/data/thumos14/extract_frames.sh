47ec6217c41acff157493d74f5aad3c3a01c8574
dreamerlin <528557675@qq.com>
2020-11-27 21:46:53 +0800

    modify valiation

--- tools/data/thumos14/extract_frames.sh
+++ tools/data/thumos14/extract_frames.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd ../
-python build_rawframes.py ../../data/thumos14/videos/validation/ ../../data/thumos14/rawframes/validation/ --level 1 --flow-type tvl1 --ext mp4 --task both
+python build_rawframes.py ../../data/thumos14/videos/val/ ../../data/thumos14/rawframes/val/ --level 1 --flow-type tvl1 --ext mp4 --task both
 echo "Raw frames (RGB and tv-l1) Generated for val set"
 
 python build_rawframes.py ../../data/thumos14/videos/test/ ../../data/thumos14/rawframes/test/ --level 1 --flow-type tvl1 --ext mp4 --task both
