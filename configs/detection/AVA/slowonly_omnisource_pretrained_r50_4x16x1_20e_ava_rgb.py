dc5a2bd542cf67ccc3c7b63e1404291e8f4ecd26
HaodongDuan <duanhaodong@sensetime.com>
2020-11-27 15:34:37 +0800

    remove arg: val_videos_per_gpu

--- configs/detection/AVA/slowonly_omnisource_pretrained_r50_4x16x1_20e_ava_rgb.py
+++ configs/detection/AVA/slowonly_omnisource_pretrained_r50_4x16x1_20e_ava_rgb.py
@@ -97,7 +97,7 @@ data = dict(
     videos_per_gpu=16,
     workers_per_gpu=4,
     # During testing, each video may have different shape
-    val_videos_per_gpu=1,
+    val_dataloader=dict(videos_per_gpu=1),
     train=dict(
         type=dataset_type,
         ann_file=ann_file_train,
