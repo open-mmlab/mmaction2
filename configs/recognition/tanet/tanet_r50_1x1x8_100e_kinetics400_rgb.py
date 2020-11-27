576d86668948295e136ed8e5864443fbea4d983f
congee524 <congee524@gmail.com>
2020-11-27 16:15:48 +0800

    fix tam bug

--- configs/recognition/tanet/tanet_r50_1x1x8_100e_kinetics400_rgb.py
+++ configs/recognition/tanet/tanet_r50_1x1x8_100e_kinetics400_rgb.py
@@ -14,7 +14,7 @@ model = dict(
         spatial_type='avg',
         consensus=dict(type='AvgConsensus', dim=1),
         dropout_ratio=0.5,
-        init_std=0.01))
+        init_std=0.001))
 train_cfg = None
 test_cfg = dict(average_clips=None)
 # dataset settings
@@ -27,7 +27,7 @@ ann_file_test = 'data/kinetics400/kinetics400_val_list_rawframes.txt'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=False)
 train_pipeline = [
-    dict(type='SampleFrames', clip_len=1, frame_interval=1, num_clips=8),
+    dict(type='DenseSampleFrames', clip_len=1, frame_interval=1, num_clips=8),
     dict(type='RawFrameDecode'),
     dict(type='Resize', scale=(-1, 256)),
     dict(
@@ -35,7 +35,8 @@ train_pipeline = [
         input_size=224,
         scales=(1, 0.875, 0.75, 0.66),
         random_crop=False,
-        max_wh_scale_gap=1),
+        max_wh_scale_gap=1,
+        num_fixed_crops=13),
     dict(type='Resize', scale=(224, 224), keep_ratio=False),
     dict(type='Flip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
@@ -45,7 +46,7 @@ train_pipeline = [
 ]
 val_pipeline = [
     dict(
-        type='SampleFrames',
+        type='DenseSampleFrames',
         clip_len=1,
         frame_interval=1,
         num_clips=8,
@@ -61,14 +62,14 @@ val_pipeline = [
 ]
 test_pipeline = [
     dict(
-        type='SampleFrames',
+        type='DenseSampleFrames',
         clip_len=1,
         frame_interval=1,
         num_clips=8,
         test_mode=True),
     dict(type='RawFrameDecode'),
     dict(type='Resize', scale=(-1, 256)),
-    dict(type='TenCrop', crop_size=224),
+    dict(type='CenterCrop', crop_size=224),
     dict(type='Flip', flip_ratio=0),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='FormatShape', input_format='NCHW'),
@@ -78,6 +79,8 @@ test_pipeline = [
 data = dict(
     videos_per_gpu=8,
     workers_per_gpu=4,
+    val_dataloader=dict(videos_per_gpu=4),
+    test_dataloader=dict(videos_per_gpu=4),
     train=dict(
         type=dataset_type,
         ann_file=ann_file_train,
@@ -95,15 +98,19 @@ data = dict(
         pipeline=test_pipeline))
 # optimizer
 optimizer = dict(
-    type='SGD', lr=0.01, momentum=0.9,
-    weight_decay=0.0001)  # this lr is used for 8 gpus
-optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
+    type='SGD',
+    constructor='TSMOptimizerConstructor',
+    paramwise_cfg=dict(fc_lr5=True),
+    lr=0.01,  # this lr is used for 8 gpus
+    momentum=0.9,
+    weight_decay=0.0001)
+optimizer_config = dict(grad_clip=dict(max_norm=20, norm_type=2))
 # learning policy
-lr_config = dict(policy='step', step=[40, 80])
+lr_config = dict(policy='step', step=[50, 75, 90])
 total_epochs = 100
 checkpoint_config = dict(interval=5)
 evaluation = dict(
-    interval=5, metrics=['top_k_accuracy', 'mean_class_accuracy'], topk=(1, 5))
+    interval=2, metrics=['top_k_accuracy', 'mean_class_accuracy'], topk=(1, 5))
 log_config = dict(
     interval=20,
     hooks=[
