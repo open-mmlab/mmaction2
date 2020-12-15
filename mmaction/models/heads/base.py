7f6e9ea662574877ebce5239f43d698eeefb6cd6
xusu <xusu@sensetime.com>
2020-12-15 16:52:44 +0800

    Minor fix unittest.
    Fix docstring.

--- mmaction/models/heads/base.py
+++ mmaction/models/heads/base.py
@@ -36,7 +36,7 @@ class BaseHead(nn.Module, metaclass=ABCMeta):
         num_classes (int): Number of classes to be classified.
         in_channels (int): Number of channels in input feature.
         loss_cls (dict): Config for building loss.
-            Default: dict(type='CrossEntropyLoss').
+            Default: dict(type='CrossEntropyLoss', loss_weight=1.0).
         multi_class (bool): Determines whether it is a multi-class
             recognition task. Default: False.
         label_smooth_eps (float): Epsilon used in label smooth.
