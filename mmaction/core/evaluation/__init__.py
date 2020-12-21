7347b9018fff9c775214eee447055d3824d834ef
dreamerlin <528557675@qq.com>
2020-12-21 11:54:43 +0800

    resolve conflict

--- mmaction/core/evaluation/__init__.py
+++ mmaction/core/evaluation/__init__.py
@@ -1,9 +1,5 @@
-from .accuracy import (MeanClsAcc, TopKAcc, average_precision_at_temporal_iou,
-                       average_recall_at_avg_proposals, confusion_matrix,
-                       get_weighted_score, interpolated_precision_recall,
-                       mean_average_precision, mean_class_accuracy,
-                       mmit_mean_average_precision, pairwise_temporal_iou,
-                       softmax, top_k_accuracy)
+from .accuracy import (MeanClsAcc, TopKAcc, confusion_matrix,
+                       get_weighted_score, mean_class_accuracy, top_k_accuracy)
 from .base import BaseMetrics
 from .eval_detection import ActivityNetLocalization
 from .eval_hooks import DistEpochEvalHook, EpochEvalHook
@@ -23,5 +19,5 @@ __all__ = [
     'ActivityNetLocalization', 'interpolated_precision_recall',
     'mmit_mean_average_precision', 'TopKAcc', 'MeanClsAcc', 'BaseMetrics',
     'average_precision_at_temporal_iou', 'eval_ap', 'TemporalMeanAP', 'MeanAP',
-    'binary_precision_recall_curve', 'ARAN', 'temporal_iop'
+    'binary_precision_recall_curve', 'ARAN', 'temporal_iop', 'temporal_iou'
 ]
--- mmaction/core/evaluation/recall.py
+++ mmaction/core/evaluation/recall.py
@@ -1,6 +1,7 @@
+import warnings
+
 import numpy as np
 import torch
-import warnings
 
 from ..registry import METRICS
 from .base import BaseMetrics
