355cbe2a1b9889391759a10e841c832947bca301
xusu <xusu@sensetime.com>
2020-12-29 17:24:12 +0800

    Fix unittest.

--- tests/test_runtime/test_precise_bn.py
+++ tests/test_runtime/test_precise_bn.py
@@ -3,6 +3,7 @@ import copy
 import pytest
 import torch
 import torch.nn as nn
+from mmcv.parallel import MMDistributedDataParallel
 from mmcv.runner import EpochBasedRunner, build_optimizer
 from mmcv.utils import get_logger
 from torch.utils.data import DataLoader, Dataset
@@ -16,7 +17,7 @@ class ExampleDataset(Dataset):
         self.index = 0
 
     def __getitem__(self, idx):
-        results = dict(imgs=torch.tensor([1]))
+        results = dict(imgs=torch.tensor([1.0], dtype=torch.float32))
         return results
 
     def __len__(self):
@@ -35,11 +36,32 @@ class ExampleModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv = nn.Linear(1, 1)
-        self.bn = nn.BatchNorm2d(1)
+        self.bn = nn.BatchNorm1d(1)
         self.test_cfg = None
 
     def forward(self, imgs, return_loss=False):
         return self.bn(self.conv(imgs))
+
+    def train_step(self, data_batch, optimizer, **kwargs):
+        outputs = {
+            'loss': 0.5,
+            'log_vars': {
+                'accuracy': 0.98
+            },
+            'num_samples': 1
+        }
+        return outputs
+
+
+class NoBNExampleModel(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.conv = nn.Linear(1, 1)
+        self.test_cfg = None
+
+    def forward(self, imgs, return_loss=False):
+        return self.conv(imgs)
 
     def train_step(self, data_batch, optimizer, **kwargs):
         outputs = {
@@ -85,6 +107,7 @@ def test_precise_bn():
         runner.register_hook(precise_bn_hook)
         runner.run([loader], [('train', 1)], 1)
 
+    # test non-DDP model
     test_bigger_dataset = BiggerDataset()
     loader = DataLoader(test_bigger_dataset, batch_size=2)
     precise_bn_hook = PreciseBNHook(loader, num_iters=5)
@@ -94,3 +117,37 @@ def test_precise_bn():
         model=model, batch_processor=None, optimizer=optimizer, logger=logger)
     runner.register_hook(precise_bn_hook)
     runner.run([loader], [('train', 1)], 1)
+
+    # test model without bn layer
+    loader = DataLoader(test_bigger_dataset, batch_size=2)
+    precise_bn_hook = PreciseBNHook(loader, num_iters=5)
+    assert precise_bn_hook.num_iters == 5
+    assert precise_bn_hook.interval == 1
+    model = NoBNExampleModel()
+    runner = EpochBasedRunner(
+        model=model, batch_processor=None, optimizer=optimizer, logger=logger)
+    runner.register_hook(precise_bn_hook)
+    runner.run([loader], [('train', 1)], 1)
+
+    @pytest.mark.skipif(
+        not torch.cuda.is_available(), reason='requires CUDA support')
+    def test_ddp_model_precise_bn():
+        # test DDP model
+        test_bigger_dataset = BiggerDataset()
+        loader = DataLoader(test_bigger_dataset, batch_size=2)
+        precise_bn_hook = PreciseBNHook(loader, num_iters=5)
+        assert precise_bn_hook.num_iters == 5
+        assert precise_bn_hook.interval == 1
+        model = ExampleModel()
+        model = MMDistributedDataParallel(
+            model.cuda(),
+            device_ids=[torch.cuda.current_device()],
+            broadcast_buffers=False,
+            find_unused_parameters=True)
+        runner = EpochBasedRunner(
+            model=model,
+            batch_processor=None,
+            optimizer=optimizer,
+            logger=logger)
+        runner.register_hook(precise_bn_hook)
+        runner.run([loader], [('train', 1)], 1)
