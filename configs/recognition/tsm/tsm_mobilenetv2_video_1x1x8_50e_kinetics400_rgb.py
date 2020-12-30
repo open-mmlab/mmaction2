f36412456767001ab19949bb02be04322ded5540
irvingzhang0512 <irvingzhang0512@gmail.com>
2020-12-30 17:20:25 +0800

    update default training configs

--- configs/recognition/tsm/tsm_mobilenetv2_video_1x1x8_50e_kinetics400_rgb.py
+++ configs/recognition/tsm/tsm_mobilenetv2_video_1x1x8_50e_kinetics400_rgb.py
@@ -105,9 +105,9 @@ optimizer = dict(
     type='SGD',
     constructor='TSMOptimizerConstructor',
     paramwise_cfg=dict(fc_lr5=True),
-    lr=0.0025,  # this lr is used for 4 gpus
+    lr=0.01,  # this lr is used for 8 gpus
     momentum=0.9,
-    weight_decay=0.0001)
+    weight_decay=0.00004)
 optimizer_config = dict(grad_clip=dict(max_norm=20, norm_type=2))
 # learning policy
 lr_config = dict(policy='step', step=[20, 40])
