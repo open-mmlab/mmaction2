a841cc98baf724f51008f8d671871bf320028e7b
kenny <duanhaodong@sensetime.com>
2021-01-04 19:41:49 +0800

    reorganize code

--- mmaction/models/losses/base.py
+++ mmaction/models/losses/base.py
@@ -1,6 +1,5 @@
 from abc import ABCMeta, abstractmethod
 
-import torch
 import torch.nn as nn
 
 
@@ -13,18 +12,11 @@ class BaseWeightedLoss(nn.Module, metaclass=ABCMeta):
     Args:
         loss_weight (float): Factor scalar multiplied on the loss.
             Default: 1.0.
-        class_weight (list[float] | None): Loss weight for each class. If set
-            as None, use the same weight 1 for all classes. Only applies
-            to CrossEntropyLoss and BCELossWithLogits (should not be set when
-            using other losses). Default: None.
     """
 
-    def __init__(self, loss_weight=1.0, class_weight=None):
+    def __init__(self, loss_weight=1.0):
         super().__init__()
         self.loss_weight = loss_weight
-        self.class_weight = None
-        if class_weight is not None:
-            self.class_weight = torch.Tensor(class_weight)
 
     @abstractmethod
     def _forward(self, *args, **kwargs):
@@ -42,9 +34,6 @@ class BaseWeightedLoss(nn.Module, metaclass=ABCMeta):
         Returns:
             torch.Tensor: The calculated loss.
         """
-        if self.class_weight is not None:
-            kwargs['weight'] = self.class_weight.to(args[0].device)
-
         ret = self._forward(*args, **kwargs)
         if isinstance(ret, dict):
             for k in ret:
