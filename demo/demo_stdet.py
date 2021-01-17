5173a98ba92d8ca465ab77971494a62d72fab59a
Kenny <dhd.efz@gmail.com>
2021-01-17 11:01:11 +0800

    update code

--- demo/demo_stdet.py
+++ demo/demo_stdet.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import os.path as osp
 import warnings
 
@@ -7,6 +8,7 @@ import mmcv
 import numpy as np
 import torch
 from mmcv.runner import load_checkpoint
+from tqdm import tqdm
 
 from mmaction.models import build_detector
 
@@ -64,6 +66,7 @@ def parse_args():
 def frame_extraction(video_path):
     # Load the video, extract frames into /tmp/video_name
     target_dir = osp.join('/tmp', osp.basename(osp.splitext(video_path)[0]))
+    os.makedirs(target_dir, exist_ok=True)
     # Should be able to handle videos up to several hours
     frame_tmpl = osp.join(target_dir, 'img_{:06d}.jpg')
     vid = cv2.VideoCapture(video_path)
@@ -86,7 +89,8 @@ def detection_inference(args, frame_paths):
     assert model.CLASSES[0] == 'person', ('We require you to use a detector '
                                           'trained on COCO')
     results = []
-    for frame_path in frame_paths:
+    print('Performing Human Detection for each frame')
+    for frame_path in tqdm(frame_paths):
         result = inference_detector(model, frame_path)
         # We only keep human detections with score larger than det_score_thr
         result = result[0][result[0][4] >= args.det_score_thr]
@@ -97,40 +101,44 @@ def detection_inference(args, frame_paths):
 def main():
     args = parse_args()
 
-    frame_paths, frames = frame_extraction(args.video)
-    human_detections = detection_inference(args, frame_paths)
-    num_frame = len(frames)
-    h, w, _ = frames[0].shape
+    frame_paths, original_frames = frame_extraction(args.video)
+    num_frame = len(frame_paths)
+    h, w, _ = original_frames[0].shape
 
-    # All images will be resized to shortedge 256
-    new_w, new_h = mmcv.rescale_size((w, h), (-1, 256))
-    frames = [mmcv.imresize(img, (new_w, new_h)) for img in frames]
+    # resize frames to shortside 256
+    new_w, new_h = mmcv.rescale_size((w, h), (256, np.Inf))
+    frames = [mmcv.imresize(img, (new_w, new_h)) for img in original_frames]
     w_ratio, h_ratio = new_w / w, new_h / h
-    for i in range(num_frame):
-        det_result = human_detections[i]
-        det_result[:, 0:4:2] *= w_ratio
-        det_result[:, 1:4:2] *= h_ratio
 
-    # build the spatio temporal action detection from a config file and
-    # checkpoint file/url
-    # We will only use the 'model' part of the config file
+    # Get clip_len, frame_interval and calculate center index of each clip
     config = mmcv.Config.fromfile(args.config)
     val_pipeline = config['val_pipeline']
     sampler = [x for x in val_pipeline if x['type'] == 'SampleAVAFrames'][0]
     clip_len, frame_interval = sampler['clip_len'], sampler['frame_interval']
     window_size = clip_len * frame_interval
     assert clip_len % 2 == 0, 'We would like to have an even clip_len'
+    # Note that it's 1 based here
+    timestamps = np.arange(window_size // 2, num_frame + 1 - window_size // 2,
+                           args.predict_stepsize)
+
+    # Get Human detection results
+    center_frames = [frame_paths[ind - 1] for ind in timestamps]
+    human_detections = detection_inference(args, center_frames)
+    for i in range(len(human_detections)):
+        det_result = human_detections[i]
+        det_result[:, 0:4:2] *= w_ratio
+        det_result[:, 1:4:2] *= h_ratio
+
+    # Get img_norm_cfg
     img_norm_cfg = config['img_norm_cfg']
 
+    # Build STDET model
     config.model.backbone.pretrained = None
     model = build_detector(config.model, test_cfg=config.test_cfg)
 
     load_checkpoint(model, args.checkpoint, map_location=args.device)
     model.to(args.device)
     model.eval()
-
-    timestamps = np.arange(window_size // 2, num_frame + 1 - window_size // 2,
-                           args.predict_stepsize)
 
     for timestamp in timestamps:
         start_frame = timestamp - (clip_len // 2 - 1) * frame_interval
@@ -142,6 +150,7 @@ def main():
         input_array = np.stack(imgs).transpose((1, 0, 2, 3))[np.newaxis]
         input_tensor = torch.from_numpy(input_array).to(args.device)
         input_tensor *= 2.
+
         # pass
 
 
