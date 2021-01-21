f13888779c296b16a7bf4d583a0b99ae8c7b67e5
bkhuang <congee524@gmail.com>
2021-01-21 17:19:41 +0800

    remove dataset base

--- configs/recognition_audio/audioonly/audioonly_r50_64x1x1_100e_kinetics400_audio_feature.py
+++ configs/recognition_audio/audioonly/audioonly_r50_64x1x1_100e_kinetics400_audio_feature.py
@@ -1,8 +1,68 @@
 _base_ = [
-    '../../_base_/models/audioonly_r50.py',
-    '../../_base_/datasets/kinetics400_64x1x1_audio_feature.py',
-    '../../_base_/default_runtime.py'
+    '../../_base_/models/audioonly_r50.py', '../../_base_/default_runtime.py'
 ]
+
+# dataset settings
+dataset_type = 'AudioFeatureDataset'
+data_root = 'data/kinetics400/audio_feature_train'
+data_root_val = 'data/kinetics400/audio_feature_val'
+ann_file_train = 'data/kinetics400/kinetics400_train_list_audio_feature.txt'
+ann_file_val = 'data/kinetics400/kinetics400_val_list_audio_feature.txt'
+ann_file_test = 'data/kinetics400/kinetics400_val_list_audio_feature.txt'
+train_pipeline = [
+    dict(type='LoadAudioFeature'),
+    dict(type='SampleFrames', clip_len=64, frame_interval=1, num_clips=1),
+    dict(type='AudioFeatureSelector'),
+    dict(type='FormatAudioShape', input_format='NCTF'),
+    dict(type='Collect', keys=['audios', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['audios'])
+]
+val_pipeline = [
+    dict(type='LoadAudioFeature'),
+    dict(
+        type='SampleFrames',
+        clip_len=64,
+        frame_interval=1,
+        num_clips=1,
+        test_mode=True),
+    dict(type='AudioFeatureSelector'),
+    dict(type='FormatAudioShape', input_format='NCTF'),
+    dict(type='Collect', keys=['audios', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['audios'])
+]
+test_pipeline = [
+    dict(type='LoadAudioFeature'),
+    dict(
+        type='SampleFrames',
+        clip_len=64,
+        frame_interval=1,
+        num_clips=10,
+        test_mode=True),
+    dict(type='AudioFeatureSelector'),
+    dict(type='FormatAudioShape', input_format='NCTF'),
+    dict(type='Collect', keys=['audios', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['audios'])
+]
+data = dict(
+    videos_per_gpu=160,
+    workers_per_gpu=4,
+    train=dict(
+        type=dataset_type,
+        ann_file=ann_file_train,
+        data_prefix=data_root,
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=ann_file_val,
+        data_prefix=data_root_val,
+        pipeline=val_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=ann_file_test,
+        data_prefix=data_root_val,
+        pipeline=test_pipeline))
+evaluation = dict(
+    interval=5, metrics=['top_k_accuracy', 'mean_class_accuracy'])
 
 # optimizer
 optimizer = dict(
--- configs/recognition_audio/resnet/tsn_r50_64x1x1_100e_kinetics400_audio.py
+++ configs/recognition_audio/resnet/tsn_r50_64x1x1_100e_kinetics400_audio.py
@@ -4,6 +4,74 @@ _base_ = [
     '../../_base_/default_runtime.py'
 ]
 
+# dataset settings
+dataset_type = 'AudioDataset'
+data_root = 'data/kinetics400/audios'
+data_root_val = 'data/kinetics400/audios'
+ann_file_train = 'data/kinetics400/kinetics400_train_list_audio.txt'
+ann_file_val = 'data/kinetics400/kinetics400_val_list_audio.txt'
+ann_file_test = 'data/kinetics400/kinetics400_val_list_audio.txt'
+train_pipeline = [
+    dict(type='AudioDecodeInit'),
+    dict(type='SampleFrames', clip_len=64, frame_interval=1, num_clips=1),
+    dict(type='AudioDecode'),
+    dict(type='AudioAmplify', ratio=1.5),
+    dict(type='MelLogSpectrogram'),
+    dict(type='FormatAudioShape', input_format='NCTF'),
+    dict(type='Collect', keys=['audios', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['audios'])
+]
+val_pipeline = [
+    dict(type='AudioDecodeInit'),
+    dict(
+        type='SampleFrames',
+        clip_len=64,
+        frame_interval=1,
+        num_clips=1,
+        test_mode=True),
+    dict(type='AudioDecode'),
+    dict(type='AudioAmplify', ratio=1.5),
+    dict(type='MelLogSpectrogram'),
+    dict(type='FormatAudioShape', input_format='NCTF'),
+    dict(type='Collect', keys=['audios', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['audios'])
+]
+test_pipeline = [
+    dict(type='AudioDecodeInit'),
+    dict(
+        type='SampleFrames',
+        clip_len=64,
+        frame_interval=1,
+        num_clips=1,
+        test_mode=True),
+    dict(type='AudioDecodeInit'),
+    dict(type='AudioAmplify', ratio=1.5),
+    dict(type='MelLogSpectrogram'),
+    dict(type='FormatAudioShape', input_format='NCTF'),
+    dict(type='Collect', keys=['audios', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['audios'])
+]
+data = dict(
+    videos_per_gpu=320,
+    workers_per_gpu=4,
+    train=dict(
+        type=dataset_type,
+        ann_file=ann_file_train,
+        data_prefix=data_root,
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=ann_file_val,
+        data_prefix=data_root_val,
+        pipeline=val_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=ann_file_test,
+        data_prefix=data_root_val,
+        pipeline=test_pipeline))
+evaluation = dict(
+    interval=5, metrics=['top_k_accuracy', 'mean_class_accuracy'])
+
 # optimizer
 optimizer = dict(
     type='SGD', lr=0.1, momentum=0.9,
