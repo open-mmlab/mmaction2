25dd8e7b4afeb878d49eaec8ba4d0abf69ed64a2
bkhuang <congee524@gmail.com>
2021-01-21 16:58:42 +0800

    add lfb_infer_bank

--- configs/detection/lfb/lfb_slowonly_infer_bank.py
+++ configs/detection/lfb/lfb_slowonly_infer_bank.py
@@ -1,14 +1,10 @@
 # model settings
-
-lfb_prefix_path = 'data/ava_lfb'
-
 model = dict(
     type='FastRCNN',
     backbone=dict(
         type='ResNet3d',
         pretrained2d=True,
-        pretrained=  # noqa: E251
-        'https://download.openmmlab.com/mmaction/recognition/i3d/i3d_nl_embedded_gaussian_r50_32x2x1_100e_kinetics400_rgb/i3d_nl_embedded_gaussian_r50_32x2x1_100e_kinetics400_rgb_20200813-6e6aef1b.pth',  # noqa: E501
+        pretrained=None,
         depth=50,
         spatial_strides=(1, 2, 2, 1),
         conv1_stride_t=1,
@@ -31,23 +27,14 @@ model = dict(
             roi_layer_type='RoIAlign',
             output_size=7,
             with_temporal_pool=True,
-            with_global=False),  # output channel 2048 without global feat
-        shared_head=dict(
-            type='FBOHead',
-            lfb_prefix_path=lfb_prefix_path,
-            fbo_type='non_local',
-            fbo_cfg=dict(
-                num_non_local_layers=2,
-                num_st_feat_channels=2048,
-                num_lt_feat_channels=2048,
-                num_latent_channels=512)),
+            with_global=False),
         bbox_head=dict(
             type='BBoxHeadAVA',
-            in_channels=2569,
+            in_channels=2048,
             num_classes=81,
             multilabel=True,
             dropout_ratio=0.5)))
-
+# model training and testing settings
 train_cfg = dict(
     rcnn=dict(
         assigner=dict(
@@ -65,6 +52,7 @@ train_cfg = dict(
         debug=False))
 test_cfg = dict(rcnn=dict(action_thr=0.00))
 
+# dataset settings
 dataset_type = 'AVADataset'
 data_root = 'data/ava/rawframes'
 anno_root = 'data/ava/annotations'
@@ -84,9 +72,14 @@ proposal_file_val = f'{anno_root}/ava_dense_proposals_val.FAIR.recall_93.9.pkl'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=False)
 
+mc_cfg = dict(
+    server_list_cfg='/mnt/lustre/share/memcached_client/server_list.conf',
+    client_cfg='/mnt/lustre/share/memcached_client/client.conf',
+    sys_path='/mnt/lustre/share/pymc/py3')
+
 train_pipeline = [
     dict(type='SampleAVAFrames', clip_len=32, frame_interval=2),
-    dict(type='RawFrameDecode'),
+    dict(type='RawFrameDecode', io_backend='memcached', **mc_cfg),
     dict(type='RandomRescale', scale_range=(256, 320)),
     dict(type='RandomCrop', size=256),
     dict(type='Flip', flip_ratio=0.5),
@@ -103,12 +96,12 @@ train_pipeline = [
     dict(
         type='Collect',
         keys=['img', 'proposals', 'gt_bboxes', 'gt_labels'],
-        meta_keys=['scores', 'entity_ids', 'img_key'])
+        meta_keys=['scores', 'entity_ids'])
 ]
 # The testing is w/o. any cropping / flipping
 val_pipeline = [
     dict(type='SampleAVAFrames', clip_len=32, frame_interval=2),
-    dict(type='RawFrameDecode'),
+    dict(type='RawFrameDecode', io_backend='memcached', **mc_cfg),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='FormatShape', input_format='NCTHW', collapse=True),
@@ -119,7 +112,7 @@ val_pipeline = [
     dict(
         type='Collect',
         keys=['img', 'proposals'],
-        meta_keys=['scores', 'img_shape', 'img_key'],
+        meta_keys=['scores', 'img_shape'],
         nested=True)
 ]
 
@@ -132,6 +125,7 @@ data = dict(
         type=dataset_type,
         ann_file=ann_file_train,
         exclude_file=exclude_file_train,
+        filename_tmpl='img_{:06d}.jpg',
         pipeline=train_pipeline,
         label_file=label_file,
         proposal_file=proposal_file_train,
@@ -141,9 +135,39 @@ data = dict(
         type=dataset_type,
         ann_file=ann_file_val,
         exclude_file=exclude_file_val,
+        filename_tmpl='img_{:06d}.jpg',
         pipeline=val_pipeline,
         label_file=label_file,
         proposal_file=proposal_file_val,
         person_det_score_thr=0.9,
         data_prefix=data_root))
 data['test'] = data['val']
+
+optimizer = dict(type='SGD', lr=0.1125, momentum=0.9, weight_decay=0.00001)
+# this lr is used for 8 gpus
+
+optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
+
+# learning policy
+lr_config = dict(
+    policy='step',
+    step=[10, 15],
+    warmup='linear',
+    warmup_by_epoch=True,
+    warmup_iters=5,
+    warmup_ratio=0.1)
+total_epochs = 20
+checkpoint_config = dict(interval=1)
+workflow = [('train', 1)]
+evaluation = dict(interval=1)
+log_config = dict(
+    interval=20, hooks=[
+        dict(type='TextLoggerHook'),
+    ])
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+
+work_dir = './work_dirs/lfb_baseline/'
+load_from = 'https://download.openmmlab.com/mmaction/recognition/i3d/i3d_nl_embedded_gaussian_r50_32x2x1_100e_kinetics400_rgb/i3d_nl_embedded_gaussian_r50_32x2x1_100e_kinetics400_rgb_20200813-6e6aef1b.pth'  # noqa: E501
+resume_from = None
+find_unused_parameters = False
