48e93031364b1e2094ff37c5e2dd43140148b9c7
bkhuang <congee524@gmail.com>
2021-01-22 14:28:18 +0800

    update test_flip

--- tests/test_data/test_pipelines/test_augmentation/test_augmentations.py
+++ tests/test_data/test_pipelines/test_augmentation/test_augmentations.py
@@ -1,6 +1,5 @@
 import copy
 
-import mmcv
 import numpy as np
 import pytest
 from numpy.testing import assert_array_almost_equal
@@ -663,75 +662,6 @@ class TestAugumentations:
         assert repr(resize) == (f'{resize.__class__.__name__ }'
                                 f'(scale={(341, 256)}, keep_ratio={False}, ' +
                                 f'interpolation=bilinear, lazy={True})')
-
-    def test_flip(self):
-        with pytest.raises(ValueError):
-            # direction must be in ['horizontal', 'vertical']
-            Flip(direction='vertically')
-
-        target_keys = ['imgs', 'flip_direction', 'modality']
-
-        # do not flip imgs.
-        imgs = list(np.random.rand(2, 64, 64, 3))
-        results = dict(imgs=copy.deepcopy(imgs), modality='RGB')
-        results['gt_bboxes'] = np.array([[0, 0, 60, 60]])
-        results['proposals'] = None
-        flip = Flip(flip_ratio=0, direction='horizontal')
-        flip_results = flip(results)
-        assert self.check_keys_contain(flip_results.keys(), target_keys)
-        assert np.array_equal(imgs, results['imgs'])
-        assert id(flip_results['imgs']) == id(results['imgs'])
-        assert np.shape(flip_results['imgs']) == np.shape(imgs)
-
-        # always flip imgs horizontally.
-        imgs = list(np.random.rand(2, 64, 64, 3))
-        results = dict(imgs=copy.deepcopy(imgs), modality='RGB')
-        flip = Flip(flip_ratio=1, direction='horizontal')
-        flip_results = flip(results)
-        assert self.check_keys_contain(flip_results.keys(), target_keys)
-        if flip_results['flip'] is True:
-            assert self.check_flip(imgs, flip_results['imgs'],
-                                   flip_results['flip_direction'])
-        assert id(flip_results['imgs']) == id(results['imgs'])
-        assert np.shape(flip_results['imgs']) == np.shape(imgs)
-
-        # flip flow images horizontally
-        imgs = [
-            np.arange(16).reshape(4, 4).astype(np.float32),
-            np.arange(16, 32).reshape(4, 4).astype(np.float32)
-        ]
-        results = dict(imgs=copy.deepcopy(imgs), modality='Flow')
-        flip = Flip(flip_ratio=1, direction='horizontal')
-        flip_results = flip(results)
-        assert self.check_keys_contain(flip_results.keys(), target_keys)
-        imgs = [x.reshape(4, 4, 1) for x in imgs]
-        flip_results['imgs'] = [
-            x.reshape(4, 4, 1) for x in flip_results['imgs']
-        ]
-        if flip_results['flip'] is True:
-            assert self.check_flip([imgs[0]],
-                                   [mmcv.iminvert(flip_results['imgs'][0])],
-                                   flip_results['flip_direction'])
-            assert self.check_flip([imgs[1]], [flip_results['imgs'][1]],
-                                   flip_results['flip_direction'])
-        assert id(flip_results['imgs']) == id(results['imgs'])
-        assert np.shape(flip_results['imgs']) == np.shape(imgs)
-
-        # always flip imgs vertivally.
-        imgs = list(np.random.rand(2, 64, 64, 3))
-        results = dict(imgs=copy.deepcopy(imgs), modality='RGB')
-        flip = Flip(flip_ratio=1, direction='vertical')
-        flip_results = flip(results)
-        assert self.check_keys_contain(flip_results.keys(), target_keys)
-        if flip_results['flip'] is True:
-            assert self.check_flip(imgs, flip_results['imgs'],
-                                   flip_results['flip_direction'])
-        assert id(flip_results['imgs']) == id(results['imgs'])
-        assert np.shape(flip_results['imgs']) == np.shape(imgs)
-
-        assert repr(flip) == (f'{flip.__class__.__name__}'
-                              f'(flip_ratio={1}, direction=vertical, '
-                              f'lazy={False})')
 
     def test_flip_lazy(self):
         with pytest.raises(ValueError):
