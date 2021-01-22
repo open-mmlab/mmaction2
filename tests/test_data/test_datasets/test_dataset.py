bc8a8f9f2a188c86f38d1de60c387caf2ecbbfb6
bkhuang <congee524@gmail.com>
2021-01-22 12:03:30 +0800

    update test_audio_visual_dataset.py

--- tests/test_data/test_datasets/test_dataset.py
+++ tests/test_data/test_datasets/test_dataset.py
@@ -5,9 +5,8 @@ import pytest
 from mmcv import ConfigDict
 from numpy.testing import assert_array_almost_equal
 
-from mmaction.datasets import (AudioVisualDataset, HVUDataset, RawframeDataset,
-                               RawVideoDataset, RepeatDataset, SSNDataset,
-                               VideoDataset)
+from mmaction.datasets import (HVUDataset, RawframeDataset, RawVideoDataset,
+                               RepeatDataset, SSNDataset, VideoDataset)
 
 
 class TestDataset:
@@ -286,27 +285,6 @@ class TestDataset:
             assert info['total_frames'] == 5
             assert set(info['label']) == set(label)
         assert rawframe_dataset.start_index == 1
-
-    def test_audio_visual_dataset(self):
-        test_dataset = AudioVisualDataset(
-            self.frame_ann_file,
-            self.frame_pipeline,
-            self.data_prefix,
-            video_prefix=self.data_prefix,
-            data_prefix=self.data_prefix)
-        video_infos = test_dataset.video_infos
-        frame_dir = osp.join(self.data_prefix, 'test_imgs')
-        audio_path = osp.join(self.data_prefix, 'test_imgs.npy')
-        filename = osp.join(self.data_prefix, 'test_imgs.mp4')
-        assert video_infos == [
-            dict(
-                frame_dir=frame_dir,
-                audio_path=audio_path,
-                filename=filename,
-                total_frames=5,
-                label=127)
-        ] * 2
-        assert test_dataset.start_index == 1
 
     def test_dataset_realpath(self):
         dataset = RawframeDataset(self.frame_ann_file, self.frame_pipeline,
