595609a1acc8dc1096fea86c62381a969e3e5d86
xusu <xusu@sensetime.com>
2021-01-22 19:51:47 +0800

    Fix precise_bn unittest.

--- tests/test_runtime/test_train.py
+++ tests/test_runtime/test_train.py
@@ -75,6 +75,7 @@ def test_train_model():
         data=dict(
             videos_per_gpu=1,
             workers_per_gpu=0,
+            train=dict(type='ExampleDataset'),
             val=dict(type='ExampleDataset')),
         optimizer=dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001),
         optimizer_config=dict(grad_clip=dict(max_norm=40, norm_type=2)),
