257b5da15f761dbce2a028f72718d2b3d55c117c
bkhuang <congee524@gmail.com>
2021-01-22 16:07:43 +0800

    update test_decode

--- tests/test_data/test_pipelines/test_loading/base.py
+++ tests/test_data/test_pipelines/test_loading/base.py
@@ -23,11 +23,6 @@ class ExampleSSNInstance:
 
 class BaseTestLoading:
 
-    @staticmethod
-    def check_keys_contain(result_keys, target_keys):
-        """Check if all elements in target_keys is in result_keys."""
-        return set(target_keys).issubset(set(result_keys))
-
     @classmethod
     def setup_class(cls):
         cls.data_prefix = osp.normpath(
