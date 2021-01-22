cb140391adb1d5607ab7df1fdb4e36f1d1696374
bkhuang <congee524@gmail.com>
2021-01-22 13:25:27 +0800

    update test_repeat_dataset

--- tests/test_data/test_datasets/test_dataset.py
+++ tests/test_data/test_datasets/test_dataset.py
@@ -4,8 +4,7 @@ import numpy as np
 import pytest
 from mmcv import ConfigDict
 
-from mmaction.datasets import (RawframeDataset, RepeatDataset, SSNDataset,
-                               VideoDataset)
+from mmaction.datasets import SSNDataset, VideoDataset
 
 
 class TestDataset:
@@ -266,25 +265,6 @@ class TestDataset:
             start_index=3)
         assert len(video_dataset) == 2
         assert video_dataset.start_index == 3
-
-    def test_repeat_dataset(self):
-        rawframe_dataset = RawframeDataset(self.frame_ann_file,
-                                           self.frame_pipeline,
-                                           self.data_prefix)
-        repeat_dataset = RepeatDataset(rawframe_dataset, 5)
-        assert len(repeat_dataset) == 10
-        result_a = repeat_dataset[0]
-        result_b = repeat_dataset[2]
-        assert set(result_a.keys()) == set(result_b.keys())
-        for key in result_a:
-            if isinstance(result_a[key], np.ndarray):
-                assert np.equal(result_a[key], result_b[key]).all()
-            elif isinstance(result_a[key], list):
-                assert all(
-                    np.array_equal(a, b)
-                    for (a, b) in zip(result_a[key], result_b[key]))
-            else:
-                assert result_a[key] == result_b[key]
 
     def test_ssn_dataset(self):
         # test ssn dataset
