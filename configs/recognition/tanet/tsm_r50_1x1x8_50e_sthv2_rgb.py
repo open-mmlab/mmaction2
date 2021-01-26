48f6a22c762d93ed461ef37ad9e4bedaa11763ed
bkhuang <congee524@gmail.com>
2021-01-26 14:59:29 +0800

    add sth

--- configs/recognition/tanet/tsm_r50_1x1x8_50e_sthv2_rgb.py
+++ configs/recognition/tanet/tsm_r50_1x1x8_50e_sthv2_rgb.py
@@ -9,32 +9,27 @@ model = dict(
         tam_cfg=dict()),
     cls_head=dict(
         type='TSMHead',
-        num_classes=400,
+        num_classes=174,
         in_channels=2048,
         spatial_type='avg',
         consensus=dict(type='AvgConsensus', dim=1),
         dropout_ratio=0.5,
         init_std=0.001))
+# model training and testing settings
 train_cfg = None
 test_cfg = dict(average_clips='prob')
 # dataset settings
 dataset_type = 'RawframeDataset'
-data_root = 'data/kinetics400/rawframes_train'
-data_root_val = 'data/kinetics400/rawframes_val'
-ann_file_train = 'data/kinetics400/kinetics400_train_list_rawframes.txt'
-ann_file_val = 'data/kinetics400/kinetics400_val_list_rawframes.txt'
-ann_file_test = 'data/kinetics400/kinetics400_val_list_rawframes.txt'
-
-mc_cfg = dict(
-    server_list_cfg='/mnt/lustre/share/memcached_client/server_list.conf',
-    client_cfg='/mnt/lustre/share/memcached_client/client.conf',
-    sys_path='/mnt/lustre/share/pymc/py3')
-
+data_root = 'data/sthv2/rawframes'
+data_root_val = 'data/sthv2/rawframes'
+ann_file_train = 'data/sthv2/sthv2_train_list_rawframes.txt'
+ann_file_val = 'data/sthv2/sthv2_val_list_rawframes.txt'
+ann_file_test = 'data/sthv2/sthv2_val_list_rawframes.txt'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=False)
 train_pipeline = [
-    dict(type='DenseSampleFrames', clip_len=1, frame_interval=1, num_clips=8),
-    dict(type='RawFrameDecode', io_backend='memcached', **mc_cfg),
+    dict(type='SampleFrames', clip_len=1, frame_interval=1, num_clips=8),
+    dict(type='RawFrameDecode'),
     dict(type='Resize', scale=(-1, 256)),
     dict(
         type='MultiScaleCrop',
@@ -44,7 +39,6 @@ train_pipeline = [
         max_wh_scale_gap=1,
         num_fixed_crops=13),
     dict(type='Resize', scale=(224, 224), keep_ratio=False),
-    dict(type='Flip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='FormatShape', input_format='NCHW'),
     dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
@@ -52,15 +46,14 @@ train_pipeline = [
 ]
 val_pipeline = [
     dict(
-        type='DenseSampleFrames',
+        type='SampleFrames',
         clip_len=1,
         frame_interval=1,
-        num_clips=8,
+        num_clips=16,
         test_mode=True),
-    dict(type='RawFrameDecode', io_backend='memcached', **mc_cfg),
+    dict(type='RawFrameDecode'),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='CenterCrop', crop_size=224),
-    dict(type='Flip', flip_ratio=0),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='FormatShape', input_format='NCHW'),
     dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
@@ -68,15 +61,14 @@ val_pipeline = [
 ]
 test_pipeline = [
     dict(
-        type='DenseSampleFrames',
+        type='SampleFrames',
         clip_len=1,
         frame_interval=1,
-        num_clips=8,
+        num_clips=16,
         test_mode=True),
-    dict(type='RawFrameDecode', io_backend='memcached', **mc_cfg),
+    dict(type='RawFrameDecode'),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='CenterCrop', crop_size=224),
-    dict(type='Flip', flip_ratio=0),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='FormatShape', input_format='NCHW'),
     dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
@@ -85,21 +77,22 @@ test_pipeline = [
 data = dict(
     videos_per_gpu=8,
     workers_per_gpu=4,
-    val_dataloader=dict(videos_per_gpu=4),
-    test_dataloader=dict(videos_per_gpu=4),
     train=dict(
         type=dataset_type,
         ann_file=ann_file_train,
         data_prefix=data_root,
+        filename_tmpl='{:05}.jpg',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
         ann_file=ann_file_val,
+        filename_tmpl='{:05}.jpg',
         data_prefix=data_root_val,
         pipeline=val_pipeline),
     test=dict(
         type=dataset_type,
         ann_file=ann_file_test,
+        filename_tmpl='{:05}.jpg',
         data_prefix=data_root_val,
         pipeline=test_pipeline))
 # optimizer
@@ -109,14 +102,14 @@ optimizer = dict(
     paramwise_cfg=dict(fc_lr5=True),
     lr=0.01,  # this lr is used for 8 gpus
     momentum=0.9,
-    weight_decay=0.0001)
+    weight_decay=0.001)
 optimizer_config = dict(grad_clip=dict(max_norm=20, norm_type=2))
 # learning policy
-lr_config = dict(policy='step', step=[50, 75, 90])
-total_epochs = 100
-checkpoint_config = dict(interval=2)
+lr_config = dict(policy='step', step=[30, 40, 45])
+total_epochs = 50
+checkpoint_config = dict(interval=1)
 evaluation = dict(
-    interval=2, metrics=['top_k_accuracy', 'mean_class_accuracy'])
+    interval=1, metrics=['top_k_accuracy', 'mean_class_accuracy'])
 log_config = dict(
     interval=20,
     hooks=[
@@ -126,7 +119,7 @@ log_config = dict(
 # runtime settings
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/tanet_r50_dense_1x1x8_100e_kinetics400_rgb/'
+work_dir = './work_dirs/tanet_r50_1x1x8_50e_sthv2_rgb/'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
