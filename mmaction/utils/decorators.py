c94e3c04bcd787edef47f14f282eaa4c3eed8f0f
bkhuang <congee524@gmail.com>
2021-02-01 15:20:52 +0800

    add unit test

--- mmaction/utils/decorators.py
+++ mmaction/utils/decorators.py
@@ -1,22 +1,6 @@
 from types import MethodType
 
 
-def import_module_error_class(module_name):
-    """When a class is imported incorrectly due to a missing module, raise an
-    import error when the class is instantiated."""
-
-    def decorate(cls):
-
-        def __init__(self, **kwargs):
-            raise ImportError(
-                f'Please install {module_name} to use {self.__name__}.')
-
-        cls.__init__ = MethodType(__init__, cls)
-        return cls
-
-    return decorate
-
-
 def import_module_error_func(module_name):
     """When a function is imported incorrectly due to a missing module, raise
     an import error when the function is called."""
@@ -29,5 +13,21 @@ def import_module_error_func(module_name):
             return func(*args, **kwargs)
 
         return new_func
+
+    return decorate
+
+
+def import_module_error_class(module_name):
+    """When a class is imported incorrectly due to a missing module, raise an
+    import error when the class is instantiated."""
+
+    def decorate(cls):
+
+        def import_error_init(*args, **kwargs):
+            raise ImportError(
+                f'Please install {module_name} to use {cls.__name__}.')
+
+        cls.__init__ = MethodType(import_error_init, cls)
+        return cls
 
     return decorate
