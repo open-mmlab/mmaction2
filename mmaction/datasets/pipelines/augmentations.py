a0cc324e892ee55c625c5be611890273994397c9
irvingzhang0512 <irvingzhang0512@gmail.com>
2021-02-01 20:01:40 +0800

    fix test

--- mmaction/datasets/pipelines/augmentations.py
+++ mmaction/datasets/pipelines/augmentations.py
@@ -164,10 +164,10 @@ class Imgaug:
                         shear=17.19 * cur_level * random.choice([-1, 1])),
                     dict(
                         type='TranslateX',
-                        px=100 * cur_level * random.choice([-1, 1])),
+                        px=int(100 * cur_level * random.choice([-1, 1]))),
                     dict(
                         type='TranslateY',
-                        px=100 * cur_level * random.choice([-1, 1])),
+                        px=int(100 * cur_level * random.choice([-1, 1]))),
                     dict(
                         type='Rotate',
                         rotate=30 * cur_level * random.choice([-1, 1])),
