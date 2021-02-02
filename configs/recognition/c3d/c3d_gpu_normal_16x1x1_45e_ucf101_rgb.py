249d24543bf2b8b8e1550f47ccfe262e627afb7d
bkhuang <congee524@gmail.com>
2021-02-02 15:48:59 +0800

    add c3d gpu normal cfg

---
+++ configs/recognition/c3d/c3d_gpu_normal_16x1x1_45e_ucf101_rgb.py
@@ -0,0 +1,107 @@
+_base_ = '../../_base_/models/c3d_sports1m_pretrained.py'
+
+module_hooks = [
+    dict(
+        type='GpuNormalize',
+        input_format='NCTHW',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375])
+]
+
+# dataset settings
+dataset_type = 'RawframeDataset'
+data_root = 'data/ucf101/rawframes'
+data_root_val = 'data/ucf101/rawframes'
+split = 1  # official train/test splits. valid numbers: 1, 2, 3
+ann_file_train = f'data/ucf101/ucf101_train_split_{split}_rawframes.txt'
+ann_file_val = f'data/ucf101/ucf101_val_split_{split}_rawframes.txt'
+ann_file_test = f'data/ucf101/ucf101_val_split_{split}_rawframes.txt'
+
+mc_cfg = dict(
+    server_list_cfg='/mnt/lustre/share/memcached_client/server_list.conf',
+    client_cfg='/mnt/lustre/share/memcached_client/client.conf',
+    sys_path='/mnt/lustre/share/pymc/py3')
+
+train_pipeline = [
+    dict(type='SampleFrames', clip_len=16, frame_interval=1, num_clips=1),
+    dict(type='RawFrameDecode', io_backend='memcached', **mc_cfg),
+    dict(type='Resize', scale=(128, 171)),
+    dict(type='RandomCrop', size=112),
+    dict(type='Flip', flip_ratio=0.5),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['imgs', 'label'])
+]
+val_pipeline = [
+    dict(
+        type='SampleFrames',
+        clip_len=16,
+        frame_interval=1,
+        num_clips=1,
+        test_mode=True),
+    dict(type='RawFrameDecode', io_backend='memcached', **mc_cfg),
+    dict(type='Resize', scale=(128, 171)),
+    dict(type='CenterCrop', crop_size=112),
+    dict(type='Flip', flip_ratio=0),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['imgs', 'label'])
+]
+test_pipeline = [
+    dict(
+        type='SampleFrames',
+        clip_len=16,
+        frame_interval=1,
+        num_clips=10,
+        test_mode=True),
+    dict(type='RawFrameDecode', io_backend='memcached', **mc_cfg),
+    dict(type='Resize', scale=(128, 171)),
+    dict(type='CenterCrop', crop_size=112),
+    dict(type='Flip', flip_ratio=0),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['imgs', 'label'])
+]
+data = dict(
+    videos_per_gpu=30,
+    workers_per_gpu=2,
+    test_dataloader=dict(videos_per_gpu=1),
+    train=dict(
+        type=dataset_type,
+        ann_file=ann_file_train,
+        data_prefix=data_root,
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=ann_file_val,
+        data_prefix=data_root_val,
+        pipeline=val_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=ann_file_test,
+        data_prefix=data_root_val,
+        pipeline=test_pipeline))
+# optimizer
+optimizer = dict(
+    type='SGD', lr=0.001, momentum=0.9,
+    weight_decay=0.0005)  # this lr is used for 8 gpus
+optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
+# learning policy
+lr_config = dict(policy='step', step=[20, 40])
+total_epochs = 50
+checkpoint_config = dict(interval=5)
+evaluation = dict(
+    interval=5, metrics=['top_k_accuracy', 'mean_class_accuracy'])
+log_config = dict(
+    interval=20,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        # dict(type='TensorboardLoggerHook'),
+    ])
+# runtime settings
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+work_dir = './work_dirs/c3d_gpu_normal_16x1x1_45e_ucf101_rgb/'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
