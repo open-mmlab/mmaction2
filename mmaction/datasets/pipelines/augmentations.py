0d0061f78d4edd0025adda496d8fe2df6c8073fd
bkhuang <congee524@gmail.com>
2021-02-03 14:37:50 +0800

    add label transform for flip

--- mmaction/datasets/pipelines/augmentations.py
+++ mmaction/datasets/pipelines/augmentations.py
@@ -1091,16 +1091,23 @@ class Flip:
         flip_ratio (float): Probability of implementing flip. Default: 0.5.
         direction (str): Flip imgs horizontally or vertically. Options are
             "horizontal" | "vertical". Default: "horizontal".
+        label_transforms (Dict[int, int] | None): Transform the label of the
+            flipped image with the specific label. Default: None.
         lazy (bool): Determine whether to apply lazy operation. Default: False.
     """
     _directions = ['horizontal', 'vertical']
 
-    def __init__(self, flip_ratio=0.5, direction='horizontal', lazy=False):
+    def __init__(self,
+                 flip_ratio=0.5,
+                 direction='horizontal',
+                 label_transforms=None,
+                 lazy=False):
         if direction not in self._directions:
             raise ValueError(f'Direction {direction} is not supported. '
                              f'Currently support ones are {self._directions}')
         self.flip_ratio = flip_ratio
         self.direction = direction
+        self.label_transforms = label_transforms
         self.lazy = lazy
 
     def __call__(self, results):
@@ -1119,6 +1126,11 @@ class Flip:
 
         results['flip'] = flip
         results['flip_direction'] = self.direction
+
+        if self.label_transforms is not None and flip:
+            label = results['label']
+            if label in self.label_transforms.keys():
+                results['label'] = self.label_transforms[label]
 
         if not self.lazy:
             if flip:
