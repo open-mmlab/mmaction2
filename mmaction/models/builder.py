cf608deaefd5d0aeb9f888dbe52800ccf20389d0
bkhuang <congee524@gmail.com>
2021-02-24 13:00:30 +0800

    update warning msg of build_model and build_recognizer

--- mmaction/models/builder.py
+++ mmaction/models/builder.py
@@ -55,7 +55,9 @@ def build_recognizer(cfg, train_cfg=None, test_cfg=None):
     if train_cfg is not None or test_cfg is not None:
         warnings.warn(
             'train_cfg and test_cfg is deprecated, '
-            'please specify them in model', UserWarning)
+            'please specify them in model. Details see this '
+            '[PR](https://github.com/open-mmlab/mmaction2/pull/629)',
+            UserWarning)
     assert cfg.get('train_cfg') is None or train_cfg is None, \
         'train_cfg specified in both outer field and model field '
     assert cfg.get('test_cfg') is None or test_cfg is None, \
@@ -83,6 +85,12 @@ def build_model(cfg, train_cfg=None, test_cfg=None):
     if obj_type in RECOGNIZERS:
         return build_recognizer(cfg, train_cfg, test_cfg)
     if obj_type in DETECTORS:
+        if train_cfg is not None or test_cfg is not None:
+            warnings.warn(
+                'train_cfg and test_cfg is deprecated, '
+                'please specify them in model. Details see this '
+                '[PR](https://github.com/open-mmlab/mmaction2/pull/629)',
+                UserWarning)
         return build_detector(cfg, train_cfg, test_cfg)
     model_in_mmdet = ['FastRCNN']
     if obj_type in model_in_mmdet:
