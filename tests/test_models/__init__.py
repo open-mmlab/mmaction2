b9d1519938a7f4a66fbef567d61c382b078b88f6
Kenny <dhd.efz@gmail.com>
2021-03-03 13:23:17 +0800

    add unittest for detector

--- tests/test_models/__init__.py
+++ tests/test_models/__init__.py
@@ -1,10 +1,12 @@
 from .base import (check_norm_state, generate_backbone_demo_inputs,
-                   generate_gradcam_inputs, generate_recognizer_demo_inputs,
-                   get_audio_recognizer_cfg, get_cfg, get_localizer_cfg,
+                   generate_detector_demo_inputs, generate_gradcam_inputs,
+                   generate_recognizer_demo_inputs, get_audio_recognizer_cfg,
+                   get_cfg, get_detector_cfg, get_localizer_cfg,
                    get_recognizer_cfg)
 
 __all__ = [
     'check_norm_state', 'generate_backbone_demo_inputs',
     'generate_recognizer_demo_inputs', 'generate_gradcam_inputs', 'get_cfg',
-    'get_recognizer_cfg', 'get_audio_recognizer_cfg', 'get_localizer_cfg'
+    'get_recognizer_cfg', 'get_audio_recognizer_cfg', 'get_localizer_cfg',
+    'get_detector_cfg', 'generate_detector_demo_inputs'
 ]
