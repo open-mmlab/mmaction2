22099c177da0e3f9acc1c89d449e7ecc3e4413a7
bkhuang <congee524@gmail.com>
2021-03-05 15:10:20 +0800

    remove train_cfg and test_cfg in bmn and bsn and modify average_clips judge

--- tools/test.py
+++ tools/test.py
@@ -141,13 +141,13 @@ def main():
         torch.backends.cudnn.benchmark = True
     cfg.data.test.test_mode = True
 
-    if cfg.model.get('test_cfg') is None and cfg.get('test_cfg') is None:
-        cfg.model.setdefault('test_cfg',
-                             dict(average_clips=args.average_clips))
-    else:
+    if args.average_clips is not None:
         # You can set average_clips during testing, it will override the
         # original settting
-        if args.average_clips is not None:
+        if cfg.model.get('test_cfg') is None and cfg.get('test_cfg') is None:
+            cfg.model.setdefault('test_cfg',
+                                 dict(average_clips=args.average_clips))
+        else:
             if cfg.model.get('test_cfg') is not None:
                 cfg.model.test_cfg.average_clips = args.average_clips
             else:
