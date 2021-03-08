1396c3fc0ba4ea2b9bc0369b63a6f4223892a5df
congee <35596075+congee524@users.noreply.github.com>
2021-03-08 17:42:26 +0800

    fix flow norm cfg typo (#693)

--- configs/recognition/tsn/tsn_r50_320p_1x1x8_110e_kinetics400_flow.py
+++ configs/recognition/tsn/tsn_r50_320p_1x1x8_110e_kinetics400_flow.py
@@ -7,8 +7,7 @@ data_root_val = 'data/kinetics400/rawframes_val_320p'
 ann_file_train = 'data/kinetics400/kinetics400_flow_train_list_320p.txt'
 ann_file_val = 'data/kinetics400/kinetics400_flow_val_list_320p.txt'
 ann_file_test = 'data/kinetics400/kinetics400_flow_val_list_320p.txt'
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=False)
+img_norm_cfg = dict(mean=[128, 128], std=[128, 128])
 train_pipeline = [
     dict(type='SampleFrames', clip_len=5, frame_interval=1, num_clips=8),
     dict(type='RawFrameDecode'),
