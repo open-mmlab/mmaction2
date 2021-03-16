60a6b15d629e9b1c0067774cbf2e069b2f374283
irvingzhang0512 <irvingzhang0512@gmail.com>
2021-03-17 00:16:18 +0800

    add unittest

--- tests/test_models/test_common_modules/test_base_head.py
+++ tests/test_models/test_common_modules/test_base_head.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 from mmcv.utils import assert_dict_has_keys
 
 from mmaction.models import BaseHead
@@ -30,4 +31,42 @@ def test_base_head():
     gt_labels = torch.LongTensor([2] * 3).squeeze()
     losses = head.loss(cls_scores, gt_labels)
     assert_dict_has_keys(losses, ['loss_cls'])
+    assert losses.get('loss_cls') > 0, 'cls loss should be non-zero'
+
+    # Test Soft label with batch size > 1
+    cls_scores = torch.rand((3, 3))
+    gt_labels = torch.LongTensor([[2] * 3])
+    gt_one_hot_labels = F.one_hot(gt_labels, num_classes=3).squeeze()
+    losses = head.loss(cls_scores, gt_one_hot_labels)
+    assert 'loss_cls' in losses.keys()
+    assert losses.get('loss_cls') > 0, 'cls loss should be non-zero'
+
+    # Test Soft label with batch size = 1
+    cls_scores = torch.rand((1, 3))
+    gt_labels = torch.LongTensor([2])
+    gt_one_hot_labels = F.one_hot(gt_labels, num_classes=3).squeeze()
+    losses = head.loss(cls_scores, gt_one_hot_labels)
+    assert 'loss_cls' in losses.keys()
+    assert losses.get('loss_cls') > 0, 'cls loss should be non-zero'
+
+    # test multi-class & label smoothing
+    head = ExampleHead(
+        3,
+        400,
+        dict(type='BCELossWithLogits'),
+        multi_class=True,
+        label_smooth_eps=0.1)
+
+    # batch size > 1
+    cls_scores = torch.rand((2, 3))
+    gt_labels = torch.LongTensor([[1, 0, 1], [0, 1, 0]]).squeeze()
+    losses = head.loss(cls_scores, gt_labels)
+    assert 'loss_cls' in losses.keys()
+    assert losses.get('loss_cls') > 0, 'cls loss should be non-zero'
+
+    # batch size = 1
+    cls_scores = torch.rand((1, 3))
+    gt_labels = torch.LongTensor([[1, 0, 1]]).squeeze()
+    losses = head.loss(cls_scores, gt_labels)
+    assert 'loss_cls' in losses.keys()
     assert losses.get('loss_cls') > 0, 'cls loss should be non-zero'
--- tests/test_models/test_recognizers/test_recognizer2d.py
+++ tests/test_models/test_recognizers/test_recognizer2d.py
@@ -57,6 +57,18 @@ def test_tsn():
         for one_img in img_list:
             recognizer(one_img, None, return_loss=False)
 
+    # test mixup forward
+    config = get_recognizer_cfg(
+        'tsn/tsn_r50_video_mixup_1x1x8_100e_kinetics400_rgb.py')
+    config.model['backbone']['pretrained'] = None
+    recognizer = build_recognizer(config.model)
+    input_shape = (2, 8, 3, 32, 32)
+    demo_inputs = generate_recognizer_demo_inputs(input_shape)
+    imgs = demo_inputs['imgs']
+    gt_labels = demo_inputs['gt_labels']
+    losses = recognizer(imgs, gt_labels)
+    assert isinstance(losses, dict)
+
 
 def test_tsm():
     config = get_recognizer_cfg('tsm/tsm_r50_1x1x8_50e_kinetics400_rgb.py')
