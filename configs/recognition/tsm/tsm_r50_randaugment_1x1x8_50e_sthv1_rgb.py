80aa4c802ebe19840f284c82debb39cb8014342a
irvingzhang0512 <irvingzhang0512@gmail.com>
2021-03-17 09:43:38 +0800

    fix config

--- configs/recognition/tsm/tsm_r50_randaugment_1x1x8_50e_sthv1_rgb.py
+++ configs/recognition/tsm/tsm_r50_randaugment_1x1x8_50e_sthv1_rgb.py
@@ -13,11 +13,8 @@ data_root_val = 'data/sthv1/rawframes'
 ann_file_train = 'data/sthv1/sthv1_train_list_rawframes.txt'
 ann_file_val = 'data/sthv1/sthv1_val_list_rawframes.txt'
 ann_file_test = 'data/sthv1/sthv1_val_list_rawframes.txt'
-
-sthv1_flip_label_map = {2: 4, 4: 2, 30: 41, 41: 30, 52: 66, 66: 52}
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=False)
-
 train_pipeline = [
     dict(type='SampleFrames', clip_len=1, frame_interval=1, num_clips=8),
     dict(type='RawFrameDecode'),
@@ -30,7 +27,6 @@ train_pipeline = [
         max_wh_scale_gap=1,
         num_fixed_crops=13),
     dict(type='Resize', scale=(224, 224), keep_ratio=False),
-    dict(type='Flip', flip_ratio=0.5, flip_label_map=sthv1_flip_label_map),
     dict(type='Imgaug', transforms='default'),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='FormatShape', input_format='NCHW'),
@@ -95,4 +91,4 @@ evaluation = dict(
 optimizer = dict(weight_decay=0.0005)
 
 # runtime settings
-work_dir = './work_dirs/tsm_r50_flip_1x1x8_50e_sthv1_rgb/'
+work_dir = './work_dirs/tsm_r50_randaugment_1x1x8_50e_sthv1_rgb/'
