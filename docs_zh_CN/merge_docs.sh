2911bc7630647ef38529c92364263997a1de20c7
bkhuang <congee524@gmail.com>
2021-03-30 17:33:14 +0800

    fix bugs

--- docs_zh_CN/merge_docs.sh
+++ docs_zh_CN/merge_docs.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
-
-sed -i '$a\\n' ../demo/README_zh-CN.md
-
 # gather models
 cat  ../configs/localization/*/README_zh-CN.md | sed "s/md###t/html#t/g" | sed "s/#/#&/" | sed '1i\# 时序动作检测模型' | sed 's/](\/docs_zh_CN\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmaction2/tree/master/=g' > localization_models.md
 cat  ../configs/recognition/*/README_zh-CN.md | sed "s/md###t/html#t/g" | sed "s/#/#&/" | sed '1i\# 动作识别模型' | sed 's/](\/docs_zh_CN\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmaction2/tree/master/=g' > recognition_models.md
 cat  ../configs/recognition_audio/*/README_zh-CN.md | sed "s/md###t/html#t/g" | sed "s/#/#&/" | sed 's/](\/docs_zh_CN\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmaction2/tree/master/=g' >> recognition_models.md
 cat  ../configs/detection/*/README_zh-CN.md  | sed "s/md###t/html#t/g" | sed "s/#/#&/" | sed '1i\# 时空动作检测模型' | sed 's/](\/docs_zh_CN\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmaction2/tree/master/=g' > detection_models.md
-
-# demo
-cat  ../demo/README_zh-CN.md | sed "s/md###t/html#t/g" | sed 's=](/=](https://github.com/open-mmlab/mmaction2/tree/master/=g' > demo.md
 
 # gather datasets
 cat  ../tools/data/*/README_zh-CN.md | sed 's/# Preparing/# /g' | sed 's/#/#&/' > prepare_data.md
@@ -34,9 +28,10 @@ cat prepare_data.md >> supported_datasets.md
 sed -i 's/](\/docs_zh_CN\//](/g' supported_datasets.md
 sed -i 's=](/=](https://github.com/open-mmlab/mmaction2/tree/master/=g' supported_datasets.md
 
+sed -i "s/md###t/html#t/g" demo.md
+sed -i 's=](/=](https://github.com/open-mmlab/mmaction2/tree/master/=g' demo.md
 sed -i 's=](/=](https://github.com/open-mmlab/mmaction2/tree/master/=g' benchmark.md
 sed -i 's=](/=](https://github.com/open-mmlab/mmaction2/tree/master/=g' getting_started.md
 sed -i 's=](/=](https://github.com/open-mmlab/mmaction2/tree/master/=g' install.md
-sed -i 's=](/=](https://github.com/open-mmlab/mmaction2/tree/master/=g' changelog.md
 sed -i 's/](\/docs_zh_CN\//](/g' ./tutorials/*.md
 sed -i 's=](/=](https://github.com/open-mmlab/mmaction2/tree/master/=g' ./tutorials/*.md
