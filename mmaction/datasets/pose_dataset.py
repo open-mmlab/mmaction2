8e2b3385dbef01dcf1e7438d4cd8a18ca874d8ba
Kenny <dhd.efz@gmail.com>
2021-04-05 17:37:43 +0800

    add pose_dataset

---
+++ mmaction/datasets/pose_dataset.py
@@ -0,0 +1,185 @@
+import copy
+import os.path as osp
+
+import mmcv
+import numpy as np
+from mmcv.utils import print_log
+
+from ..core import mean_class_accuracy, top_k_accuracy
+from ..utils import get_root_logger
+from .base import BaseDataset
+from .registry import DATASETS
+
+
+@DATASETS.register_module()
+class PoseDataset(BaseDataset):
+    """Pose dataset for action recognition.
+
+    The dataset loads pose and apply specified transforms to return a
+    dict containing pose information.
+
+    The ann_file is a pickle file, the json file contains a list of
+    annotations, the fields of an annotation include frame_dir(video_id),
+    total_frames, label, kp, kpscore.
+
+    Args:
+        ann_file (str): Path to the annotation file.
+        pipeline (list[dict | callable]): A sequence of data transforms.
+        resample (float | None): The sampling probability for classes 61-120 in
+            NTU-120 XSub split. If set as 1.5, it means the probability of
+            sampling a video in classes 61-120 is 1.5 times than the
+            probability of sampling a video in classes 1-60. None means not
+            applicable (only applicable to NTU-120 XSub). Default: None.
+        valid_ratio (float | None): The valid_ratio for videos in KineticsPose.
+            For a video with n frames, it is a valid training sample only if
+            n * valid_ratio frames have human pose. None means not applicable
+            (only applicable to Kinetics Pose). Default: None.
+        box_thre (str | None): The threshold for human proposals. Only boxes
+            with confidence score larger than `box_thre` is kept. None means
+            not applicable (only applicable to Kinetics Pose [ours]). Allowed
+            choices are '0.5', '0.6', '0.7', '0.8', '0.9'. Default: None.
+        **kwargs: Keyword arguments for ``BaseDataset``.
+    """
+
+    def __init__(self,
+                 ann_file,
+                 pipeline,
+                 resample=None,
+                 valid_ratio=None,
+                 box_thre=None,
+                 **kwargs):
+        # For NTU-120 X-Sub
+        self.resample = resample
+        modality = 'Pose'
+
+        super().__init__(
+            ann_file, pipeline, start_index=0, modality=modality, **kwargs)
+
+        # box_thre, which should be a string
+        self.box_thre = box_thre
+        if self.box_thre is not None:
+            assert box_thre in ['0.5', '0.6', '0.7', '0.8', '0.9']
+
+        # Thresholding Training Examples
+        self.valid_ratio = valid_ratio
+        if self.valid_ratio is not None:
+            assert isinstance(self.valid_ratio, float)
+            if self.box_thre is None:
+                self.video_infos = self.video_infos = [
+                    x for x in self.video_infos
+                    if x['valid_frames'] / x['total_frames'] >= valid_ratio
+                ]
+            else:
+                key = f'valid@{self.box_thre}'
+                self.video_infos = [
+                    x for x in self.video_infos
+                    if x[key] / x['total_frames'] >= valid_ratio
+                ]
+                if self.box_thre != '0.5':
+                    box_thre = float(self.box_thre)
+                    for item in self.video_infos:
+                        inds = [
+                            i for i, score in enumerate(item['box_score'])
+                            if score >= box_thre
+                        ]
+                        item['anno_inds'] = np.array(inds)
+
+        logger = get_root_logger()
+        logger.info(f'{len(self)} videos remain after valid thresholding')
+
+    def load_annotations(self):
+        """Load annotation file to get video information."""
+        assert self.ann_file.endswith('.pkl')
+        return self.load_pkl_annotations()
+
+    def load_pkl_annotations(self):
+        data = mmcv.load(self.ann_file)
+
+        for i, item in enumerate(data):
+            # Sometimes we may need to load anno from the file
+            if 'filename' in item:
+                item['filename'] = osp.join(self.data_prefix, item['filename'])
+            if 'frame_dir' in item:
+                item['frame_dir'] = osp.join(self.data_prefix,
+                                             item['frame_dir'])
+        return data
+
+    def prepare_train_frames(self, idx):
+        """Prepare the frames for training given the index."""
+        results = copy.deepcopy(self.video_infos[idx])
+
+        results['modality'] = self.modality
+        results['start_index'] = self.start_index
+        return self.pipeline(results)
+
+    def prepare_test_frames(self, idx):
+        """Prepare the frames for testing given the index."""
+        results = copy.deepcopy(self.video_infos[idx])
+
+        results['modality'] = self.modality
+        results['start_index'] = self.start_index
+        return self.pipeline(results)
+
+    def evaluate(self,
+                 results,
+                 metrics='top_k_accuracy',
+                 topk=(1, 5),
+                 logger=None,
+                 **kwargs):
+        """Evaluation in rawframe dataset.
+
+        Args:
+            results (list): Output results.
+            metrics (str | sequence[str]): Metrics to be performed.
+                Defaults: 'top_k_accuracy'.
+            logger (obj): Training logger. Defaults: None.
+            topk (tuple[int]): K value for top_k_accuracy metric.
+                Defaults: (1, 5).
+            logger (logging.Logger | None): Logger for recording.
+                Default: None.
+
+        Return:
+            dict: Evaluation results dict.
+        """
+        if not isinstance(results, list):
+            raise TypeError(f'results must be a list, but got {type(results)}')
+        assert len(results) == len(self), (
+            f'The length of results is not equal to the dataset len: '
+            f'{len(results)} != {len(self)}')
+
+        if not isinstance(topk, (int, tuple)):
+            raise TypeError(
+                f'topk must be int or tuple of int, but got {type(topk)}')
+
+        metrics = metrics if isinstance(metrics, (list, tuple)) else [metrics]
+        allowed_metrics = ['top_k_accuracy', 'mean_class_accuracy']
+
+        for metric in metrics:
+            if metric not in allowed_metrics:
+                raise KeyError(f'metric {metric} is not supported')
+
+        eval_results = {}
+        gt_labels = [ann['label'] for ann in self.video_infos]
+
+        for metric in metrics:
+            msg = f'Evaluating {metric}...'
+            if logger is None:
+                msg = '\n' + msg
+            print_log(msg, logger=logger)
+
+            if metric == 'top_k_accuracy':
+                top_k_acc = top_k_accuracy(results, gt_labels, topk)
+                log_msg = []
+                for k, acc in zip(topk, top_k_acc):
+                    eval_results[f'top{k}_acc'] = acc
+                    log_msg.append(f'\ntop{k}_acc\t{acc:.4f}')
+                log_msg = ''.join(log_msg)
+                print_log(log_msg, logger=logger)
+
+            if metric == 'mean_class_accuracy':
+                mean_acc = mean_class_accuracy(results, gt_labels)
+                eval_results['mean_class_accuracy'] = mean_acc
+                log_msg = f'\nmean_acc\t{mean_acc:.4f}'
+                print_log(log_msg, logger=logger)
+
+        return eval_results
