55611df7d9cdb747d5aa25503cfd3c3f62cca52b
irving <irvingzhang0512@gmail.com>
2021-05-21 01:07:25 +0800

    remove extract frames by opencv

--- tools/data/ava/extract_rgb_frames_opencv.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd ../
-python build_rawframes.py ../../data/ava/videos_15min/ ../../data/ava/rawframes/ --task rgb --level 1 --use-opencv --mixed-ext
-echo "Genearte raw frames (RGB only)"
-
-cd ava/
