3e5b432fd6a1d339513c72bd41b093f656eb953a
wangruohui <12756472+wangruohui@users.noreply.github.com>
2021-06-03 12:26:52 +0800

    fix lint

--- demo/faster_rcnn_r50_fpn_2x_coco.py
+++ demo/faster_rcnn_r50_fpn_2x_coco.py
@@ -102,8 +102,7 @@ model = dict(
         rcnn=dict(
             score_thr=0.05,
             nms=dict(type='nms', iou_threshold=0.5),
-            max_per_img=100)
-    ))
+            max_per_img=100)))
 
 # dataset config
 dataset_type = 'CocoDataset'
