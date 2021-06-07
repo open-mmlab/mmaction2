9d5cabc02c735de957b9e75a0471b3917d51f18b
irvingzhang0512 <irvingzhang0512@gmail.com>
2021-06-07 09:44:05 +0800

    add tool

---
+++ tools/check_videos.py
@@ -0,0 +1,95 @@
+import argparse
+import os
+import warnings
+
+from mmcv import Config, DictAction
+from tqdm import tqdm
+
+from mmaction.datasets import build_dataset
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='MMAction2 check datasets')
+    parser.add_argument('config', help='test config file path')
+    parser.add_argument(
+        '--options',
+        nargs='+',
+        action=DictAction,
+        default={},
+        help='custom options for evaluation, the key-value pair in xxx=yyy '
+        'format will be kwargs for dataset.evaluate() function (deprecate), '
+        'change to --eval-options instead.')
+    parser.add_argument(
+        '--cfg-options',
+        nargs='+',
+        action=DictAction,
+        default={},
+        help='override some settings in the used config, the key-value pair '
+        'in xxx=yyy format will be merged into config file. For example, '
+        "'--cfg-options model.backbone.depth=18 model.backbone.with_cp=True'")
+    parser.add_argument(
+        '--output-file',
+        default='corrupted-video.txt',
+        help='Output file path which keeps corrupted/missing video file paths')
+    parser.add_argument('--split', default='train')
+    parser.add_argument(
+        '--decoder', default='decord', help='Video decoder type')
+    parser.add_argument(
+        '--remove-corrupted-videos',
+        action='store_true',
+        help='Whether to delete all corrupted videos')
+    args = parser.parse_args()
+
+    if args.options and args.eval_options:
+        raise ValueError(
+            '--options and --eval-options cannot be both '
+            'specified, --options is deprecated in favor of --eval-options')
+    if args.options:
+        warnings.warn('--options is deprecated in favor of --eval-options')
+        args.eval_options = args.options
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    assert args.split in ['train', 'val', 'test']
+
+    decoder_to_pipeline = dict(
+        decord='DecordInit',
+        opencv='OpenCVInit',
+        pyav='PyAVInit',
+    )
+    assert args.decoder in decoder_to_pipeline
+
+    cfg = Config.fromfile(args.config)
+    cfg.merge_from_dict(args.cfg_options)
+
+    dataset_type = cfg.data[args.split].type
+    assert dataset_type == 'VideoDataset'
+
+    # modify video decord pipeline
+    cfg.data[args.split].pipeline = [
+        dict(type=decoder_to_pipeline[args.decoder])
+    ]
+    dataset = build_dataset(cfg.data[args.split],
+                            dict(test_mode=(args.split != 'train')))
+
+    writer = open(args.output_file, 'w')
+    cnt = 0
+    for i in tqdm(range(len(dataset))):
+        try:
+            dataset[i]
+        except:  # noqa
+            writer.write(dataset.video_infos[i]['filename'] + '\n')
+            cnt += 1
+
+    print(f'Checked {len(dataset)} videos, {cnt} is/are corrupted/missing. ')
+    writer.close()
+
+    if args.remove_corrupted_videos:
+        print('Start deleting corrupted videos')
+        with open(args.output_file, 'r') as f:
+            for line in f:
+                if os.path.exists(line.strip()):
+                    os.remove(line.strip())
