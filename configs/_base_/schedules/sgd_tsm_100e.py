6770ff36d0f9e8cc133163e9f89af0a7c300eacd
dreamerlin <528557675@qq.com>
2021-07-01 16:29:23 +0800

    update TSM

--- configs/_base_/schedules/sgd_tsm_100e.py
+++ configs/_base_/schedules/sgd_tsm_100e.py
@@ -3,7 +3,7 @@ optimizer = dict(
     type='SGD',
     constructor='TSMOptimizerConstructor',
     paramwise_cfg=dict(fc_lr5=True),
-    lr=0.02,  # this lr is used for 8 gpus
+    lr=0.01,  # this lr is used for 8 gpus
     momentum=0.9,
     weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=20, norm_type=2))
