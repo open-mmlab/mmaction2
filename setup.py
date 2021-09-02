cd6c15d9d3d4d55491f8971d215438b701479710
Kenny <dhd.efz@gmail.com>
2021-09-02 11:29:25 +0800

    support python 3.9

--- setup.py
+++ setup.py
@@ -171,6 +171,7 @@ if __name__ == '__main__':
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
         ],
         url='https://github.com/open-mmlab/mmaction2',
         license='Apache License 2.0',
