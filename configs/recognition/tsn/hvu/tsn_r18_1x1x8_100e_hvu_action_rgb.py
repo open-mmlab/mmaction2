c33c4907fdc3cb468743ceaea31f3fb7492c939a
Kenny <dhd.efz@gmail.com>
2021-09-02 11:58:37 +0800

    hvu frames to videos

--- configs/recognition/tsn/hvu/tsn_r18_1x1x8_100e_hvu_action_rgb.py
+++ configs/recognition/tsn/hvu/tsn_r18_1x1x8_100e_hvu_action_rgb.py
@@ -18,17 +18,18 @@ model = dict(
         loss_cls=dict(type='BCELossWithLogits', loss_weight=333.)))
 
 # dataset settings
-dataset_type = 'RawframeDataset'
-data_root = 'data/hvu/rawframes_train'
-data_root_val = 'data/hvu/rawframes_val'
+dataset_type = 'VideoDataset'
+data_root = 'data/hvu/videos_train'
+data_root_val = 'data/hvu/videos_val'
 ann_file_train = f'data/hvu/hvu_{target_cate}_train.json'
 ann_file_val = f'data/hvu/hvu_{target_cate}_val.json'
 ann_file_test = f'data/hvu/hvu_{target_cate}_val.json'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=False)
 train_pipeline = [
+    dict(type='DecordInit'),
     dict(type='SampleFrames', clip_len=1, frame_interval=1, num_clips=8),
-    dict(type='RawFrameDecode'),
+    dict(type='DecordDecode'),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='RandomResizedCrop'),
     dict(type='Resize', scale=(224, 224), keep_ratio=False),
@@ -39,13 +40,14 @@ train_pipeline = [
     dict(type='ToTensor', keys=['imgs', 'label'])
 ]
 val_pipeline = [
+    dict(type='DecordInit'),
     dict(
         type='SampleFrames',
         clip_len=1,
         frame_interval=1,
         num_clips=8,
         test_mode=True),
-    dict(type='RawFrameDecode'),
+    dict(type='DecordDecode'),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='CenterCrop', crop_size=256),
     dict(type='Normalize', **img_norm_cfg),
@@ -54,13 +56,14 @@ val_pipeline = [
     dict(type='ToTensor', keys=['imgs'])
 ]
 test_pipeline = [
+    dict(type='DecordInit'),
     dict(
         type='SampleFrames',
         clip_len=1,
         frame_interval=1,
         num_clips=25,
         test_mode=True),
-    dict(type='RawFrameDecode'),
+    dict(type='DecordDecode'),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='ThreeCrop', crop_size=256),
     dict(type='Normalize', **img_norm_cfg),
@@ -78,24 +81,21 @@ data = dict(
         data_prefix=data_root,
         pipeline=train_pipeline,
         multi_class=True,
-        num_classes=category_nums[target_cate],
-        filename_tmpl='img_{:05d}.jpg'),
+        num_classes=category_nums[target_cate]),
     val=dict(
         type=dataset_type,
         ann_file=ann_file_val,
         data_prefix=data_root_val,
         pipeline=val_pipeline,
         multi_class=True,
-        num_classes=category_nums[target_cate],
-        filename_tmpl='img_{:05d}.jpg'),
+        num_classes=category_nums[target_cate]),
     test=dict(
         type=dataset_type,
         ann_file=ann_file_test,
         data_prefix=data_root_val,
         pipeline=test_pipeline,
         multi_class=True,
-        num_classes=category_nums[target_cate],
-        filename_tmpl='img_{:05d}.jpg'))
+        num_classes=category_nums[target_cate]))
 evaluation = dict(interval=2, metrics=['mean_average_precision'])
 
 # runtime settings
