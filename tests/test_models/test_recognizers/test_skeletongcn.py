773b4d87556d9e438d6aebd088b64abbe61f2b62
gengenkai <1457154934@qq.com>
2021-09-09 19:27:45 +0800

    add unittest 0909

--- tests/test_models/test_recognizers/test_skeletongcn.py
+++ tests/test_models/test_recognizers/test_skeletongcn.py
@@ -1,3 +1,4 @@
+import pytest
 import torch
 
 from mmaction.models import build_recognizer
@@ -6,8 +7,31 @@ from ..base import generate_recognizer_demo_inputs, get_skeletongcn_cfg
 
 def test_skeletongcn():
     config = get_skeletongcn_cfg('stgcn/stgcn_ntu-rgbd60_xsub.py')
-    config.model['backbone']['pretrained'] = None
+    with pytest.raises(TypeError):
+        # "pretrained" must be a str or None
+        config.model['backbone']['pretrained'] = ['None']
+        recognizer = build_recognizer(config.model)
 
+    config.model['backbone']['pretrained'] = None
+    recognizer = build_recognizer(config.model)
+
+    input_shape = (1, 3, 300, 17, 2)
+    demo_inputs = generate_recognizer_demo_inputs(input_shape, 'skeleton')
+
+    skeletons = demo_inputs['imgs']
+    gt_labels = demo_inputs['gt_labels']
+
+    losses = recognizer(skeletons, gt_labels)
+    assert isinstance(losses, dict)
+
+    # Test forward test
+    with torch.no_grad():
+        skeleton_list = [skeleton[None, :] for skeleton in skeletons]
+        for one_skeleton in skeleton_list:
+            recognizer(one_skeleton, None, return_loss=False)
+
+    # test stgcn without edge importance weighting
+    config.model['backbone']['edge_importance_weighting'] = False
     recognizer = build_recognizer(config.model)
 
     input_shape = (1, 3, 300, 17, 2)
