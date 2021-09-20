f45d97ae07403610c69c7e67078c2a532ce1c61d
Haodong Duan <dhd.efz@gmail.com>
2021-09-20 14:03:36 +0800

    new doc style

--- docs_zh_CN/conf.py
+++ docs_zh_CN/conf.py
@@ -15,6 +15,8 @@ import os
 import subprocess
 import sys
 
+import pytorch_sphinx_theme
+
 sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
@@ -61,7 +63,8 @@ source_suffix = {'.rst': 'restructuredtext', '.md': 'markdown'}
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'pytorch_sphinx_theme'
+html_theme_path = [pytorch_sphinx_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -69,7 +72,6 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = []
 
 language = 'zh_CN'
-
 master_doc = 'index'
 
 
