a9719428d5830788025b632fcdbdb97ba2dac2a2
Jintao Lin <528557675@qq.com>
2021-09-28 14:48:15 +0800

    [Fix] Fix device bug for class weight (#1188)
    * fix device error
    
    * fix device error

--- mmaction/models/losses/cross_entropy_loss.py
+++ mmaction/models/losses/cross_entropy_loss.py
@@ -58,6 +58,7 @@ class CrossEntropyLoss(BaseWeightedLoss):
 
             lsm = F.log_softmax(cls_score, 1)
             if self.class_weight is not None:
+                self.class_weight = self.class_weight.to(cls_score.device)
                 lsm = lsm * self.class_weight.unsqueeze(0)
             loss_cls = -(label * lsm).sum(1)
 
