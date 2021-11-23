5ba44b4ae45bfd26ffdd5def8510f33fc20fde11
Haodong Duan <dhd.efz@gmail.com>
2021-11-23 21:51:09 +0800

    [Fix] init_random_seed

--- mmaction/apis/__init__.py
+++ mmaction/apis/__init__.py
@@ -1,9 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .inference import inference_recognizer, init_recognizer
 from .test import multi_gpu_test, single_gpu_test
-from .train import train_model
+from .train import init_random_seed, train_model
 
 __all__ = [
     'train_model', 'init_recognizer', 'inference_recognizer', 'multi_gpu_test',
-    'single_gpu_test'
+    'single_gpu_test', 'init_random_seed'
 ]
--- mmaction/apis/train.py
+++ mmaction/apis/train.py
@@ -2,7 +2,9 @@
 import copy as cp
 import os.path as osp
 
+import numpy as np
 import torch
+import torch.distributed as dist
 from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
 from mmcv.runner import (DistSamplerSeedHook, EpochBasedRunner, OptimizerHook,
                          build_optimizer, get_dist_info)
@@ -13,6 +15,39 @@ from ..core import (DistEvalHook, EvalHook, OmniSourceDistSamplerSeedHook,
 from ..datasets import build_dataloader, build_dataset
 from ..utils import PreciseBNHook, get_root_logger
 from .test import multi_gpu_test
+
+
+def init_random_seed(seed=None, device='cuda'):
+    """Initialize random seed.
+
+    If the seed is not set, the seed will be automatically randomized,
+    and then broadcast to all processes to prevent some potential bugs.
+    Args:
+        seed (int, Optional): The seed. Default to None.
+        device (str): The device where the seed will be put on.
+            Default to 'cuda'.
+    Returns:
+        int: Seed to be used.
+    """
+    if seed is not None:
+        return seed
+
+    # Make sure all ranks share the same random seed to prevent
+    # some potential bugs. Please refer to
+    # https://github.com/open-mmlab/mmdetection/issues/6339
+    rank, world_size = get_dist_info()
+    seed = np.random.randint(2**31)
+
+    if world_size == 1:
+        return seed
+
+    if rank == 0:
+        random_num = torch.tensor(seed, dtype=torch.int32, device=device)
+    else:
+        random_num = torch.tensor(0, dtype=torch.int32, device=device)
+
+    dist.broadcast(random_num, src=0)
+    return random_num.item()
 
 
 def train_model(model,
