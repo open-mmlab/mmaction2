3a908fa769de92bc87a1a3c16ef877d797246873
liguankai <liguankai@sensetime.com>
2022-02-07 09:29:43 +0800

    0207 slowfast steplr

---
+++ configs/recognition/slowfast/slowfast_r50_8x8x1_256e_kinetics400_rgb_steplr.py
@@ -0,0 +1,18 @@
+_base_ = ['./slowfast_r50_4x16x1_256e_kinetics400_rgb.py']
+
+model = dict(
+    backbone=dict(
+        resample_rate=4,  # tau
+        speed_ratio=4,  # alpha
+        channel_ratio=8,  # beta_inv
+        slow_pathway=dict(fusion_kernel=5)))
+
+lr_config = dict(
+    policy='step',
+    min_lr=0,
+    warmup='linear',
+    warmup_by_epoch=True,
+    warmup_iters=34,
+    step=[94, 154, 196])
+
+work_dir = './work_dirs/slowfast_r50_8x8x1_256e_kinetics400_rgb_steplr'
