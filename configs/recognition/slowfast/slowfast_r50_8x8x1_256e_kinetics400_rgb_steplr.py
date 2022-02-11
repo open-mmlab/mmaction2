3d16a41cc3ad1e148709fe5034b3b11a79723475
liguankai <liguankai@sensetime.com>
2022-02-11 15:47:34 +0800

    0211 add prebn steplr config

--- configs/recognition/slowfast/slowfast_r50_8x8x1_256e_kinetics400_rgb_steplr.py
+++ configs/recognition/slowfast/slowfast_r50_8x8x1_256e_kinetics400_rgb_steplr.py
@@ -1,5 +1,7 @@
 _base_ = ['./slowfast_r50_8x8x1_256e_kinetics400_rgb.py']
 
+model = dict(backbone=dict(slow_pathway=dict(lateral_norm=True)))
+
 lr_config = dict(
     policy='step',
     min_lr=0,
