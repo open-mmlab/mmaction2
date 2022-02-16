181f6c09dec48ad8a616df0132513515492ad367
liguankai <liguankai@sensetime.com>
2022-02-16 14:33:10 +0800

    0216 multigrid

--- mmaction/apis/__init__.py
+++ mmaction/apis/__init__.py
@@ -2,9 +2,8 @@
 from .inference import inference_recognizer, init_recognizer
 from .test import multi_gpu_test, single_gpu_test
 from .train import init_random_seed, train_model
-from .train_multi import train_model_multi
 
 __all__ = [
     'train_model', 'init_recognizer', 'inference_recognizer', 'multi_gpu_test',
-    'single_gpu_test', 'init_random_seed', 'train_model_multi'
+    'single_gpu_test', 'init_random_seed'
 ]
