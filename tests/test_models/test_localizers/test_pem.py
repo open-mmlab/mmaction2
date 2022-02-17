28bfcd7d9f910cc64a9ac11dcaa47333ac11bb4e
Haodong Duan <dhd.efz@gmail.com>
2022-02-17 12:40:30 +0800

    fix CI

--- tests/test_models/test_localizers/test_pem.py
+++ tests/test_models/test_localizers/test_pem.py
@@ -1,10 +1,14 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import platform
+
+import pytest
 import torch
 
 from mmaction.models import build_localizer
 from ..base import get_localizer_cfg
 
 
+@pytest.mark.skipif(platform.system() == 'Windows', reason='Windows mem limit')
 def test_pem():
     model_cfg = get_localizer_cfg(
         'bsn/bsn_pem_400x100_1x16_20e_activitynet_feature.py')
