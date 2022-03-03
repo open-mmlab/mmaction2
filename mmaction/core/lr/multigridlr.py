b333112136318367ba16d52cfb4a4bc8871fa767
liguankai <liguankai@sensetime.com>
2022-03-03 11:37:22 +0800

    modify muligrid lr

--- mmaction/core/lr/multigridlr.py
+++ mmaction/core/lr/multigridlr.py
@@ -12,16 +12,29 @@ class RelativeStepLrUpdaterHook(LrUpdaterHook):
         **kwargs (dict): Same as that of mmcv.
     """
 
-    def __init__(self, runner, steps, lrs, **kwargs):
+    def __init__(self,
+                 runner,
+                 steps,
+                 lrs,
+                 warmup_epochs=34,
+                 warmuplr_start=0.01,
+                 **kwargs):
         super().__init__(**kwargs)
         assert len(steps) == (len(lrs))
         self.steps = steps
         self.lrs = lrs
+        self.warmup_epochs = warmup_epochs
+        self.warmuplr_start = warmuplr_start
+        self.warmuplr_end = self.lrs[0]
         super().before_run(runner)
 
     def get_lr(self, runner, base_lr):
         """Similar to that of mmcv."""
         progress = runner.epoch if self.by_epoch else runner.iter
+        if progress <= self.warmup_epochs:
+            alpha = (self.warmuplr_end -
+                     self.warmuplr_start) / self.warmup_epochs
+            return progress * alpha + self.warmuplr_start
         for i in range(len(self.steps)):
             if progress < self.steps[i]:
                 return self.lrs[i]
