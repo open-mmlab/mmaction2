da1daad72b167ed3d55310b033fce2b2c7d1d3d5
Haodong Duan <dhd.efz@gmail.com>
2022-03-30 12:42:16 +0800

    fix bug

--- mmaction/models/heads/roi_head.py
+++ mmaction/models/heads/roi_head.py
@@ -2,7 +2,6 @@
 import numpy as np
 
 from mmaction.core.bbox import bbox2result
-from mmaction.utils import import_module_error_class
 
 try:
     from mmdet.core.bbox import bbox2roi
@@ -118,7 +117,6 @@ if mmdet_imported:
             return det_bboxes, det_labels
 else:
     # Just define an empty class, so that __init__ can import it.
-    @import_module_error_class('mmdet')
     class AVARoIHead:
 
         def __init__(self, *args, **kwargs):
