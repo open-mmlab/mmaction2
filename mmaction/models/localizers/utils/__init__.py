0b82c93d68571b0c68ef0da61c36d7e354ae3227
daiwenxun.vendor <daiwenxun@pjlab.org.cn>
2022-07-14 20:57:29 +0800

    p3

--- mmaction/models/localizers/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .post_processing import post_processing
-
-__all__ = ['post_processing']
--- mmaction/utils/multigrid/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .longshortcyclehook import LongShortCycleHook
-from .short_sampler import ShortCycleSampler
-from .subbn_aggregate import SubBatchNorm3dAggregationHook
-
-__all__ = [
-    'ShortCycleSampler', 'LongShortCycleHook', 'SubBatchNorm3dAggregationHook'
-]
