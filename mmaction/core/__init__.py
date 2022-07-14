e5e73100ed96bc87ce11bf992bb425690113e92d
actioning <hkpjlab@gmail.com>
2022-07-14 13:09:19 +0800

    fix conflict at mmaction/core/__init__.py

--- mmaction/core/__init__.py
+++ mmaction/core/__init__.py
@@ -8,10 +8,5 @@ from .lr import *  # noqa: F401, F403
 from .optimizer import *  # noqa: F401, F403
 from .runner import *  # noqa: F401, F403
 from .scheduler import *  # noqa: F401, F403
-<<<<<<< HEAD
+from .utils import *  # noqa: F401, F403
 from .visualization import *  # noqa: F401, F403
-from .utils import * # noqa: F401, F403
-=======
-from .utils import * # noqa: F401, F403
-from .visualization import *  # noqa: F401, F403
->>>>>>> 8c8b3902 (type hint)
