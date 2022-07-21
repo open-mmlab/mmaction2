514e50b5a6a6a07fc70efd64cd091d958e20c689
wxDai <daiwenxun@pjlab.org.cn>
2022-07-21 15:23:42 +0800

    [Refactor] Update Version  (#1715)
    * update version
    
    * Update Version
    
    Co-authored-by: KaiHoo <kaiorhu@gmail.com>

--- mmaction/__init__.py
+++ mmaction/__init__.py
@@ -5,8 +5,8 @@ from mmengine.utils import digit_version
 
 from .version import __version__
 
-mmcv_minimum_version = '1.4.2'
-mmcv_maximum_version = '1.7.0'
+mmcv_minimum_version = '2.0.0rc0'
+mmcv_maximum_version = '2.1.0'
 mmcv_version = digit_version(mmcv.__version__)
 
 mmengine_minimum_version = '0.0.0'
