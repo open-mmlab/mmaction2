ddf762c883879b3a824a7a7e2e2f6449d4b9abfe
daiwenxun.vendor <daiwenxun@pjlab.org.cn>
2022-08-10 15:17:56 +0800

    add base dataset

--- mmaction/datasets/__init__.py
+++ mmaction/datasets/__init__.py
@@ -4,10 +4,12 @@ from .pose_dataset import PoseDataset
 from .rawframe_dataset import RawframeDataset
 from .transforms import *  # noqa: F401, F403
 from .video_dataset import VideoDataset
+from .base import BaseActionDataset
 
 __all__ = [
     'VideoDataset',
     'RawframeDataset',
     'AVADataset',
     'PoseDataset',
+    'BaseActionDataset'
 ]
