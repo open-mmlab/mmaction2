a86c84e8300a03c8c83c6ddb33a89a1eef9da93f
daiwenxun.vendor <daiwenxun@pjlab.org.cn>
2022-08-15 11:25:31 +0800

    change ``analysis`` to ``analysis_tools``

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .analysis import *  # noqa: F401, F403
+from .analysis_tools import *  # noqa: F401, F403
 from .data import *  # noqa: F401, F403
 from .deployment import *  # noqa: F401, F403
 from .misc import *  # noqa: F401, F403
