f6563895f52ae41fce101575a746b2c2c09dbd92
daiwenxun.vendor <daiwenxun@pjlab.org.cn>
2022-08-15 14:34:32 +0800

    update trn

--- configs/recognition/trn/trn_r50_1x1x8_50e_8xb16_sthv1_rgb.py
+++ configs/recognition/trn/trn_r50_1x1x8_50e_8xb16_sthv1_rgb.py
@@ -1,22 +1,17 @@
-_base_ = [
-    '../../_base_/models/trn_r50.py', '../../_base_/schedules/sgd_tsm_50e.py',
-    '../../_base_/default_runtime.py'
-]
+_base_ = ['trn_r50_1x1x8_50e_8xb16_sthv2_rgb.py']
 
 # model settings
 model = dict(cls_head=dict(num_classes=174))
 
 # dataset settings
 dataset_type = 'RawframeDataset'
-data_root = 'data/sthv2/rawframes'
-data_root_val = 'data/sthv2/rawframes'
-ann_file_train = 'data/sthv2/sthv2_train_list_rawframes.txt'
-ann_file_val = 'data/sthv2/sthv2_val_list_rawframes.txt'
-ann_file_test = 'data/sthv2/sthv2_val_list_rawframes.txt'
+data_root = 'data/sthv1/rawframes'
+data_root_val = 'data/sthv1/rawframes'
+ann_file_train = 'data/sthv1/sthv1_train_list_rawframes.txt'
+ann_file_val = 'data/sthv1/sthv1_val_list_rawframes.txt'
+ann_file_test = 'data/sthv1/sthv1_val_list_rawframes.txt'
 
-sthv2_flip_label_map = {86: 87, 87: 86, 93: 94, 94: 93, 166: 167, 167: 166}
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=False)
+sthv1_flip_label_map = {2: 4, 4: 2, 30: 41, 41: 30, 52: 66, 66: 52}
 train_pipeline = [
     dict(type='SampleFrames', clip_len=1, frame_interval=1, num_clips=8),
     dict(type='RawFrameDecode'),
@@ -29,8 +24,7 @@ train_pipeline = [
         max_wh_scale_gap=1,
         num_fixed_crops=13),
     dict(type='Resize', scale=(224, 224), keep_ratio=False),
-    dict(type='Flip', flip_ratio=0.5, flip_label_map=sthv2_flip_label_map),
-    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Flip', flip_ratio=0.5, flip_label_map=sthv1_flip_label_map),
     dict(type='FormatShape', input_format='NCHW'),
     dict(type='PackActionInputs')
 ]
@@ -44,7 +38,6 @@ val_pipeline = [
     dict(type='RawFrameDecode'),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='CenterCrop', crop_size=224),
-    dict(type='Normalize', **img_norm_cfg),
     dict(type='FormatShape', input_format='NCHW'),
     dict(type='PackActionInputs')
 ]
@@ -59,61 +52,41 @@ test_pipeline = [
     dict(type='RawFrameDecode'),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='ThreeCrop', crop_size=256),
-    dict(type='Normalize', **img_norm_cfg),
     dict(type='FormatShape', input_format='NCHW'),
     dict(type='PackActionInputs')
 ]
-
 train_dataloader = dict(
     batch_size=16,
-    num_workers=2,
+    num_workers=8,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=True),
     dataset=dict(
         type=dataset_type,
         ann_file=ann_file_train,
         data_prefix=dict(img=data_root),
+        filename_tmpl='{:05}.jpg',
         pipeline=train_pipeline))
 val_dataloader = dict(
     batch_size=16,
-    num_workers=2,
+    num_workers=8,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=False),
     dataset=dict(
         type=dataset_type,
         ann_file=ann_file_val,
         data_prefix=dict(img=data_root_val),
+        filename_tmpl='{:05}.jpg',
         pipeline=val_pipeline,
         test_mode=True))
 test_dataloader = dict(
     batch_size=1,
-    num_workers=2,
+    num_workers=8,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=False),
     dataset=dict(
         type=dataset_type,
         ann_file=ann_file_val,
         data_prefix=dict(img=data_root_val),
+        filename_tmpl='{:05}.jpg',
         pipeline=test_pipeline,
         test_mode=True))
-
-val_evaluator = dict(type='AccMetric')
-test_evaluator = val_evaluator
-
-val_cfg = dict(interval=1)
-test_cfg = dict()
-
-# optimizer
-optimizer = dict(lr=0.002, paramwise_cfg=dict(fc_lr5=False), weight_decay=5e-4)
-# learning policy
-train_cfg = dict(by_epoch=True, max_epochs=50)
-
-param_scheduler = [
-    dict(
-        type='MultiStepLR',
-        begin=0,
-        end=50,
-        by_epoch=True,
-        milestones=[30, 45],
-        gamma=0.1)
-]
