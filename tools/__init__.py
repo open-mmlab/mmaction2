a653254717e149b94f45c25b29e585376b484bfd
wxDai <daiwenxun@pjlab.org.cn>
2022-08-15 12:40:59 +0800

    [Refactor] change ``analysis`` to ``analysis_tools`` (#1787)
    * change ``analysis`` to ``analysis_tools``
    
    * delete ``__init__.py`` of ``tools`` dir

--- tools/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .analysis import *  # noqa: F401, F403
-from .data import *  # noqa: F401, F403
-from .deployment import *  # noqa: F401, F403
-from .misc import *  # noqa: F401, F403
