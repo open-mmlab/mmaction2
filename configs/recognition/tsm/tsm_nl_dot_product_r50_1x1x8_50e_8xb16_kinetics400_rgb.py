e0d833db5913c5a248525c6b37f2d6a48c68252b
KaiHoo <kaiorhu@gmail.com>
2022-08-16 10:26:56 +0800

    [Refactor] Update TSM model

---
+++ configs/recognition/tsm/tsm_nl_dot_product_r50_1x1x8_50e_8xb16_kinetics400_rgb.py
@@ -0,0 +1,12 @@
+_base_ = ['tsm_r50_1x1x8_50e_8xb16_kinetics400_rgb.py']
+
+# model settings
+model = dict(
+    backbone=dict(
+        non_local=((0, 0, 0), (1, 0, 1, 0), (1, 0, 1, 0, 1, 0), (0, 0, 0)),
+        non_local_cfg=dict(
+            sub_sample=True,
+            use_scale=False,
+            norm_cfg=dict(type='BN3d', requires_grad=True),
+            mode='dot_product'),
+    ))
