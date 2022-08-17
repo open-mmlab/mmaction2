676720383415f61ca6c70e3c8649b4cf72be3f01
KaiHoo <kaiorhu@gmail.com>
2022-08-17 10:22:57 +0800

    [Refactor] refactor LFB

--- mmaction/models/roi_heads/shared_heads/__init__.py
+++ mmaction/models/roi_heads/shared_heads/__init__.py
@@ -1,4 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .misc_head import ACRNHead
+from .acrn_head import ACRNHead
+from .fbo_head import FBOHead
+from .lfb_infer_head import LFBInferHead
 
-__all__ = ['ACRNHead']
+__all__ = ['ACRNHead', 'LFBInferHead', 'FBOHead']
