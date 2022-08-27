4e50a824d3abb708619978de65a30eee2daf81bd
wxDai <daiwenxun@pjlab.org.cn>
2022-08-27 16:50:45 +0800

    add get_started.md (#1836)

--- mmaction/apis/inference.py
+++ mmaction/apis/inference.py
@@ -1,40 +1,32 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-import os
-import os.path as osp
-import re
-import warnings
 from operator import itemgetter
+from typing import List, Optional, Tuple, Union
 
 import mmengine
-import numpy as np
 import torch
-from mmcv.parallel import collate, scatter
-from mmengine.dataset import Compose
+import torch.nn as nn
+from mmengine.dataset import Compose, pseudo_collate
 from mmengine.runner import load_checkpoint
 
-from mmaction.core import OutputHook
 from mmaction.registry import MODELS
 
 
-def init_recognizer(config, checkpoint=None, device='cuda:0', **kwargs):
+def init_recognizer(config: Union[str, mmengine.Config],
+                    checkpoint: Optional[str] = None,
+                    device: Union[str, torch.device] = 'cuda:0') -> nn.Module:
     """Initialize a recognizer from config file.
 
     Args:
-        config (str | :obj:`mmcv.Config`): Config file path or the config
+        config (Union[str, mmengine.Config]): Config file path or the config
             object.
-        checkpoint (str | None, optional): Checkpoint path/url. If set to None,
-            the model will not load any weights. Default: None.
-        device (str | :obj:`torch.device`): The desired device of returned
-            tensor. Default: 'cuda:0'.
+        checkpoint (str, optional): Checkpoint path/url. If set to None,
+            the model will not load any weights. Defaults to None.
+        device (Union[str, ``torch.device``]): The desired device of returned
+            tensor. Defaults to ``cuda:0``.
 
     Returns:
         nn.Module: The constructed recognizer.
     """
-    if 'use_frames' in kwargs:
-        warnings.warn('The argument `use_frames` is deprecated PR #1191. '
-                      'Now you can use models trained with frames or videos '
-                      'arbitrarily. ')
-
     if isinstance(config, str):
         config = mmengine.Config.fromfile(config)
     elif not isinstance(config, mmengine.Config):
@@ -43,7 +35,7 @@ def init_recognizer(config, checkpoint=None, device='cuda:0', **kwargs):
 
     # pretrained model is unnecessary since we directly load checkpoint later
     config.model.backbone.pretrained = None
-    model = MODELS.build(config.model, test_cfg=config.get('test_cfg'))
+    model = MODELS.build(config.model)
 
     if checkpoint is not None:
         load_checkpoint(model, checkpoint, map_location='cpu')
@@ -53,140 +45,40 @@ def init_recognizer(config, checkpoint=None, device='cuda:0', **kwargs):
     return model
 
 
-def inference_recognizer(model, video, outputs=None, as_tensor=True, **kwargs):
+def inference_recognizer(model: nn.Module,
+                         video: str) -> List[Tuple[int, float]]:
     """Inference a video with the recognizer.
 
     Args:
         model (nn.Module): The loaded recognizer.
-        video (str | dict | ndarray): The video file path / url or the
-            rawframes directory path / results dictionary (the input of
-            pipeline) / a 4D array T x H x W x 3 (The input video).
-        outputs (list(str) | tuple(str) | str | None) : Names of layers whose
-            outputs need to be returned, default: None.
-        as_tensor (bool): Same as that in ``OutputHook``. Default: True.
+        video (str): The video file path.
 
     Returns:
-        dict[tuple(str, float)]: Top-5 recognition result dict.
-        dict[torch.tensor | np.ndarray]:
-            Output feature maps from layers specified in `outputs`.
+        List[Tuple(int, float)]: Top-5 recognition result dict.
     """
-    if 'use_frames' in kwargs:
-        warnings.warn('The argument `use_frames` is deprecated PR #1191. '
-                      'Now you can use models trained with frames or videos '
-                      'arbitrarily. ')
-    if 'label_path' in kwargs:
-        warnings.warn('The argument `use_frames` is deprecated PR #1191. '
-                      'Now the label file is not needed in '
-                      'inference_recognizer. ')
-
-    input_flag = None
-    if isinstance(video, dict):
-        input_flag = 'dict'
-    elif isinstance(video, np.ndarray):
-        assert len(video.shape) == 4, 'The shape should be T x H x W x C'
-        input_flag = 'array'
-    elif isinstance(video, str) and video.startswith('http'):
-        input_flag = 'video'
-    elif isinstance(video, str) and osp.exists(video):
-        if osp.isfile(video):
-            if video.endswith('.npy'):
-                input_flag = 'audio'
-            else:
-                input_flag = 'video'
-        if osp.isdir(video):
-            input_flag = 'rawframes'
-    else:
-        raise RuntimeError('The type of argument video is not supported: '
-                           f'{type(video)}')
-
-    if isinstance(outputs, str):
-        outputs = (outputs, )
-    assert outputs is None or isinstance(outputs, (tuple, list))
-
     cfg = model.cfg
-    device = next(model.parameters()).device  # model device
-    # build the data pipeline
-    test_pipeline = cfg.data.test.pipeline
-    # Alter data pipelines & prepare inputs
-    if input_flag == 'dict':
-        data = video
-    if input_flag == 'array':
-        modality_map = {2: 'Flow', 3: 'RGB'}
-        modality = modality_map.get(video.shape[-1])
-        data = dict(
-            total_frames=video.shape[0],
-            label=-1,
-            start_index=0,
-            array=video,
-            modality=modality)
-        for i in range(len(test_pipeline)):
-            if 'Decode' in test_pipeline[i]['type']:
-                test_pipeline[i] = dict(type='ArrayDecode')
-        test_pipeline = [x for x in test_pipeline if 'Init' not in x['type']]
-    if input_flag == 'video':
-        data = dict(filename=video, label=-1, start_index=0, modality='RGB')
-        if 'Init' not in test_pipeline[0]['type']:
-            test_pipeline = [dict(type='OpenCVInit')] + test_pipeline
-        else:
-            test_pipeline[0] = dict(type='OpenCVInit')
-        for i in range(len(test_pipeline)):
-            if 'Decode' in test_pipeline[i]['type']:
-                test_pipeline[i] = dict(type='OpenCVDecode')
-    if input_flag == 'rawframes':
-        filename_tmpl = cfg.data.test.get('filename_tmpl', 'img_{:05}.jpg')
-        modality = cfg.data.test.get('modality', 'RGB')
-        start_index = cfg.data.test.get('start_index', 1)
 
-        # count the number of frames that match the format of `filename_tmpl`
-        # RGB pattern example: img_{:05}.jpg -> ^img_\d+.jpg$
-        # Flow patteren example: {}_{:05d}.jpg -> ^x_\d+.jpg$
-        pattern = f'^{filename_tmpl}$'
-        if modality == 'Flow':
-            pattern = pattern.replace('{}', 'x')
-        pattern = pattern.replace(
-            pattern[pattern.find('{'):pattern.find('}') + 1], '\\d+')
-        total_frames = len(
-            list(
-                filter(lambda x: re.match(pattern, x) is not None,
-                       os.listdir(video))))
-        data = dict(
-            frame_dir=video,
-            total_frames=total_frames,
-            label=-1,
-            start_index=start_index,
-            filename_tmpl=filename_tmpl,
-            modality=modality)
-        if 'Init' in test_pipeline[0]['type']:
-            test_pipeline = test_pipeline[1:]
-        for i in range(len(test_pipeline)):
-            if 'Decode' in test_pipeline[i]['type']:
-                test_pipeline[i] = dict(type='RawFrameDecode')
-    if input_flag == 'audio':
-        data = dict(
-            audio_path=video,
-            total_frames=len(np.load(video)),
-            start_index=cfg.data.test.get('start_index', 1),
-            label=-1)
+    # Build the data pipeline
+    val_pipeline_cfg = cfg.val_dataloader.dataset.pipeline
+    if 'Init' not in val_pipeline_cfg[0]['type']:
+        val_pipeline_cfg = [dict(type='OpenCVInit')] + val_pipeline_cfg
+    else:
+        val_pipeline_cfg[0] = dict(type='OpenCVInit')
+    for i in range(len(val_pipeline_cfg)):
+        if 'Decode' in val_pipeline_cfg[i]['type']:
+            val_pipeline_cfg[i] = dict(type='OpenCVDecode')
+    val_pipeline = Compose(val_pipeline_cfg)
 
-    test_pipeline = Compose(test_pipeline)
-    data = test_pipeline(data)
-    data = collate([data], samples_per_gpu=1)
+    # Prepare & process inputs
+    data = dict(filename=video, label=-1, start_index=0, modality='RGB')
+    data = val_pipeline(data)
+    data = pseudo_collate([data])
 
-    if next(model.parameters()).is_cuda:
-        # scatter to specified GPU
-        data = scatter(data, [device])[0]
+    # Forward the model
+    with torch.no_grad():
+        pred_scores = model.val_step(data)[0].pred_scores.item.tolist()
+        score_tuples = tuple(zip(range(len(pred_scores)), pred_scores))
+        score_sorted = sorted(score_tuples, key=itemgetter(1), reverse=True)
+        top5_label = score_sorted[:5]
 
-    # forward the model
-    with OutputHook(model, outputs=outputs, as_tensor=as_tensor) as h:
-        with torch.no_grad():
-            scores = model(return_loss=False, **data)[0]
-        returned_features = h.layer_outputs if outputs else None
-
-    num_classes = scores.shape[-1]
-    score_tuples = tuple(zip(range(num_classes), scores))
-    score_sorted = sorted(score_tuples, key=itemgetter(1), reverse=True)
-
-    top5_label = score_sorted[:5]
-    if outputs:
-        return top5_label, returned_features
     return top5_label
