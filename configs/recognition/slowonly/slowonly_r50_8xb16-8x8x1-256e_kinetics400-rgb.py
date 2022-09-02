9aa81717dd053866e1a8afc4f1e7271776013b2d
Kai Hu <kaiorhu@gmail.com>
2022-09-02 02:52:39 -0400

    [Fix] Fix configs and README for SlowOnly (#1889)
    * [Fix] Fix configs for SlowOnly

--- configs/recognition/slowonly/slowonly_r50_8xb16-8x8x1-256e_kinetics400-rgb.py
+++ configs/recognition/slowonly/slowonly_r50_8xb16-8x8x1-256e_kinetics400-rgb.py
@@ -1,9 +1,4 @@
-_base_ = [
-    '../../_base_/models/slowonly_r50.py', '../../_base_/default_runtime.py'
-]
-
-# model settings
-model = dict(backbone=dict(pretrained=None))
+_base_ = ['slowonly_r50_8xb16-4x16x1-256e_kinetics400-rgb.py']
 
 # dataset settings
 dataset_type = 'VideoDataset'
@@ -11,54 +6,52 @@ data_root = 'data/kinetics400/videos_train'
 data_root_val = 'data/kinetics400/videos_val'
 ann_file_train = 'data/kinetics400/kinetics400_train_list_videos.txt'
 ann_file_val = 'data/kinetics400/kinetics400_val_list_videos.txt'
-ann_file_test = 'data/kinetics400/kinetics400_val_list_videos.txt'
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=False)
+
 train_pipeline = [
     dict(type='DecordInit'),
-    dict(type='SampleFrames', clip_len=4, frame_interval=16, num_clips=1),
+    dict(type='SampleFrames', clip_len=8, frame_interval=8, num_clips=1),
     dict(type='DecordDecode'),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='RandomResizedCrop'),
     dict(type='Resize', scale=(224, 224), keep_ratio=False),
     dict(type='Flip', flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
     dict(type='FormatShape', input_format='NCTHW'),
     dict(type='PackActionInputs')
 ]
+
 val_pipeline = [
     dict(type='DecordInit'),
     dict(
         type='SampleFrames',
-        clip_len=4,
-        frame_interval=16,
+        clip_len=8,
+        frame_interval=8,
         num_clips=1,
         test_mode=True),
     dict(type='DecordDecode'),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='CenterCrop', crop_size=224),
-    dict(type='Normalize', **img_norm_cfg),
     dict(type='FormatShape', input_format='NCTHW'),
     dict(type='PackActionInputs')
 ]
+
 test_pipeline = [
     dict(type='DecordInit'),
     dict(
         type='SampleFrames',
-        clip_len=4,
-        frame_interval=16,
+        clip_len=8,
+        frame_interval=8,
         num_clips=10,
         test_mode=True),
     dict(type='DecordDecode'),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='ThreeCrop', crop_size=256),
-    dict(type='Normalize', **img_norm_cfg),
     dict(type='FormatShape', input_format='NCTHW'),
     dict(type='PackActionInputs')
 ]
+
 train_dataloader = dict(
-    batch_size=8,
-    num_workers=2,
+    batch_size=16,
+    num_workers=8,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=True),
     dataset=dict(
@@ -66,9 +59,10 @@ train_dataloader = dict(
         ann_file=ann_file_train,
         data_prefix=dict(video=data_root),
         pipeline=train_pipeline))
+
 val_dataloader = dict(
-    batch_size=8,
-    num_workers=2,
+    batch_size=16,
+    num_workers=8,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=False),
     dataset=dict(
@@ -77,42 +71,15 @@ val_dataloader = dict(
         data_prefix=dict(video=data_root_val),
         pipeline=val_pipeline,
         test_mode=True))
+
 test_dataloader = dict(
     batch_size=1,
-    num_workers=2,
+    num_workers=8,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=False),
     dataset=dict(
         type=dataset_type,
-        ann_file=ann_file_test,
+        ann_file=ann_file_val,
         data_prefix=dict(video=data_root_val),
         pipeline=test_pipeline,
         test_mode=True))
-
-val_evaluator = dict(type='AccMetric')
-test_evaluator = val_evaluator
-
-val_cfg = dict(interval=1)
-test_cfg = dict()
-# optimizer
-optimizer = dict(
-    type='SGD', lr=0.1, momentum=0.9,
-    weight_decay=0.0001)  # this lr is used for 8 gpus
-
-# learning policy
-param_scheduler = [
-    dict(type='LinearLR', start_factor=0.1, by_epoch=True, begin=0, end=34),
-    dict(
-        type='CosineAnnealingLR',
-        T_max=222,
-        eta_min=0,
-        by_epoch=True,
-        begin=34,
-        end=256)
-]
-train_cfg = dict(by_epoch=True, max_epochs=256)
-
-# runtime settings
-default_hooks = dict(
-    optimizer=dict(grad_clip=dict(max_norm=40, norm_type=2)),
-    checkpoint=dict(interval=4))
