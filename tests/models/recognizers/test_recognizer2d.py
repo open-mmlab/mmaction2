023579f908317ff030cd0c165c91114984a74640
wxDai <daiwenxun@pjlab.org.cn>
2022-09-02 14:51:05 +0800

    update trn (#1888)

--- tests/models/recognizers/test_recognizer2d.py
+++ tests/models/recognizers/test_recognizer2d.py
@@ -165,7 +165,7 @@ def test_tsm():
 
 def test_trn():
     register_all_modules()
-    config = get_recognizer_cfg('trn/trn_r50_1x1x8_50e_8xb16_sthv1_rgb.py')
+    config = get_recognizer_cfg('trn/trn_r50_8xb16-1x1x8-50e_sthv1-rgb.py')
     config.model['backbone']['pretrained'] = None
 
     recognizer = MODELS.build(config.model)
