e6045ed24e15872622905d807b067c39bfb36126
wxDai <daiwenxun@pjlab.org.cn>
2022-09-27 11:08:27 +0800

    modify stgcn-ntu60 (#1914)

--- configs/_base_/models/stgcn.py
+++ configs/_base_/models/stgcn.py
@@ -1,6 +1,3 @@
-preprocess_cfg = dict(
-    mean=[960., 540., 0.5], std=[1920, 1080, 1.], format_shape='NCTVM')
-
 model = dict(
     type='RecognizerGCN',
     backbone=dict(
@@ -13,6 +10,5 @@ model = dict(
         num_classes=60,
         in_channels=256,
         loss_cls=dict(type='CrossEntropyLoss')),
-    data_preprocessor=dict(type='ActionDataPreprocessor', **preprocess_cfg),
     train_cfg=None,
     test_cfg=None)
