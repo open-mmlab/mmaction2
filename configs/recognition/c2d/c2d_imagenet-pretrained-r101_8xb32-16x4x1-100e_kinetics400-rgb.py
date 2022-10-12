2c309e21d5f61fe3508a46e486de9bfdaef15ea2
InvincibleWyq <wyq1217@outlook.com>
2022-10-12 13:06:59 +0800

    rename cfg

--- configs/recognition/c2d/c2d_imagenet-pretrained-r101_8xb32-16x4x1-100e_kinetics400-rgb.py
+++
@@ -1,7 +0,0 @@
-_base_ = ['c2d_imagenet-pretrained-r50_8xb32-16x4x1-100e_kinetics400-rgb.py']
-
-model = dict(
-    backbone=dict(
-        pretrained=('https://download.pytorch.org/'
-                    'models/resnet101-cd907fc2.pth'),
-        depth=101))
