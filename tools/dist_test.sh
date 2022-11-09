c3fe6d867e99fd18c3ae3e4e6174311060c255f2
wxDai <daiwenxun@pjlab.org.cn>
2022-11-09 20:18:28 +0800

    [Docs] Refactor `4_train_test.md` (#2038)

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-NNODES=${NNODES:-1}
-NODE_RANK=${NODE_RANK:-0}
-MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
+set -x
+
 CONFIG=$1
 CHECKPOINT=$2
 GPUS=$3
+NNODES=${NNODES:-1}
+NODE_RANK=${NODE_RANK:-0}
 PORT=${PORT:-29500}
+MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 # Arguments starting from the forth one are captured by ${@:4}
--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-NNODES=${NNODES:-1}
-NODE_RANK=${NODE_RANK:-0}
-MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
+set -x
+
 CONFIG=$1
 GPUS=$2
+NNODES=${NNODES:-1}
+NODE_RANK=${NODE_RANK:-0}
 PORT=${PORT:-29500}
+MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nnodes=$NNODES --node_rank=$NODE_RANK --master_addr=$MASTER_ADDR \
