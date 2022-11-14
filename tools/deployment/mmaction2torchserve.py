d3e62bdd17a31e276afc686e2fba3671008d5f75
austinmw <austinmw89@gmail.com>
2022-11-14 02:21:09 -0500

    switch to mmengine

--- tools/deployment/mmaction2torchserve.py
+++ tools/deployment/mmaction2torchserve.py
@@ -4,7 +4,8 @@ from argparse import ArgumentParser, Namespace
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
-import mmcv
+from mmengine.config import Config
+from mmengine.utils import mkdir_or_exist
 
 try:
     from model_archiver.model_packaging import package_model
@@ -38,9 +39,9 @@ def mmaction2torchserve(
         force (bool): If True, if there is an existing `{model_name}.mar` file
             under `output_folder` it will be overwritten.
     """
-    mmcv.mkdir_or_exist(output_folder)
+    mkdir_or_exist(output_folder)
 
-    config = mmcv.Config.fromfile(config_file)
+    config = Config.fromfile(config_file)
 
     with TemporaryDirectory() as tmpdir:
         config.dump(f'{tmpdir}/config.py')
@@ -107,3 +108,4 @@ if __name__ == '__main__':
     mmaction2torchserve(args.config, args.checkpoint, args.output_folder,
                         args.model_name, args.label_file, args.model_version,
                         args.force)
+    
