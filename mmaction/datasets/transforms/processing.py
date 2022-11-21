1b63b84aa93753a3e267901ec03caeab4fae2fe5
daiwenxun.vendor <daiwenxun@pjlab.org.cn>
2022-11-21 11:40:58 +0800

    add PreNormalize3D

--- mmaction/datasets/transforms/processing.py
+++ mmaction/datasets/transforms/processing.py
@@ -1491,3 +1491,94 @@ class MelSpectrogram(BaseTransform):
                     f'n_mels={self.n_mels}, '
                     f'fixed_length={self.fixed_length})')
         return repr_str
+
+
+@TRANSFORMS.register_module()
+class PreNormalize3D:
+    """PreNormalize for NTURGB+D 3D keypoints (x, y, z).
+    Codes adapted from https://github.com/lshiwjx/2s-AGCN."""
+
+    def unit_vector(self, vector: np.ndarray) -> np.ndarray:
+        """Returns the unit vector of the vector."""
+        return vector / np.linalg.norm(vector)
+
+    def angle_between(self, v1: np.ndarray, v2: np.ndarray) -> float:
+        """Returns the angle in radians between vectors 'v1' and 'v2'."""
+        if np.abs(v1).sum() < 1e-6 or np.abs(v2).sum() < 1e-6:
+            return 0
+        v1_u = self.unit_vector(v1)
+        v2_u = self.unit_vector(v2)
+        return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+
+    def rotation_matrix(self, axis, theta):
+        """Returns the rotation matrix associated with counterclockwise rotation
+        about the given axis by theta radians."""
+        if np.abs(axis).sum() < 1e-6 or np.abs(theta) < 1e-6:
+            return np.eye(3)
+        axis = np.asarray(axis)
+        axis = axis / np.sqrt(np.dot(axis, axis))
+        a = np.cos(theta / 2.0)
+        b, c, d = -axis * np.sin(theta / 2.0)
+        aa, bb, cc, dd = a * a, b * b, c * c, d * d
+        bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
+        return np.array([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
+                        [2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)],
+                        [2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc]])
+
+    def __init__(self, zaxis=[0, 1], xaxis=[8, 4], align_spine=True, align_center=True):
+        self.zaxis = zaxis
+        self.xaxis = xaxis
+        self.align_spine = align_spine
+        self.align_center = align_center
+
+    def __call__(self, results):
+        skeleton = results['keypoint']
+        total_frames = results.get('total_frames', skeleton.shape[1])
+
+        M, T, V, C = skeleton.shape
+        assert T == total_frames
+        if skeleton.sum() == 0:
+            return results
+
+        index0 = [i for i in range(T) if not np.all(np.isclose(skeleton[0, i], 0))]
+
+        assert M in [1, 2]
+        if M == 2:
+            index1 = [i for i in range(T) if not np.all(np.isclose(skeleton[1, i], 0))]
+            if len(index0) < len(index1):
+                skeleton = skeleton[:, np.array(index1)]
+                skeleton = skeleton[[1, 0]]
+            else:
+                skeleton = skeleton[:, np.array(index0)]
+        else:
+            skeleton = skeleton[:, np.array(index0)]
+
+        T_new = skeleton.shape[1]
+
+        if self.align_center:
+            if skeleton.shape[2] == 25:
+                main_body_center = skeleton[0, 0, 1].copy()
+            else:
+                main_body_center = skeleton[0, 0, -1].copy()
+            mask = ((skeleton != 0).sum(-1) > 0)[..., None]
+            skeleton = (skeleton - main_body_center) * mask
+
+        if self.align_spine:
+            joint_bottom = skeleton[0, 0, self.zaxis[0]]
+            joint_top = skeleton[0, 0, self.zaxis[1]]
+            axis = np.cross(joint_top - joint_bottom, [0, 0, 1])
+            angle = self.angle_between(joint_top - joint_bottom, [0, 0, 1])
+            matrix_z = self.rotation_matrix(axis, angle)
+            skeleton = np.einsum('abcd,kd->abck', skeleton, matrix_z)
+
+            joint_rshoulder = skeleton[0, 0, self.xaxis[0]]
+            joint_lshoulder = skeleton[0, 0, self.xaxis[1]]
+            axis = np.cross(joint_rshoulder - joint_lshoulder, [1, 0, 0])
+            angle = self.angle_between(joint_rshoulder - joint_lshoulder, [1, 0, 0])
+            matrix_x = self.rotation_matrix(axis, angle)
+            skeleton = np.einsum('abcd,kd->abck', skeleton, matrix_x)
+
+        results['keypoint'] = skeleton
+        results['total_frames'] = T_new
+        results['body_center'] = main_body_center
+        return results
