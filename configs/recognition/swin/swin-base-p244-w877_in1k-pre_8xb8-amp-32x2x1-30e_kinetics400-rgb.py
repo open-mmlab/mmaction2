c57a27009d84b1c6626e59c3bc08467c5e0bc58f
Dai-Wenxun <wxdai2001@gmail.com>
2022-12-08 19:28:57 +0800

    fix flops

--- configs/recognition/swin/swin-base-p244-w877_in1k-pre_8xb8-amp-32x2x1-30e_kinetics400-rgb.py
+++ configs/recognition/swin/swin-base-p244-w877_in1k-pre_8xb8-amp-32x2x1-30e_kinetics400-rgb.py
@@ -19,11 +19,11 @@ ann_file_train = 'data/kinetics400/kinetics400_train_list_videos.txt'
 ann_file_val = 'data/kinetics400/kinetics400_val_list_videos.txt'
 ann_file_test = 'data/kinetics400/kinetics400_val_list_videos.txt'
 
-# file_client_args = dict(
-#     io_backend='petrel',
-#     path_mapping=dict(
-#         {'data/kinetics400': 's3://openmmlab/datasets/action/Kinetics400'}))
-file_client_args = dict(io_backend='disk')
+file_client_args = dict(
+    io_backend='petrel',
+    path_mapping=dict(
+        {'data/kinetics400': 's3://openmmlab/datasets/action/Kinetics400'}))
+# file_client_args = dict(io_backend='disk')
 train_pipeline = [
     dict(type='DecordInit', **file_client_args),
     dict(type='SampleFrames', clip_len=32, frame_interval=2, num_clips=1),
