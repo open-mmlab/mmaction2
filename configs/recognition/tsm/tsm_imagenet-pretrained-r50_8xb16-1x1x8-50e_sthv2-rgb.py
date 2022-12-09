82515a3911bdaf9d06651f9091b9ca3e19c44cef
KaiHoo <kaiorhu@gmail.com>
2022-12-08 23:50:19 -0500

    update README

--- configs/recognition/tsm/tsm_imagenet-pretrained-r50_8xb16-1x1x8-50e_sthv2-rgb.py
+++ configs/recognition/tsm/tsm_imagenet-pretrained-r50_8xb16-1x1x8-50e_sthv2-rgb.py
@@ -121,3 +121,9 @@ optim_wrapper = dict(
     paramwise_cfg=dict(fc_lr5=True),
     optimizer=dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0005),
     clip_grad=dict(max_norm=20, norm_type=2))
+
+# Default setting for scaling LR automatically
+#   - `enable` means enable scaling LR automatically
+#       or not by default.
+#   - `base_batch_size` = (8 GPUs) x (16 samples per GPU).
+auto_scale_lr = dict(enable=False, base_batch_size=128)
