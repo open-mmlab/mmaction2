2439d3c6e752c1c9bf8bae242a0f1e9f22ee8dfb
KaiHoo <kaiorhu@gmail.com>
2022-12-12 19:36:51 -0500

    rm file args

--- configs/recognition/tsn/tsn_imagenet-pretrained-r50_8xb32-1x1x16-50e_sthv2-rgb.py
+++ configs/recognition/tsn/tsn_imagenet-pretrained-r50_8xb32-1x1x16-50e_sthv2-rgb.py
@@ -1,9 +1,5 @@
 _base_ = ['tsn_imagenet-pretrained-r50_8xb32-1x1x8-50e_sthv2-rgb.py']
 
-# file_client_args = dict(
-#      io_backend='petrel',
-#      path_mapping=dict(
-#          {'data/sthv2': 's3://openmmlab/datasets/action/sthv2'}))
 file_client_args = dict(io_backend='disk')
 
 train_pipeline = [
