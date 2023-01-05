17feae82b9a4bf48c2b0fe75132aa6f18c918a2b
wxDai <daiwenxun@pjlab.org.cn>
2023-01-05 14:54:29 +0800

    [Feature] Support STGCN++ (#2156)

--- mmaction/models/utils/gcn_utils.py
+++ mmaction/models/utils/gcn_utils.py
@@ -310,3 +310,112 @@ class unit_tcn(BaseModule):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Defines the computation performed at every call."""
         return self.drop(self.bn(self.conv(x)))
+
+
+class mstcn(BaseModule):
+    """The multi-scale temporal convolutional network.
+
+    Args:
+        in_channels (int): Number of input channels.
+        out_channels (int): Number of output channels.
+        mid_channels (int): Number of middle channels. Defaults to None.
+        dropout (float): Dropout probability. Defaults to 0.
+        ms_cfg (list): The config of multi-scale branches. Defaults to
+            ``[(3, 1), (3, 2), (3, 3), (3, 4), ('max', 3), '1x1']``.
+        stride (int): Stride of the temporal convolution. Defaults to 1.
+        init_cfg (dict or list[dict]): Initialization config dict.
+            Defaults to None.
+    """
+
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 mid_channels: int = None,
+                 dropout: float = 0.,
+                 ms_cfg: List = [(3, 1), (3, 2), (3, 3), (3, 4), ('max', 3),
+                                 '1x1'],
+                 stride: int = 1,
+                 init_cfg: Union[Dict, List[Dict]] = None) -> None:
+        super().__init__(init_cfg=init_cfg)
+        # Multiple branches of temporal convolution
+        self.ms_cfg = ms_cfg
+        num_branches = len(ms_cfg)
+        self.num_branches = num_branches
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.act = nn.ReLU()
+
+        if mid_channels is None:
+            mid_channels = out_channels // num_branches
+            rem_mid_channels = out_channels - mid_channels * (num_branches - 1)
+        else:
+            assert isinstance(mid_channels, float) and mid_channels > 0
+            mid_channels = int(out_channels * mid_channels)
+            rem_mid_channels = mid_channels
+
+        self.mid_channels = mid_channels
+        self.rem_mid_channels = rem_mid_channels
+
+        branches = []
+        for i, cfg in enumerate(ms_cfg):
+            branch_c = rem_mid_channels if i == 0 else mid_channels
+            if cfg == '1x1':
+                branches.append(
+                    nn.Conv2d(
+                        in_channels,
+                        branch_c,
+                        kernel_size=1,
+                        stride=(stride, 1)))
+                continue
+            assert isinstance(cfg, tuple)
+            if cfg[0] == 'max':
+                branches.append(
+                    Sequential(
+                        nn.Conv2d(in_channels, branch_c, kernel_size=1),
+                        nn.BatchNorm2d(branch_c), self.act,
+                        nn.MaxPool2d(
+                            kernel_size=(cfg[1], 1),
+                            stride=(stride, 1),
+                            padding=(1, 0))))
+                continue
+            assert isinstance(cfg[0], int) and isinstance(cfg[1], int)
+            branch = Sequential(
+                nn.Conv2d(in_channels, branch_c, kernel_size=1),
+                nn.BatchNorm2d(branch_c), self.act,
+                unit_tcn(
+                    branch_c,
+                    branch_c,
+                    kernel_size=cfg[0],
+                    stride=stride,
+                    dilation=cfg[1],
+                    norm=None))
+            branches.append(branch)
+
+        self.branches = ModuleList(branches)
+        tin_channels = mid_channels * (num_branches - 1) + rem_mid_channels
+
+        self.transform = Sequential(
+            nn.BatchNorm2d(tin_channels), self.act,
+            nn.Conv2d(tin_channels, out_channels, kernel_size=1))
+
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.drop = nn.Dropout(dropout, inplace=True)
+
+    def inner_forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Defines the computation performed at every call."""
+        N, C, T, V = x.shape
+
+        branch_outs = []
+        for tempconv in self.branches:
+            out = tempconv(x)
+            branch_outs.append(out)
+
+        feat = torch.cat(branch_outs, dim=1)
+        feat = self.transform(feat)
+        return feat
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Defines the computation performed at every call."""
+        out = self.inner_forward(x)
+        out = self.bn(out)
+        return self.drop(out)
