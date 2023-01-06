1983c93192e5facb5ede29fa3bcad293c6e82082
KaiHoo <kaiorhu@gmail.com>
2023-01-05 20:54:20 -0500

    fix

--- tests/models/backbones/test_resnet_omni.py
+++ tests/models/backbones/test_resnet_omni.py
@@ -9,14 +9,12 @@ from mmaction.testing import generate_backbone_demo_inputs
 
 def test_x3d_backbone():
     """Test x3d backbone."""
-    with pytest.raises(AssertionError):
-        OmniResNet()
+    _ = OmniResNet()
 
-    with pytest.raises(AssertionError):
-        resnet50 = torchvision.models.resnet50()
-        params = resnet50.state_dict()
-        torch.save(params, './r50.pth')
-        model = OmniResNet(pretrain_2d='./r50.pth')
+    resnet50 = torchvision.models.resnet50()
+    params = resnet50.state_dict()
+    torch.save(params, './r50.pth')
+    model = OmniResNet(pretrain_2d='./r50.pth')
 
     input_shape = (2, 3, 8, 64, 64)
     videos = generate_backbone_demo_inputs(input_shape)
