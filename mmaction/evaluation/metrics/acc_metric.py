ab751dd108f006c3579d437509ad19b2893e9b38
Dai-Wenxun <wxdai2001@gmail.com>
2023-01-29 15:32:29 +0800

    fix

--- mmaction/evaluation/metrics/acc_metric.py
+++ mmaction/evaluation/metrics/acc_metric.py
@@ -1,5 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from typing import Any, Sequence, Tuple
+from typing import Any, Optional, Sequence, Tuple, Union
 
 from mmeval import Accuracy as MMEVAL_Accuracy
 
@@ -8,6 +8,10 @@ from mmaction.registry import METRICS
 
 @METRICS.register_module()
 class AccMetric(MMEVAL_Accuracy):
+    default_prefix: Optional[str] = 'acc'
+
+    def __init__(self, topk: Union[int, Sequence[int]] = (1, 5)):
+        super(AccMetric, self).__init__(topk=topk)
 
     def process(self, data_batch: Sequence[Tuple[Any, dict]],
                 data_samples: Sequence[dict]) -> None:
@@ -28,7 +32,7 @@ class AccMetric(MMEVAL_Accuracy):
             label = data_sample['gt_labels']
             predictions.append(pred['item'])
             labels.append(label['item'])
-            self.add(predictions, labels)
+        self.add(predictions, labels)
 
     def evaluate(self, *args, **kwargs) -> dict:
         """Returns metric results and reset state.
