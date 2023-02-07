a9d5b3f4373c1733fdc769b09216d220cb6e9b53
lilin <lilin@pjlab.org.cn>
2023-02-07 10:49:34 +0800

    rename inferencer

--- mmaction/apis/inferencers/__init__.py
+++ mmaction/apis/inferencers/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .actionrecog_inferencer import ActionRecogInferencer
-from .mmaction_inferencer import MMAction2Inferencer
+from .mmaction2_inferencer import MMAction2Inferencer
 
 __all__ = ['ActionRecogInferencer', 'MMAction2Inferencer']
