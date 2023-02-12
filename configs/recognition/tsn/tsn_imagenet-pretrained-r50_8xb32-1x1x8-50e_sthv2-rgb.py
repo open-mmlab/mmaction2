f6ea38ad5bfa29fb24f665769c0e29c3f4ee726a
leexinhao <1520491933@qq.com>
2023-02-12 11:08:09 +0800

    Add lip_label_map for Flip in some configs

--- configs/recognition/tsn/tsn_imagenet-pretrained-r50_8xb32-1x1x8-50e_sthv2-rgb.py
+++ configs/recognition/tsn/tsn_imagenet-pretrained-r50_8xb32-1x1x8-50e_sthv2-rgb.py
@@ -14,6 +14,7 @@ ann_file_val = 'data/sthv2/sthv2_val_list_videos.txt'
 
 file_client_args = dict(io_backend='disk')
 
+sthv2_flip_label_map = {86: 87, 87: 86, 93: 94, 94: 93, 166: 167, 167: 166}
 train_pipeline = [
     dict(type='DecordInit', **file_client_args),
     dict(type='SampleFrames', clip_len=1, frame_interval=1, num_clips=8),
@@ -26,7 +27,7 @@ train_pipeline = [
         random_crop=False,
         max_wh_scale_gap=1),
     dict(type='Resize', scale=(224, 224), keep_ratio=False),
-    dict(type='Flip', flip_ratio=0.5),
+    dict(type='Flip', flip_ratio=0.5, flip_label_map=sthv2_flip_label_map),
     dict(type='FormatShape', input_format='NCHW'),
     dict(type='PackActionInputs')
 ]
