eb9cf8f2fbb397f34282873600eca5b9cb9f7e8d
Dai-Wenxun <wxDai2001@gmail.com>
2023-02-19 11:18:16 +0800

    add blending UT for data_preprocessor

--- tests/models/data_preprocessors/test_data_preprocessor.py
+++ tests/models/data_preprocessors/test_data_preprocessor.py
@@ -5,6 +5,7 @@ import torch
 import pytest
 from mmaction.models import ActionDataPreprocessor
 from mmaction.structures import ActionDataSample
+from mmaction.utils import register_all_modules
 
 from numpy.testing import assert_array_equal
 
@@ -40,8 +41,18 @@ def test_data_preprocessor():
     psr = ActionDataPreprocessor(format_shape='NCTHW',
                                  to_rgb=True)
     data = psr(deepcopy(raw_data))
+    assert data['inputs'].shape == (2, 1, 3, 8, 224, 224)
     assert_array_equal(data['inputs'][0], raw_data['inputs'][0][:, [2, 1, 0]])
     assert_array_equal(data['inputs'][1], raw_data['inputs'][1][:, [2, 1, 0]])
+
+    register_all_modules()
+    psr = ActionDataPreprocessor(mean=[123.675, 116.28, 103.53],
+                                 std=[58.395, 57.12, 57.375],
+                                 format_shape='NCTHW',
+                                 blending=dict(type='MixupBlending', num_classes=5))
+    data = psr(deepcopy(raw_data), training=True)
+    assert data['data_samples'][0].gt_labels.item.shape == (5, )
+    assert data['data_samples'][1].gt_labels.item.shape == (5, )
 
     raw_data = generate_dummy_data(2, (1, 3, 224, 224))
     psr = ActionDataPreprocessor(mean=[123.675, 116.28, 103.53],
