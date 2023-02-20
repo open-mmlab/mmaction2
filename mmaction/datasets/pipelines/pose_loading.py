35f0001aa96e13d2ad270208c933e96f0bb65330
Dai-Wenxun <wxDai2001@gmail.com>
2023-02-20 14:41:06 +0800

    fix alias

--- mmaction/datasets/pipelines/pose_loading.py
+++ mmaction/datasets/pipelines/pose_loading.py
@@ -123,7 +123,7 @@ class UniformSampleFrames:
         start_index = results['start_index']
         inds = inds + start_index
 
-        results['frame_inds'] = inds.astype(np.int)
+        results['frame_inds'] = inds.astype(np.int64)
         results['clip_len'] = self.clip_len
         results['frame_interval'] = None
         results['num_clips'] = self.num_clips
@@ -659,7 +659,7 @@ class PaddingWithLoop:
         inds = np.arange(start, start + self.clip_len)
         inds = np.mod(inds, num_frames)
 
-        results['frame_inds'] = inds.astype(np.int)
+        results['frame_inds'] = inds.astype(np.int64)
         results['clip_len'] = self.clip_len
         results['frame_interval'] = None
         results['num_clips'] = self.num_clips
