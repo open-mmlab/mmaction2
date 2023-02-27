1a85fb91bbea75220c16ed0dd6f5e8410b5fb551
lilin <lilin@pjlab.org.cn>
2023-02-27 15:32:02 +0800

    [fix] specify map_location to cpu when use _load_checkpoint

--- mmaction/models/backbones/resnet3d.py
+++ mmaction/models/backbones/resnet3d.py
@@ -711,7 +711,7 @@ class ResNet3d(nn.Module):
                 debugging information.
         """
 
-        state_dict_r2d = _load_checkpoint(self.pretrained)
+        state_dict_r2d = _load_checkpoint(self.pretrained, map_location='cpu')
         if 'state_dict' in state_dict_r2d:
             state_dict_r2d = state_dict_r2d['state_dict']
 
