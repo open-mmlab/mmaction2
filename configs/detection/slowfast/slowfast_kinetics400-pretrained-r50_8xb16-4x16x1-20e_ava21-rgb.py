472fec1c7371bfe923aae4512003e1dc8dcb9905
Dai-Wenxun <wxdai2001@gmail.com>
2023-03-05 13:09:28 +0800

    fix lint

--- configs/detection/slowfast/slowfast_kinetics400-pretrained-r50_8xb16-4x16x1-20e_ava21-rgb.py
+++ configs/detection/slowfast/slowfast_kinetics400-pretrained-r50_8xb16-4x16x1-20e_ava21-rgb.py
@@ -96,11 +96,15 @@ train_pipeline = [
     dict(type='PackActionInputs')
 ]
 
+file_client_args = dict(
+    io_backend='petrel',
+    path_mapping=dict({'data/ava': 's254:s3://openmmlab/datasets/action/ava'}))
+
 # The testing is w/o. any cropping / flipping
 val_pipeline = [
     dict(
         type='SampleAVAFrames', clip_len=32, frame_interval=2, test_mode=True),
-    dict(type='RawFrameDecode'),
+    dict(type='RawFrameDecode', **file_client_args),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='FormatShape', input_format='NCTHW', collapse=True),
     dict(type='PackActionInputs')
