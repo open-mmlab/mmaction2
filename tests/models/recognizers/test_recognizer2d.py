4aebfb79129ebab3a9cc65d844750e0d9a7ba1b9
lilin <lilin@pjlab.org.cn>
2023-03-16 18:54:02 +0800

    decrese input size in tpn ut

--- tests/models/recognizers/test_recognizer2d.py
+++ tests/models/recognizers/test_recognizer2d.py
@@ -190,7 +190,7 @@ def test_tpn():
 
     recognizer = MODELS.build(config.model)
 
-    input_shape = (1, 8, 3, 224, 224)
+    input_shape = (1, 8, 3, 32, 32)
     demo_inputs = generate_recognizer_demo_inputs(input_shape)
 
     imgs = demo_inputs['imgs']
