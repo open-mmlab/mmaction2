0589389f30bc4092d7e6b60a6ee63c460527d089
lilin <lilin@pjlab.org.cn>
2023-04-06 18:21:33 +0800

    skip ut on windows

--- tests/utils/test_misc.py
+++ tests/utils/test_misc.py
@@ -1,11 +1,15 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import os
 import os.path as osp
+import platform
 from tempfile import TemporaryDirectory
+
+import pytest
 
 from mmaction.utils import frame_extract
 
 
+@pytest.mark.skipif(platform.system() == 'Windows', reason='Windows mem limit')
 def test_frame_extract():
     data_prefix = osp.normpath(osp.join(osp.dirname(__file__), '../data'))
     video_path = osp.join(data_prefix, 'test.mp4')
