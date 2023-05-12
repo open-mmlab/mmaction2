2845b3a63d375f3e8e6ee1b4b27a6dc779ac6bc7
lilin <lilin@pjlab.org.cn>
2023-05-12 12:07:48 +0800

    [Enhancement] Enhance inference apis

--- tests/apis/test_inference.py
+++ tests/apis/test_inference.py
@@ -1,13 +1,17 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import os.path as osp
 from pathlib import Path
+from tempfile import TemporaryDirectory
 from unittest import TestCase
 
 import torch
+from mmengine.testing import assert_dict_has_keys
 from parameterized import parameterized
 
-from mmaction.apis import inference_recognizer, init_recognizer
+from mmaction.apis import (detection_inference, inference_recognizer,
+                           init_recognizer, pose_inference)
 from mmaction.structures import ActionDataSample
+from mmaction.utils import frame_extract
 
 
 class TestInference(TestCase):
@@ -63,3 +67,66 @@ class TestInference(TestCase):
 
             self.assertIsInstance(result, ActionDataSample)
             self.assertTrue(result.pred_scores.item.shape, (400, ))
+
+    def test_detection_inferecne(self):
+        from mmdet.apis import init_detector
+        from mmdet.structures import DetDataSample
+
+        project_dir = osp.abspath(osp.dirname(osp.dirname(__file__)))
+        project_dir = osp.join(project_dir, '..')
+        det_config = 'demo/demo_configs/faster-rcnn_r50_fpn_2x_coco_infer.py'
+        det_ckpt = 'http://download.openmmlab.com/mmdetection/v2.0/' \
+                   'faster_rcnn/faster_rcnn_r50_fpn_2x_coco/faster_rcnn_r50_fpn_2x_coco_bbox_mAP-0.384_20200504_210434-a5d8aa15.pth'  # noqa: E501
+        video_path = 'demo/demo_skeleton.mp4'
+        video_path = osp.join(project_dir, video_path)
+        config_file = osp.join(project_dir, det_config)
+        with TemporaryDirectory() as tmpdir:
+            frm_paths, _ = frame_extract(video_path, out_dir=tmpdir)
+            results, data_samples = detection_inference(
+                config_file, det_ckpt, frm_paths)
+            self.assertTrue(results[0].shape, (4, ))
+            self.assertIsInstance(data_samples[0], DetDataSample)
+            # test with_score
+            results, data_samples = detection_inference(
+                config_file, det_ckpt, frm_paths, with_score=True)
+            self.assertTrue(results[0].shape, (5, ))
+            # test inference with model object
+            model = init_detector(config=det_config, checkpoint=det_ckpt)
+            results, data_samples = detection_inference(model, None, frm_paths)
+            self.assertTrue(results[0].shape, (4, ))
+            self.assertIsInstance(data_samples[0], DetDataSample)
+
+    def test_pose_inferecne(self):
+        from mmpose.apis import init_model
+        from mmpose.structures import PoseDataSample
+
+        project_dir = osp.abspath(osp.dirname(osp.dirname(__file__)))
+        project_dir = osp.join(project_dir, '..')
+        det_config = 'demo/demo_configs/faster-rcnn_r50_fpn_2x_coco_infer.py'
+        det_ckpt = 'http://download.openmmlab.com/mmdetection/v2.0/' \
+                   'faster_rcnn/faster_rcnn_r50_fpn_2x_coco/faster_rcnn_r50_fpn_2x_coco_bbox_mAP-0.384_20200504_210434-a5d8aa15.pth'  # noqa: E501
+        pose_config = 'demo/demo_configs/' \
+                      'td-hm_hrnet-w32_8xb64-210e_coco-256x192_infer.py'
+        pose_ckpt = 'https://download.openmmlab.com/mmpose/top_down/hrnet/' \
+                    'hrnet_w32_coco_256x192-c78dce93_20200708.pth'
+        video_path = 'demo/demo_skeleton.mp4'
+        video_path = osp.join(project_dir, video_path)
+        pose_config = osp.join(project_dir, pose_config)
+        with TemporaryDirectory() as tmpdir:
+            frm_paths, _ = frame_extract(video_path, out_dir=tmpdir)
+            det_results, _ = detection_inference(det_config, det_ckpt,
+                                                 frm_paths)
+
+            results, data_samples = pose_inference(pose_config, pose_ckpt,
+                                                   frm_paths, det_results)
+            assert_dict_has_keys(results[0], ('keypoints', 'bbox_scores',
+                                              'bboxes', 'keypoint_scores'))
+            self.assertIsInstance(data_samples[0], PoseDataSample)
+
+            # test inference with model object
+            model = init_model(config=pose_config, checkpoint=pose_ckpt)
+            results, data_samples = pose_inference(model, None, frm_paths,
+                                                   det_results)
+            assert_dict_has_keys(results[0], ('keypoints', 'bbox_scores',
+                                              'bboxes', 'keypoint_scores'))
+            self.assertIsInstance(data_samples[0], PoseDataSample)
