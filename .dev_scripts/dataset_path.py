4a5ce686b704e0e92ac650198b21c8483ec43aba
lilin <lilin@pjlab.org.cn>
2023-06-19 20:27:05 +0800

    add dataset path

---
+++ .dev_scripts/dataset_path.py
@@ -0,0 +1,8 @@
+dataset = dict(
+    kinetics400='s3://openmmlab/datasets/action/Kinetics400',
+    ucf101='s3://openmmlab/datasets/action/ucf101',
+    ava='s3://openmmlab/datasets/action/ava',
+    ntu='/mnt/petrelfs/share_data/lilin/datasets/skeleton',
+    msr_vtt='/mnt/petrelfs/share_data/lilin/datasets/msrvtt',
+    ava_kinetics='',
+    hacs='')
