1c71e455881bd9cc75e428cf2fc8c631b7fa6235
xiaoyi0919 <88712079+xiaoyi0919@users.noreply.github.com>
2023-08-11 18:07:36 +0800

    Update prepare_msvd.py

--- tools/data/video_retrieval/prepare_msvd.py
+++ tools/data/video_retrieval/prepare_msvd.py
@@ -16,6 +16,7 @@ val_json_path = osp.join(DATA_DIR, 'val.json')
 
 with open(data_path, 'rb') as F:
     data = pickle.load(F)
+
 video_dict = {}
 for one_data in data:
     caption = data[one_data]
@@ -24,10 +25,9 @@ for one_data in data:
     for cap in caption:
         video_dict[one_data].append(' '.join(cap))
 
-# train
-
 with open(train_txt_path, 'r') as f:
     train_avi = f.readlines()
+
 train_avi_list = {}
 for video in train_avi:
     train_avi_list[video.strip()+SUFFIX] = video_dict[video.strip()]
@@ -37,17 +37,20 @@ with open(train_json_path, 'w') as f:
 
 with open(test_txt_path, 'r') as f:
     test_avi = f.readlines()
+
 test_avi_list = {}
 for video in test_avi:
     test_avi_list[video.strip()+SUFFIX] = video_dict[video.strip()]
 with open(test_json_path, 'w') as f:
     json.dump(test_avi_list, f)
 
-# val
 with open(val_txt_path, 'r') as f:
     val_avi = f.readlines()
+
 val_avi_list = {}
 for video in val_avi:
     val_avi_list[video.strip()+SUFFIX] = video_dict[video.strip()]
+
 with open(val_json_path, 'w') as f:
     json.dump(val_avi_list, f)
+
