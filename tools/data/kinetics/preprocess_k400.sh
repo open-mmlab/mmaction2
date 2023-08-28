656c0a4dada0eadc26e4bab7466d16e8269158cd
lilin <lilin@pjlab.org.cn>
2023-08-28 17:02:15 +0800

    update preprocessing script

--- tools/data/kinetics/preprocess_k400.sh
+++ tools/data/kinetics/preprocess_k400.sh
@@ -5,5 +5,5 @@ set -x
 DOWNLOAD_DIR=$1
 DATA_ROOT=$2
 
-cat $DOWNLOAD_DIR/Kinetics-400/raw/*.tar.gz*  | tar -xvz -C $(dirname $DATA_ROOT)
+cat $DOWNLOAD_DIR/OpenMMLab___Kinetics-400/raw/*.tar.gz*  | tar -xvz -C $(dirname $DATA_ROOT)
 mv $(dirname $DATA_ROOT)/Kinetics-400 $DATA_ROOT
--- tools/data/kinetics/preprocess_k600.sh
+++ tools/data/kinetics/preprocess_k600.sh
@@ -5,5 +5,5 @@ set -x
 DOWNLOAD_DIR=$1
 DATA_ROOT=$2
 
-cat $DOWNLOAD_DIR/Kinetics600/raw/*.tar.gz*  | tar -xvz -C $(dirname $DATA_ROOT)
+cat $DOWNLOAD_DIR/OpenMMLab___Kinetics600/raw/*.tar.gz*  | tar -xvz -C $(dirname $DATA_ROOT)
 mv $(dirname $DATA_ROOT)/Kinetics600 $DATA_ROOT
