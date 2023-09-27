3d8ffd5198d767286531edde9fbd762f5c8e9926
Kai Hu <kaiorhu@gmail.com>
2023-09-27 02:51:21 -0400

    add test config (#2694)

---
+++ .dev_scripts/benchmark_options.py
@@ -0,0 +1,1 @@
+default_floating_range = 0.5
---
+++ .dev_scripts/dataset_path.py
@@ -0,0 +1,8 @@
+dataset = dict(
+    kinetics400='s3://openmmlab/datasets/action/Kinetics400',
+    ucf101='s3://openmmlab/datasets/action/ucf101',
+    ava='s3://openmmlab/datasets/action/ava',
+    ntu='/mnt/petrelfs/share_data/lilin/datasets/skeleton',
+    msr_vtt='/mnt/petrelfs/share_data/lilin/datasets/msrvtt',
+    hacs='/mnt/petrelfs/share_data/hukai/HACS',
+    ava_kinetics='/mnt/petrelfs/share_data/hukai/ava_kinetics/')
